7a771e39c6eb20109b2ec050f5a4a41a95cf660b
Confiks <confiks@scriptbase.org>
2019-08-12 22:46:28 +0200

    Project creation on Flutter 1.7.8+hotfix.4:
    flutter create -i swift -a java --description "IRMA Authentication" --org foundation.privacybydesign --project-name "irmamobile" irmamobile

---
+++ android/app/src/main/java/foundation/privacybydesign/irmamobile/MainActivity.java
@@ -0,0 +1,13 @@
+package foundation.privacybydesign.irmamobile;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
