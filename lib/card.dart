6c7c430ed5d0e0f2745406e80b66dddc952522e1
Edwin Martin <edwin@bitstorm.org>
2019-08-26 14:48:04 +0200

    Show personal data

--- lib/card.dart
+++ lib/card.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class IrmaCardState extends State<IrmaCard> {
   @override
   Widget build(BuildContext context) {
+
     return Container(
       // grey box
       child: Stack(
@@ -12,22 +13,74 @@ class IrmaCardState extends State<IrmaCard> {
             child: Text(
               "Persoonsgegevens",
               style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.w900,
+                fontSize: 18.0,
+                fontWeight: FontWeight.w700,
                 color: Colors.white,
               ),
             ),
             left: 100,
-            top: 30,
-          )
+            top: 15,
+          ),
+            Positioned(
+                // red box
+                child: Text(
+                    "Geboren",
+                    style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                    ),
+                ),
+                left: 15,
+                top: 60,
+            ),
+            Positioned(
+                // red box
+                child: Text(
+                    "4 juli 1990",
+                    style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                    ),
+                ),
+                left: 100,
+                top: 60,
+            ),
+            Positioned(
+                // red box
+                child: Text(
+                    "E-mail",
+                    style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                    ),
+                ),
+                left: 15,
+                top: 80,
+            ),
+            Positioned(
+                // red box
+                child: Text(
+                    "anouk.meijer@gmail.com",
+                    style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                    ),
+                ),
+                left: 100,
+                top: 80,
+            ),
         ],
       ),
       width: 320.0,
       height: 240.0,
       decoration: BoxDecoration(
-        color: Colors.red[400],
+        color: Color(0xffec0000),
         borderRadius: BorderRadius.all(
-          const Radius.circular(8.0),
+          const Radius.circular(15.0),
         ),
       ),
     );
