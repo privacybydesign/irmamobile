76b0bb95170c485594314566fe6ba30f56d0956b
Edwin Martin <edwin@bitstorm.org>
2019-08-27 16:22:54 +0200

    Remove app code from Irma home page

--- lib/screens/home/index.dart
+++ lib/screens/home/index.dart
@@ -1,40 +1,24 @@
 import 'package:flutter/material.dart';
 import 'card.dart';
 
-void main() => runApp(IrmaHome());
+class IrmaHome extends StatefulWidget {
+  IrmaHome() : super();
 
-class IrmaHome extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-        return MaterialApp(
-            title: 'IRMA Demo',
-            theme: ThemeData(
-                primarySwatch: Colors.blue,
-            ),
-            home: MyHomePage(title: 'IRMA'),
-        );
-    }
+  @override
+  _IrmaHomeState createState() => _IrmaHomeState();
 }
 
-class MyHomePage extends StatefulWidget {
-    MyHomePage({Key key, this.title}) : super(key: key);
-
-    final String title;
-
-    @override
-    _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(
-                title: Text(widget.title),
-            ),
-            body: Center(
-                child: IrmaCard()
-            ),
-        );
-    }
+class _IrmaHomeState extends State<IrmaHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('IRMA Home'),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () => Navigator.pop(context, false),
+          )),
+      body: Center(child: IrmaCard()),
+    );
+  }
 }
