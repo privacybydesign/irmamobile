4e10d0874bce4cfb8149ec0e664b22f719222ff2
Ruben Nijveld <ruben@gewooniets.nl>
2019-08-28 15:36:17 +0200

    Formatting fixes

--- lib/screens/enrollment/index.dart
+++ lib/screens/enrollment/index.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'widgets/welcome.dart';
 
 class Enrollment extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Welcome();
