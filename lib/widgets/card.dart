def52f8e22b512f593468f97aff8a48dc55b272b
Edwin Martin <edwin@bitstorm.org>
2019-09-09 17:31:54 +0200

    Move animation inside State

--- lib/widgets/card.dart
+++ lib/widgets/card.dart
@@ -3,192 +3,23 @@ import 'package:flutter/animation.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'dart:math' as math;
 
-class AnimatedCard extends AnimatedWidget {
+class _IrmaCardState extends State<IrmaCard>
+    with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
+
   static final _opacityTween = Tween<double>(begin: 0, end: 1);
   static final _heightTween = Tween<double>(begin: 240, end: 500);
   static final _rotateTween = Tween<double>(begin: 0, end: math.pi);
 
+  static const animationDuration = 250;
   static const indent = 100.0;
   static const headerBottom = 30.0;
   static const borderRadius = Radius.circular(15.0);
   static const padding = 15.0;
   static const transparentWhite = Color(0xaaffffff);
 
-  List<Map<String, String>> personalData;
   bool isUnfolded = false;
-  Animation<double> animation;
-  Map<String, Object> issuer; // Object is String | Color
-
-  AnimatedCard(
-      {Key key,
-      AnimationController controller,
-      Animation<double> this.animation,
-      this.personalData,
-      this.issuer})
-      : super(key: key, listenable: controller);
-
-  @override
-  Widget build(BuildContext context) {
-    final AnimationController controller = listenable as AnimationController;
-
-    List<Widget> getDataLines() {
-      var textLines = <Widget>[
-        Divider(color: transparentWhite),
-      ];
-
-      textLines.addAll(personalData.map((personal) {
-        return Padding(
-          padding: EdgeInsets.symmetric(vertical: 4),
-          child: Row(
-            children: [
-              Container(
-                child: Text(personal['key'],
-                    style: TextStyle(
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.w300,
-                      color: issuer['color'],
-                    )),
-                width: indent,
-              ),
-              Text(
-                personal['value'],
-                style: TextStyle(
-                  fontSize: 14.0,
-                  fontWeight: FontWeight.w500,
-                  color: issuer['color'],
-                ),
-              ),
-            ],
-          ),
-        );
-      }));
-
-      return textLines;
-    }
-
-    return Container(
-      child: Column(
-        children: <Widget>[
-          Container(
-            child: Padding(
-              padding: EdgeInsets.only(
-                  top: padding,
-                  right: padding,
-                  bottom: headerBottom,
-                  left: indent),
-              child: Text(
-                "Persoonsgegevens",
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.w700,
-                  color: issuer['color'],
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(padding),
-              child: Opacity(
-                  opacity: _opacityTween.evaluate(animation),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: getDataLines(),
-                  )),
-            ),
-          ),
-          Container(
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: Semantics(
-                    button: true,
-                    enabled: false,
-                    label: 'Uitvouwen',
-                    child: Transform(
-                      origin: Offset(27, 24),
-                      transform: Matrix4.rotationZ(
-                        _rotateTween.evaluate(animation),
-                      ),
-                      child: IconButton(
-                        icon: SvgPicture.asset('assets/icons/arrow-down.svg'),
-                        padding: EdgeInsets.only(left: padding),
-                        alignment: Alignment.centerLeft,
-                        onPressed: () {
-                          if (isUnfolded) {
-                            controller.reverse();
-                          } else {
-                            controller.forward();
-                          }
-//                          setState(() {})
-                          isUnfolded = !isUnfolded;
-                        },
-                      ),
-                    ),
-                  ),
-                ),
-                Semantics(
-                  button: true,
-                  label: 'Bijwerken',
-                  child: Opacity(
-                    opacity: _opacityTween.evaluate(animation),
-                    child: IconButton(
-                      icon: SvgPicture.asset('assets/icons/update.svg'),
-                      padding: EdgeInsets.only(right: padding),
-                      onPressed: () {
-                        print('update');
-                      },
-                    ),
-                  ),
-                ),
-                Semantics(
-                  button: true,
-                  label: 'Verwijderen',
-                  child: Opacity(
-                    opacity: _opacityTween.evaluate(animation),
-                    child: IconButton(
-                      icon: SvgPicture.asset('assets/icons/delete.svg'),
-                      padding: EdgeInsets.only(right: padding),
-                      onPressed: () {
-                        print('delete');
-                      },
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            height: 50,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                bottomLeft: borderRadius,
-                bottomRight: borderRadius,
-              ),
-            ),
-          ),
-        ],
-      ),
-      height: _heightTween.evaluate(animation),
-      margin: const EdgeInsets.symmetric(horizontal: 20),
-      decoration: BoxDecoration(
-          color: issuer['bg-color'],
-          borderRadius: BorderRadius.all(
-            borderRadius,
-          ),
-          image: DecorationImage(
-              image: AssetImage("assets/issuers/${issuer['bg']}"),
-              fit: BoxFit.fitWidth,
-              alignment: Alignment.topCenter)),
-    );
-  }
-}
-
-class _IrmaCardState extends State<IrmaCard>
-    with SingleTickerProviderStateMixin {
-  Animation<double> animation;
-  AnimationController controller;
-
-  static const animationDuration = 250;
 
   @override
   void initState() {
@@ -200,12 +31,161 @@ class _IrmaCardState extends State<IrmaCard>
   }
 
   @override
-  Widget build(BuildContext context) => AnimatedCard(
-        controller: controller,
-        animation: animation,
-        personalData: widget.personalData,
-        issuer: widget.issuer,
-      );
+  Widget build(BuildContext context) => AnimatedBuilder(
+      animation: animation,
+      builder: (buildContext, child) {
+        List<Widget> getDataLines() {
+          var textLines = <Widget>[
+            Divider(color: transparentWhite),
+          ];
+
+          textLines.addAll(widget.personalData.map((personal) {
+            return Padding(
+              padding: EdgeInsets.symmetric(vertical: 4),
+              child: Row(
+                children: [
+                  Container(
+                    child: Text(personal['key'],
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w300,
+                          color: widget.issuer['color'],
+                        )),
+                    width: indent,
+                  ),
+                  Text(
+                    personal['value'],
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w500,
+                      color: widget.issuer['color'],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }));
+
+          return textLines;
+        }
+
+        return Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      top: padding,
+                      right: padding,
+                      bottom: headerBottom,
+                      left: indent),
+                  child: Text(
+                    "Persoonsgegevens",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w700,
+                      color: widget.issuer['color'],
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(padding),
+                  child: Opacity(
+                      opacity: _opacityTween.evaluate(animation),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: getDataLines(),
+                      )),
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Semantics(
+                        button: true,
+                        enabled: false,
+                        label: 'Uitvouwen',
+                        child: Transform(
+                          origin: Offset(27, 24),
+                          transform: Matrix4.rotationZ(
+                            _rotateTween.evaluate(animation),
+                          ),
+                          child: IconButton(
+                            icon:
+                                SvgPicture.asset('assets/icons/arrow-down.svg'),
+                            padding: EdgeInsets.only(left: padding),
+                            alignment: Alignment.centerLeft,
+                            onPressed: () {
+                              if (isUnfolded) {
+                                controller.reverse();
+                              } else {
+                                controller.forward();
+                              }
+                              setState(() {
+                                isUnfolded = !isUnfolded;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                    Semantics(
+                      button: true,
+                      label: 'Bijwerken',
+                      child: Opacity(
+                        opacity: _opacityTween.evaluate(animation),
+                        child: IconButton(
+                          icon: SvgPicture.asset('assets/icons/update.svg'),
+                          padding: EdgeInsets.only(right: padding),
+                          onPressed: () {
+                            print('update');
+                          },
+                        ),
+                      ),
+                    ),
+                    Semantics(
+                      button: true,
+                      label: 'Verwijderen',
+                      child: Opacity(
+                        opacity: _opacityTween.evaluate(animation),
+                        child: IconButton(
+                          icon: SvgPicture.asset('assets/icons/delete.svg'),
+                          padding: EdgeInsets.only(right: padding),
+                          onPressed: () {
+                            print('delete');
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: borderRadius,
+                    bottomRight: borderRadius,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          height: _heightTween.evaluate(animation),
+          margin: const EdgeInsets.symmetric(horizontal: 20),
+          decoration: BoxDecoration(
+              color: widget.issuer['bg-color'],
+              borderRadius: BorderRadius.all(
+                borderRadius,
+              ),
+              image: DecorationImage(
+                  image: AssetImage("assets/issuers/${widget.issuer['bg']}"),
+                  fit: BoxFit.fitWidth,
+                  alignment: Alignment.topCenter)),
+        );
+      });
 }
 
 class IrmaCard extends StatefulWidget {
