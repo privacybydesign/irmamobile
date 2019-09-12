0e865f7fe4736f451fba37756ba2a29984f620cb
Geert-Johan Riemer <geertjohan@geertjohan.net>
2019-09-12 09:51:09 +0200

    Add prototypes view and main entrypoint

--- lib/main_prototypes.dart
+++ lib/main_prototypes.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:irmamobile/app.dart';
 
-void main() => runApp(App());
+void main() => runApp(App.prototypes());
---
+++ lib/src/prototypes/prototypes_menu_page.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:irmamobile/src/prototypes/schermflow_1.dart';
+
+class PrototypesMenuPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("IRMA Prototypes"),
+      ),
+      body: Builder(builder: (context) {
+        return _buildList(context);
+      }),
+    );
+  }
+
+  Padding _buildList(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ListView(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(bottom: 4),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Schermflows",
+                  style: Theme.of(context).textTheme.display1,
+                ),
+                Text("op basis van UX designs",
+                    style: Theme.of(context).textTheme.subhead),
+              ],
+            ),
+          ),
+          Divider(height: 0),
+          _buildListItem(context, "1. Introductie en aanmelden", () {
+            startSchermflow1(context);
+          }),
+          _buildListItem(context, "2. IRMA Account aangemaakt", null),
+          _buildListItem(context, "3. Gemeentegegevens opgehaald", null),
+          _buildListItem(context, "4. Contactgegevens toevoegen", null),
+          _buildListItem(context, "5. Gegevens toevoegen", null),
+          _buildListItem(context, "6. Meerdere gegevenskaarten", null),
+          _buildListItem(context, "7. Vrijgeven leeftijd 18+", null),
+          _buildListItem(
+              context, "8. Vrijgeven leeftijd 18+ & contactgegevens", null),
+          _buildListItem(context, "9. Stemmen Weesperstraat", null),
+          _buildListItem(
+              context, "10. Vrijgeven met eerst kaart ophalen", null),
+          _buildListItem(context, "11. Pincode resetten", null),
+          _buildListItem(context, "12. Gegevens bijna niet meer geldig", null),
+          _buildListItem(context, "13. Gegevens verouders", null),
+          _buildListItem(context, "14. eID kaart toevoegen vanuit IRMA", null),
+          _buildListItem(context, "15. eID kaart delen met IRMA", null),
+          _buildListItem(
+              context, "16. Uitvraag samenstellen door politie", null),
+          _buildListItem(
+              context, "17. Leeftijd 18+ bewijzen via QR-code", null),
+          _buildListItem(context, "18. Bevoegdheid politie opvragen", null),
+          _buildListItem(context, "19. Rijbewijs toevoegen met ID", null),
+          _buildListItem(
+              context, "20. Rijvaardigheid bewijzen via NFC & FaceID", null),
+          _buildListItem(
+              context, "21. Leeftijd 21+ bewijzen via NFC & FraceID", null),
+          _buildListItem(
+              context, "22. Leeftijd 21+ bewijzen via NFC & Pincode", null),
+          _buildListItem(context, "23. ID en rijbewijs landscape", null),
+          SizedBox(height: 12),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 4),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Dev experimenten",
+                  style: Theme.of(context).textTheme.display1,
+                ),
+              ],
+            ),
+          ),
+          Divider(height: 0),
+          _buildListItem(context, "1. foobar", null),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildListItem(
+      BuildContext context, String name, void Function() onTap) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        InkWell(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(name, style: Theme.of(context).textTheme.button),
+                Text(">", style: Theme.of(context).textTheme.button),
+              ],
+            ),
+          ),
+          onTap: () {
+            if (onTap == null) {
+              Scaffold.of(context).showSnackBar(
+                  new SnackBar(content: new Text("Not implemented yet.")));
+              return;
+            }
+            onTap();
+          },
+        ),
+        Divider(height: 0),
+      ],
+    );
+  }
+}
