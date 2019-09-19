b968665ed86f45478b20b54fbfb6078e806c50d4
Edwin Martin <edwin@bitstorm.org>
2019-09-19 11:51:37 +0200

    Move BlurText to Card

--- lib/src/widgets/card.dart
+++ lib/src/widgets/card.dart
@@ -6,7 +6,6 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
 
 import 'button.dart';
-import 'blurtext.dart';
 
 class IrmaCard extends StatefulWidget {
   Map<String, List<Map<String, String>>> personalData;
@@ -190,7 +189,7 @@ class _personalData extends StatelessWidget {
                       .copyWith(color: issuer['color'])),
               width: indent,
             ),
-            BlurText(personal['value'], issuer['color'],
+            _BlurText(personal['value'], issuer['color'],
                 personal['hidden'] == 'true' && !isCardUnblurred),
           ],
         ),
@@ -252,5 +251,48 @@ class _personalData extends StatelessWidget {
     return ListView(
       children: textLines,
     );
+  }
+}
+
+class _BlurText extends StatelessWidget {
+  String text;
+  Color color;
+  bool isTextBlurred;
+
+  _BlurText(this.text, this.color, this.isTextBlurred);
+
+  Widget build(BuildContext context) {
+    return isTextBlurred
+        ? Opacity(
+      opacity: 0.8,
+      child: Text(
+        text,
+        style: Theme.of(context)
+            .textTheme
+            .body1
+            .copyWith(fontWeight: FontWeight.w700)
+            .copyWith(color: Color(0x00ffffff))
+            .copyWith(shadows: [
+          Shadow(
+            blurRadius: 7.0,
+            color: color,
+          ),
+          Shadow(
+            blurRadius: 15.0,
+            color: color,
+          ),
+          Shadow(
+            blurRadius: 20.0,
+            color: color,
+          ),
+        ]),
+      ),
+    )
+        : Text(text,
+        style: Theme.of(context)
+            .textTheme
+            .body1
+            .copyWith(fontWeight: FontWeight.w700)
+            .copyWith(color: color));
   }
 }
