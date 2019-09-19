62ef1d0632fd0575b6d210ab8fb78a672a741fca
Edwin Martin <edwin@bitstorm.org>
2019-09-19 12:00:50 +0200

    Formatting

--- lib/src/widgets/card.dart
+++ lib/src/widgets/card.dart
@@ -264,35 +264,35 @@ class _BlurText extends StatelessWidget {
   Widget build(BuildContext context) {
     return isTextBlurred
         ? Opacity(
-      opacity: 0.8,
-      child: Text(
-        text,
-        style: Theme.of(context)
-            .textTheme
-            .body1
-            .copyWith(fontWeight: FontWeight.w700)
-            .copyWith(color: Color(0x00ffffff))
-            .copyWith(shadows: [
-          Shadow(
-            blurRadius: 7.0,
-            color: color,
-          ),
-          Shadow(
-            blurRadius: 15.0,
-            color: color,
-          ),
-          Shadow(
-            blurRadius: 20.0,
-            color: color,
-          ),
-        ]),
-      ),
-    )
+            opacity: 0.8,
+            child: Text(
+              text,
+              style: Theme.of(context)
+                  .textTheme
+                  .body1
+                  .copyWith(fontWeight: FontWeight.w700)
+                  .copyWith(color: Color(0x00ffffff))
+                  .copyWith(shadows: [
+                Shadow(
+                  blurRadius: 7.0,
+                  color: color,
+                ),
+                Shadow(
+                  blurRadius: 15.0,
+                  color: color,
+                ),
+                Shadow(
+                  blurRadius: 20.0,
+                  color: color,
+                ),
+              ]),
+            ),
+          )
         : Text(text,
-        style: Theme.of(context)
-            .textTheme
-            .body1
-            .copyWith(fontWeight: FontWeight.w700)
-            .copyWith(color: color));
+            style: Theme.of(context)
+                .textTheme
+                .body1
+                .copyWith(fontWeight: FontWeight.w700)
+                .copyWith(color: color));
   }
 }
