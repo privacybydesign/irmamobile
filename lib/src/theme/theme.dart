25399998faee07e1e864eb6a67e079a170a45734
Hanna <hanna@science.ru.nl>
2019-09-26 12:23:42 +0200

    updated according to Claudia's new specification

--- lib/src/theme/theme.dart
+++ lib/src/theme/theme.dart
@@ -21,8 +21,10 @@ class IrmaTheme {
   static final supGreen = const Color(0xFF2BC194);
 
   // Support colors (for alerts and feedback on form elements)
-  static final supValid = const Color(0xFFD44454);
+  static final supValid = const Color(0xFF079268);
   static final supInvalid = const Color(0xFFD44454);
+  static final supAlert = const Color(0xFFFFBB58);
+  static final supInformation = const Color(0xFF004C92);
 
   // Link colors
   static final linkColor = const Color(0xFF004C92);
@@ -30,6 +32,14 @@ class IrmaTheme {
 
   // Overlay color
   static final overlay50 = const Color(0xFF3C4B5A);
+
+  // Additional text styles
+  static final issuerNameTextStyle = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Montserrat',
+    color: IrmaTheme.gsclWhite,
+  );
 }
 
 ThemeData irmaTheme = ThemeData(
@@ -39,74 +49,94 @@ ThemeData irmaTheme = ThemeData(
   scaffoldBackgroundColor: IrmaTheme.primaryLight,
   fontFamily: 'Karla',
   textTheme: TextTheme(
-          // display4 is used for extremely large text
-          display4: TextStyle(
-            fontSize: 40.0,
-            fontWeight: FontWeight.w300,
-          ),
-          // display3 is used for very, very large text
-          display3: TextStyle(
-            fontSize: 34.0,
-            fontWeight: FontWeight.w400,
-          ),
-          // display2 is used for very large text
-          display2: TextStyle(
-            fontSize: 30.0,
-            fontWeight: FontWeight.w400,
-          ),
-          // display1 is used for large text
-          display1: TextStyle(
-            fontSize: 26.0,
-            fontWeight: FontWeight.w400,
-          ),
-          // headline is used for large text in dialogs
-          headline: TextStyle(
-            fontSize: 24.0,
-            fontWeight: FontWeight.w400,
-            color: IrmaTheme.primaryDark,
-          ),
-          // title is used for the primary text in app bars and dialogs
-          title: TextStyle(
-            fontSize: 22.0,
-            fontWeight: FontWeight.w500,
-          ),
-          // subhead is used for the primary text in lists
-          subhead: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.w300,
-            fontFamily: 'Montserrat',
-          ),
-          // body1 is the default text style
-          body1: TextStyle(
-            fontSize: 18.0,
-            fontWeight: FontWeight.w300,
-            fontFamily: 'Montserrat',
-            color: IrmaTheme.primaryDark,
-          ),
-          // body2 is used for emphasizing text that would otherwise be body1
-          body2: TextStyle(
-            fontSize: 18.0,
-            fontWeight: FontWeight.w500,
-            fontFamily: 'Montserrat',
-            color: IrmaTheme.primaryDark,
-          ),
-          // caption is used for auxiliary text associated with images
-          caption: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w300, fontFamily: 'Montserrat'),
-          // button is used for text on RaisedButton and FlatButton
-          button: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w600, fontFamily: 'Montserrat'),
-          // subtitle is used for medium emphasis text that's a little smaller than subhead.
-          subtitle: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w400, fontFamily: 'Montserrat'),
-          // is used for the smallest text
-          overline: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w300, fontFamily: 'Montserrat'))
-      .apply(
-    bodyColor: IrmaTheme.primaryDark,
+    // display4 is used for extremely large text
+    display4: TextStyle(
+      fontSize: 44.0,
+      fontWeight: FontWeight.w700,
+      color: IrmaTheme.gscl40,
+    ),
+    // display3 is used for very, very large text
+    display3: TextStyle(
+      fontSize: 38.0,
+      fontWeight: FontWeight.w700,
+      color: IrmaTheme.gscl40,
+    ),
+    // display2 is used for very large text
+    display2: TextStyle(
+      fontSize: 32.0,
+      fontWeight: FontWeight.w700,
+      color: IrmaTheme.gscl40,
+    ),
+    // display1 is used for large text
+    display1: TextStyle(
+      fontSize: 28.0,
+      fontWeight: FontWeight.w700,
+      color: IrmaTheme.gscl40,
+    ),
+    // headline is used for large text in dialogs
+    headline: TextStyle(
+      fontSize: 18.0,
+      fontWeight: FontWeight.w700,
+      color: IrmaTheme.gscl40,
+    ),
+    // title is used for the primary text in app bars and dialogs
+    title: TextStyle(
+      fontSize: 24.0,
+      fontWeight: FontWeight.w700,
+      color: IrmaTheme.gscl40,
+    ),
+    // subhead is used for the primary text in lists
+    subhead: TextStyle(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w300,
+      fontFamily: 'Montserrat',
+      color: IrmaTheme.gscl40,
+    ),
+    // body1 is the default text style
+    body1: TextStyle(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w300,
+      fontFamily: 'Montserrat',
+      color: IrmaTheme.primaryDark,
+    ),
+    // body2 is used for emphasizing text that would otherwise be body1
+    body2: TextStyle(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w500,
+      fontFamily: 'Montserrat',
+      color: IrmaTheme.primaryDark,
+    ),
+    // caption is used for auxiliary text associated with images
+    caption: TextStyle(
+      fontSize: 14.0,
+      fontWeight: FontWeight.w400,
+      fontFamily: 'Montserrat',
+      color: IrmaTheme.primaryDark,
+    ),
+    // button is used for text on RaisedButton and FlatButton
+    button: TextStyle(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w700,
+      fontFamily: 'Montserrat',
+      color: IrmaTheme.gsclWhite,
+    ),
+    // subtitle is used for medium emphasis text that's a little smaller than subhead.
+    subtitle: TextStyle(
+      fontSize: 14.0,
+      fontWeight: FontWeight.w400,
+      fontFamily: 'Montserrat',
+      color: IrmaTheme.gscl40,
+    ),
+    // is used for the smallest text
+    overline: TextStyle(
+      fontSize: 12.0,
+      fontWeight: FontWeight.w600,
+      fontFamily: 'Montserrat',
+      color: IrmaTheme.gscl60,
+    ),
   ),
   appBarTheme: AppBarTheme(
     elevation: 0,
-    textTheme: TextTheme(
-      title: TextStyle(
-          color: IrmaTheme.primaryDark, fontSize: 20.0), // Same as title above // TODO find more elegant solution
-    ),
     brightness: Brightness.light,
     color: IrmaTheme.primaryLight,
     iconTheme: IconThemeData(
