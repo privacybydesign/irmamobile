033275f66e2896168538300a3bedc954898f0b54
Ruben Nijveld <ruben@tweedegolf.com>
2019-10-10 12:16:31 +0200

    Routing changes for enrollment and sub-navigator navigation
    Fix routing when using a combination of cancel button and hardware back button
    
    Fixed formatting

--- lib/src/screens/enrollment/widgets/cancel_button.dart
+++ lib/src/screens/enrollment/widgets/cancel_button.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:irmamobile/src/screens/enrollment/models/enrollment_bloc.dart';
 import 'package:irmamobile/src/screens/enrollment/models/enrollment_event.dart';
+import 'package:irmamobile/src/screens/enrollment/widgets/welcome.dart';
 
 class CancelButton extends StatelessWidget {
   final String routeName;
@@ -18,7 +19,8 @@ class CancelButton extends StatelessWidget {
       icon: const Icon(Icons.arrow_back),
       onPressed: () {
         enrollmentBloc.dispatch(EnrollmentCanceled());
-        Navigator.of(context).pushNamed(routeName);
+        Navigator.of(context)
+            .popUntil((route) => route.settings.name == routeName || route.settings.name == Welcome.routeName);
       },
       tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
     );
--- lib/src/screens/enrollment/widgets/choose_pin.dart
+++ lib/src/screens/enrollment/widgets/choose_pin.dart
@@ -43,7 +43,7 @@ class ChoosePin extends StatelessWidget {
                       maxLength: 5,
                       onSubmit: (String pin) {
                         enrollmentBloc.dispatch(PinChosen(pin: pin));
-                        Navigator.of(context).pushReplacementNamed(ConfirmPin.routeName);
+                        Navigator.of(context).pushNamed(ConfirmPin.routeName);
                       })
                 ])),
           );
