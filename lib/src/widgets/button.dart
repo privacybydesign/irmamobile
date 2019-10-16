94e242911d69fb1357cf424743337ec443792d21
Edwin Martin <edwin@bitstorm.org>
2019-10-16 14:00:24 +0200

    chore: Update button to not use external animation
    Co-authored-by: Confiks <confiks@scriptbase.org>

--- lib/src/widgets/button.dart
+++ lib/src/widgets/button.dart
@@ -1,59 +1,47 @@
-import 'dart:async';
-
-import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class Button extends StatefulWidget {
-  final Animation<double> animation;
   final String svgFile;
   final String accessibleName;
   final Function() onPressed;
 
-  Button({this.animation, this.svgFile, this.accessibleName, this.onPressed});
+  Button({this.svgFile, this.accessibleName, this.onPressed});
 
   @override
   _ButtonState createState() => _ButtonState();
 }
 
 class _ButtonState extends State<Button> with SingleTickerProviderStateMixin {
-  AnimationController controller;
-
-  final _opacityTween = Tween<double>(begin: 0, end: 1);
-  final _padding = 15.0;
+  static const _padding = 15.0;
 
   bool _isBeingPressed = false;
-  Timer _timer;
 
   @override
-  Widget build(BuildContext context) => AnimatedBuilder(
-      animation: widget.animation,
-      builder: (buildContext, child) {
-        return Semantics(
-          button: true,
-          label: FlutterI18n.translate(context, widget.accessibleName),
-          child: Opacity(
-            opacity: _opacityTween.evaluate(widget.animation),
-            child: IconButton(
-              icon: SvgPicture.asset(widget.svgFile, color: _isBeingPressed ? Colors.grey[700] : Colors.white),
-              padding: EdgeInsets.only(right: _padding),
-              onPressed: () {
-                widget.onPressed();
+  Widget build(BuildContext context) {
+    return Semantics(
+      button: true,
+      label: FlutterI18n.translate(context, widget.accessibleName),
+      child: GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: widget.onPressed,
+          child: Listener(
+              behavior: HitTestBehavior.opaque,
+              onPointerDown: (tapDownDetails) {
                 setState(() {
                   _isBeingPressed = true;
                 });
-                if (_timer != null) {
-                  _timer.cancel();
-                }
-                _timer = Timer(Duration(milliseconds: 200), () {
-                  setState(() {
-                    _isBeingPressed = false;
-                  });
+              },
+              onPointerUp: (tapUpDetails) {
+                setState(() {
+                  _isBeingPressed = false;
                 });
               },
-            ),
-          ),
-        );
-      });
+              child: Padding(
+                padding: EdgeInsets.only(right: _padding),
+                child: SvgPicture.asset(widget.svgFile, color: _isBeingPressed ? Colors.grey[700] : Colors.white),
+              ))),
+    );
+  }
 }
