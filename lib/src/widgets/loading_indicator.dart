e9cf9e2c28eabedd67df03ad76fb257bebe5d0f6
Edwin Martin <edwin@bitstorm.org>
2019-12-03 11:14:12 +0100

    Use StatefulWidget

--- lib/src/widgets/loading_indicator.dart
+++ lib/src/widgets/loading_indicator.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 
-class LoadingIndicator extends StatefulWidget {
-  @override
-  _LoadingIndicatorState createState() => _LoadingIndicatorState();
-}
-
-class _LoadingIndicatorState extends State<LoadingIndicator> {
+class LoadingIndicator extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Image.asset(
         "assets/generic/loading_indicator.webp",
