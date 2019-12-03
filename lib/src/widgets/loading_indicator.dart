1d903de2163e540957168dbdbc1d7e32d6ea3281
Edwin Martin <edwin@bitstorm.org>
2019-12-03 10:28:50 +0100

    Use webp

--- lib/src/widgets/loading_indicator.dart
+++ lib/src/widgets/loading_indicator.dart
@@ -1,35 +1,15 @@
 import 'package:flutter/material.dart';
 
-import 'package:irmamobile/src/util/gif_ani.dart';
-
 class LoadingIndicator extends StatefulWidget {
   @override
   _LoadingIndicatorState createState() => _LoadingIndicatorState();
 }
 
-class _LoadingIndicatorState extends State<LoadingIndicator> with SingleTickerProviderStateMixin {
-  GifController _gifCtrl;
-
+class _LoadingIndicatorState extends State<LoadingIndicator> {
   @override
-  void initState() {
-    _gifCtrl = GifController(vsync: this, duration: Duration(milliseconds: 2000), frameCount: 50);
-    _gifCtrl.repeat();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _gifCtrl.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GifAnimation(
-      image: const AssetImage("assets/generic/loading_indicator.gif"),
-      controller: _gifCtrl,
-      width: 120,
-      height: 120,
-    );
-  }
+  Widget build(BuildContext context) => Image.asset(
+        "assets/generic/loading_indicator.webp",
+        width: 120,
+        height: 120,
+      );
 }
