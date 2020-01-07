50e05a8a589d82c82e27288ada349577f06632d7
Hanna <git@creativecode.org>
2020-01-07 19:53:35 +0100

    clean up

--- lib/src/screens/help/help_items.dart
+++ lib/src/screens/help/help_items.dart
@@ -3,24 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:irmamobile/src/models/irma_configuration.dart';
-import 'package:irmamobile/src/screens/about/widgets/links.dart';
-import 'package:irmamobile/src/theme/irma_icons.dart';
 import 'package:irmamobile/src/theme/theme.dart';
 import 'package:irmamobile/src/widgets/collapsible.dart';
-import 'package:url_launcher/url_launcher.dart';
-
-const String _markdownData = """
-# Markdown Example
-
-
-Lists now work well:
-- One
-- Two
-- Three
-
-And we can finally mix **bold** and non-bold and *italics*.
-
-""";
 
 class HelpItems extends StatefulWidget {
   const HelpItems({this.credentialType, this.parentKey, this.parentScrollController});
@@ -69,129 +53,31 @@ class _HelpItemsState extends State<HelpItems> with TickerProviderStateMixin {
             ),
             key: _collapsableKeys[0]),
         Collapsible(
-            header: FlutterI18n.translate(context, 'about.why_irma_item'),
-            onExpansionChanged: (v) => {if (v) Future.delayed(expandDuration, () => _jumpToCollapsable(1))},
-            content: WhyIrma(),
+            header: FlutterI18n.translate(context, 'about.who_is_behind_irma_item'),
+            onExpansionChanged: (v) => {if (v) Future.delayed(expandDuration, () => _jumpToCollapsable(0))},
+            content: Container(
+              child: MarkdownBody(
+                selectable: false,
+                data: FlutterI18n.translate(context, 'help.how_item'),
+                styleSheet: MarkdownStyleSheet(
+                  strong: IrmaTheme.of(context).textTheme.body2,
+                ),
+              ),
+            ),
             key: _collapsableKeys[1]),
         Collapsible(
-            header: FlutterI18n.translate(context, 'about.privacy_item'),
-            onExpansionChanged: (v) => {if (v) Future.delayed(expandDuration, () => _jumpToCollapsable(2))},
-            content: PrivacyAndSecurity(),
-            key: _collapsableKeys[2]),
-      ],
-    );
-  }
-}
-
-// TODO: The order of the bold and non-bold elements might differ in another language!
-class WhyIrma extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        RichText(
-          text: TextSpan(
-              text: FlutterI18n.translate(context, 'about.why_irma_item_1'),
-              style: IrmaTheme.of(context).boldBody,
-              children: <TextSpan>[
-                TextSpan(text: "\nLLLLlllll", style: IrmaTheme.of(context).textTheme.body1),
-              ]),
-        ),
-        MarkdownBody(
-          selectable: false,
-          data: FlutterI18n.translate(context, 'help.how_item'),
-          imageDirectory: 'https://raw.githubusercontent.com',
-        ),
-        RichText(
-          text: TextSpan(
-              text: FlutterI18n.translate(context, 'about.why_irma_item_3'),
-              style: IrmaTheme.of(context).textTheme.body1,
-              children: <TextSpan>[
-                TextSpan(
-                  text: FlutterI18n.translate(context, 'about.avg'),
-                  style: IrmaTheme.of(context).hyperlinkTextStyle,
-                  recognizer: TapGestureRecognizer()
-                    ..onTap = () {
-                      launch(FlutterI18n.translate(context, 'about.avg_link'));
-                    },
+            header: FlutterI18n.translate(context, 'about.who_is_behind_irma_item'),
+            onExpansionChanged: (v) => {if (v) Future.delayed(expandDuration, () => _jumpToCollapsable(0))},
+            content: Container(
+              child: MarkdownBody(
+                selectable: false,
+                data: FlutterI18n.translate(context, 'help.how_item'),
+                styleSheet: MarkdownStyleSheet(
+                  strong: IrmaTheme.of(context).textTheme.body2,
                 ),
-                TextSpan(
-                    text: FlutterI18n.translate(context, 'about.why_irma_item_4'),
-                    style: IrmaTheme.of(context).textTheme.body1),
-              ]),
-        ),
-        SizedBox(height: IrmaTheme.of(context).defaultSpacing),
-        RichText(
-          text: TextSpan(
-              text: FlutterI18n.translate(context, 'about.why_irma_item_5'),
-              style: IrmaTheme.of(context).boldBody,
-              children: <TextSpan>[
-                TextSpan(
-                    text: FlutterI18n.translate(context, 'about.why_irma_item_6'),
-                    style: IrmaTheme.of(context).textTheme.body1),
-              ]),
-        ),
-        SizedBox(height: IrmaTheme.of(context).smallSpacing),
-      ],
-    );
-  }
-}
-
-// TODO: The order of the bold and non-bold elements might differ in another language!
-class PrivacyAndSecurity extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Text(FlutterI18n.translate(context, 'about.privacy_explanation_1'),
-            style: IrmaTheme.of(context).textTheme.body1),
-        SizedBox(height: IrmaTheme.of(context).defaultSpacing),
-        RichText(
-          text: TextSpan(
-              text: FlutterI18n.translate(context, 'about.privacy_explanation_2'),
-              style: IrmaTheme.of(context).textTheme.body1,
-              children: <TextSpan>[
-                TextSpan(
-                  text: FlutterI18n.translate(context, 'about.privacy_explanation_3'),
-                  style: IrmaTheme.of(context).boldBody,
-                ),
-                TextSpan(
-                  text: FlutterI18n.translate(context, 'about.privacy_explanation_4'),
-                  style: IrmaTheme.of(context).textTheme.body1,
-                ),
-              ]),
-        ),
-        SizedBox(height: IrmaTheme.of(context).smallSpacing),
-        RichText(
-          text: TextSpan(
-              text: FlutterI18n.translate(context, 'about.privacy_explanation_5'),
-              style: IrmaTheme.of(context).textTheme.body1,
-              children: <TextSpan>[
-                TextSpan(
-                    text: FlutterI18n.translate(context, 'about.privacy_explanation_6'),
-                    style: IrmaTheme.of(context).boldBody),
-              ]),
-        ),
-        SizedBox(height: IrmaTheme.of(context).defaultSpacing),
-        Text(FlutterI18n.translate(context, 'about.privacy_explanation_7'),
-            style: IrmaTheme.of(context).textTheme.body1),
-        SizedBox(height: IrmaTheme.of(context).smallSpacing),
-        SizedBox(
-          width: double.infinity,
-          child: Container(
-            child: Text(
-              FlutterI18n.translate(context, 'about.check_out'),
-              textAlign: TextAlign.left,
+              ),
             ),
-          ),
-        ),
-        SizedBox(height: IrmaTheme.of(context).smallSpacing),
-        const ExternalLink(
-          "about.privacy_policy_link",
-          "about.privacy_policy",
-          Icon(IrmaIcons.view, size: 16.0),
-        ),
-        SizedBox(height: IrmaTheme.of(context).defaultSpacing),
+            key: _collapsableKeys[2]),
       ],
     );
   }
