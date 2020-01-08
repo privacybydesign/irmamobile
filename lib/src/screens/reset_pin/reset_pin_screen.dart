604d3329a57abb45b2a4cfb3d626be5f9069ae8c
Hanna <git@creativecode.org>
2020-01-08 16:39:36 +0100

    improving reset screen

--- lib/src/screens/reset_pin/reset_pin_screen.dart
+++ lib/src/screens/reset_pin/reset_pin_screen.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:irmamobile/src/data/irma_repository.dart';
 import 'package:irmamobile/src/screens/reset_pin/widgets/cancel_button.dart';
-import 'package:irmamobile/src/theme/irma_icons.dart';
 import 'package:irmamobile/src/theme/theme.dart';
 import 'package:irmamobile/src/widgets/irma_button.dart';
 import 'package:irmamobile/src/widgets/irma_text_button.dart';
 
 class ResetPinScreen extends StatelessWidget {
+  static const String routeName = '/reset';
+
   void cancel(BuildContext context) {
     Navigator.of(context).pop();
   }
@@ -22,61 +24,78 @@ class ResetPinScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         leading: CancelButton(cancel: cancel),
+        centerTitle: true,
         title: Text(FlutterI18n.translate(context, 'reset_pin.title')),
       ),
-      body: SingleChildScrollView(
-          padding: EdgeInsets.only(top: IrmaTheme.of(context).spacing * 2),
-          child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-            Center(child: const Icon(IrmaIcons.lock, size: 180)),
-            SizedBox(height: IrmaTheme.of(context).spacing),
-            Container(
-                constraints: BoxConstraints(maxWidth: IrmaTheme.of(context).spacing * 16),
-                child: Text(
-                  FlutterI18n.translate(context, 'reset_pin.message'),
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.body2,
-                )),
-            SizedBox(height: IrmaTheme.of(context).spacing),
-            Container(
-                constraints: BoxConstraints(maxWidth: IrmaTheme.of(context).spacing * 16),
-                child: Text(
-                  FlutterI18n.translate(context, 'reset_pin.existing_data_title'),
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.headline,
-                )),
-            SizedBox(height: IrmaTheme.of(context).spacing),
-            Container(
-                constraints: BoxConstraints(maxWidth: IrmaTheme.of(context).spacing * 16),
-                child: Text(
-                  FlutterI18n.translate(context, 'reset_pin.existing_data_message'),
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.body2,
-                )),
-            SizedBox(height: IrmaTheme.of(context).spacing),
-            Wrap(
-              runSpacing: IrmaTheme.of(context).spacing,
-              spacing: IrmaTheme.of(context).spacing,
-              children: <Widget>[
-                IrmaTextButton(
-                  onPressed: () => cancel(context),
-                  label: 'reset_pin.back',
-                ),
-                Column(
-                  children: <Widget>[
-                    IrmaButton(
-                      onPressed: () => confirm(context),
-                      label: 'reset_pin.reset',
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: SingleChildScrollView(
+                // padding: EdgeInsets.all(IrmaTheme.of(context).defaultSpacing),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                  SizedBox(height: IrmaTheme.of(context).defaultSpacing),
+                  Center(
+                    child: SizedBox(
+                      width: 94,
+                      height: 113,
+                      child: SvgPicture.asset(
+                        'assets/reset/prullenbak.svg',
+                      ),
                     ),
-                    Text(
-                      FlutterI18n.translate(context, 'reset_pin.existing_data_removed'),
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.body1,
-                    )
-                  ],
-                ),
-              ],
-            )
-          ])),
+                  ),
+                  SizedBox(height: IrmaTheme.of(context).largeSpacing),
+                  Text(
+                    FlutterI18n.translate(context, 'reset_pin.message'),
+                  ),
+                  SizedBox(height: IrmaTheme.of(context).defaultSpacing),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Container(
+                      child: Text(
+                        FlutterI18n.translate(context, 'reset_pin.existing_data_title'),
+                        style: Theme.of(context).textTheme.body2,
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: IrmaTheme.of(context).smallSpacing),
+                  Text(
+                    FlutterI18n.translate(context, 'reset_pin.existing_data_message'),
+                  ),
+                  SizedBox(height: IrmaTheme.of(context).defaultSpacing),
+                  Align(
+                    alignment: FractionalOffset.bottomCenter,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      padding: EdgeInsets.all(IrmaTheme.of(context).defaultSpacing),
+                      color: IrmaTheme.of(context).backgroundBlue,
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: IrmaTextButton(
+                              onPressed: () {},
+                              minWidth: 0.0,
+                              label: 'Bla',
+                            ),
+                          ),
+                          SizedBox(width: IrmaTheme.of(context).defaultSpacing),
+                          Expanded(
+                            child: IrmaButton(
+                              minWidth: 0.0,
+                              label: 'Henk',
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ), // Wrap(
+                ])),
+          ),
+        ],
+      ),
     );
   }
 }
