738b4b6c96df637ceab4a7c7804eeed3394ffe46
Hanna <git@creativecode.org>
2020-01-09 13:14:04 +0100

    maybe

--- lib/src/screens/pin/pin_screen.dart
+++ lib/src/screens/pin/pin_screen.dart
@@ -6,6 +6,10 @@ import 'package:irmamobile/src/screens/pin/bloc/pin_bloc.dart';
 import 'package:irmamobile/src/screens/pin/bloc/pin_event.dart';
 import 'package:irmamobile/src/screens/pin/bloc/pin_state.dart';
 import 'package:irmamobile/src/theme/theme.dart';
+import 'package:irmamobile/src/widgets/irma_button.dart';
+import 'package:irmamobile/src/widgets/irma_dialog.dart';
+import 'package:irmamobile/src/widgets/irma_text_button.dart';
+import 'package:irmamobile/src/widgets/irma_themed_button.dart';
 import 'package:irmamobile/src/widgets/pin_field.dart';
 
 class PinScreen extends StatefulWidget {
@@ -36,91 +40,93 @@ class _PinScreenState extends State<PinScreen> {
         }
 
         return Scaffold(
+            backgroundColor: IrmaTheme.of(context).backgroundBlue,
             body: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: IrmaTheme.of(context).defaultSpacing,
-                ),
-                SvgPicture.asset('assets/non-free/irma_logo.svg'),
-                SizedBox(
-                  height: IrmaTheme.of(context).largeSpacing,
-                ),
-                Text(
-                  FlutterI18n.translate(context, "pin.title"),
-                  style: IrmaTheme.of(context).textTheme.display1,
-                ),
-                SizedBox(
-                  height: IrmaTheme.of(context).defaultSpacing,
-                ),
-                Text(FlutterI18n.translate(context, "pin.subtitle")),
-                SizedBox(
-                  height: IrmaTheme.of(context).defaultSpacing,
-                ),
-                PinField(
-                  longPin: false,
-                  onSubmit: (pin) {
-                    FocusScope.of(context).requestFocus();
-                    _pinBloc.dispatch(
-                      Unlock(pin),
-                    );
-                  },
-                ),
-                SizedBox(
-                  height: IrmaTheme.of(context).largeSpacing,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    // TODO navigate to screen
-                  },
-                  child: Text(
-                    FlutterI18n.translate(context, "pin.button_forgot"),
-                    style: IrmaTheme.of(context).textTheme.body1.copyWith(
-                          decoration: TextDecoration.underline,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: IrmaTheme.of(context).defaultSpacing,
+                    ),
+                    Text(
+                      FlutterI18n.translate(context, "pin.title"),
+                      style: IrmaTheme.of(context).textTheme.display1,
+                    ),
+                    SizedBox(
+                      height: IrmaTheme.of(context).largeSpacing,
+                    ),
+                    SizedBox(width: 76.0, child: SvgPicture.asset('assets/non-free/irma_logo.svg')),
+                    SizedBox(
+                      height: IrmaTheme.of(context).largeSpacing,
+                    ),
+                    Text(FlutterI18n.translate(context, "pin.subtitle")),
+                    SizedBox(
+                      height: IrmaTheme.of(context).defaultSpacing,
+                    ),
+                    PinField(
+                      longPin: false,
+                      onSubmit: (pin) {
+                        FocusScope.of(context).requestFocus();
+                        _pinBloc.dispatch(
+                          Unlock(pin),
+                        );
+                      },
+                    ),
+                    SizedBox(
+                      height: IrmaTheme.of(context).defaultSpacing,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        debugPrint("on forgot pin press");
+                      },
+                      child: Text(
+                        FlutterI18n.translate(context, "pin.button_forgot"),
+                        style: IrmaTheme.of(context).hyperlinkTextStyle.copyWith(
+                              decoration: TextDecoration.underline,
+                            ),
+                      ),
+                    ),
+                    if (state.unlockInProgress)
+                      Padding(
+                          padding: EdgeInsets.all(IrmaTheme.of(context).defaultSpacing),
+                          child: const CircularProgressIndicator()),
+                    if (state.pinInvalid)
+                      IrmaDialog(
+                        height: 250,
+                        title: 'settings.advanced.delete_title',
+                        content: 'settings.advanced.delete_content',
+                        child: Wrap(
+                          direction: Axis.horizontal,
+                          verticalDirection: VerticalDirection.up,
+                          alignment: WrapAlignment.spaceEvenly,
+                          children: <Widget>[
+                            IrmaTextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              minWidth: 0.0,
+                              label: 'settings.advanced.delete_deny',
+                            ),
+                            IrmaButton(
+                              size: IrmaButtonSize.small,
+                              minWidth: 0.0,
+                              onPressed: () {},
+                              label: 'settings.advanced.delete_confirm',
+                            ),
+                          ],
                         ),
-                  ),
+                      ),
+                    if (state.errorMessage != null)
+                      Text(
+                        FlutterI18n.translate(context, state.errorMessage),
+                        style: IrmaTheme.of(context).textTheme.body1.copyWith(
+                              color: Colors.red,
+                            ),
+                      )
+                  ],
                 ),
-                SizedBox(
-                  height: IrmaTheme.of(context).defaultSpacing,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    debugPrint("on block press");
-                  },
-                  child: Text(
-                    FlutterI18n.translate(context, "pin.button_block"),
-                    style: IrmaTheme.of(context).textTheme.body1.copyWith(
-                          decoration: TextDecoration.underline,
-                        ),
-                  ),
-                ),
-                SizedBox(
-                  height: IrmaTheme.of(context).defaultSpacing,
-                ),
-                if (state.unlockInProgress)
-                  Padding(
-                      padding: EdgeInsets.all(IrmaTheme.of(context).defaultSpacing),
-                      child: const CircularProgressIndicator()),
-                if (state.pinInvalid)
-                  Text(
-                    FlutterI18n.plural(context, "pin.invalid_pin.attempts", state.remainingAttempts),
-                    style: IrmaTheme.of(context).textTheme.body1.copyWith(
-                          color: Colors.red,
-                        ),
-                    textAlign: TextAlign.center,
-                  ),
-                if (state.errorMessage != null)
-                  Text(
-                    FlutterI18n.translate(context, state.errorMessage),
-                    style: IrmaTheme.of(context).textTheme.body1.copyWith(
-                          color: Colors.red,
-                        ),
-                  )
-              ],
-            ),
-          ),
-        ));
+              ),
+            ));
       },
     );
   }
