a91b7fa7f4b54f15523979f9795c462705c38151
Hanna Schraffenberger <hanna@science.ru.nl>
2020-01-17 16:06:42 +0100

    fixed problems on real motorola phone where drawer goes into menu bar...

--- lib/src/screens/wallet/widgets/wallet_drawer.dart
+++ lib/src/screens/wallet/widgets/wallet_drawer.dart
@@ -13,105 +13,107 @@ import 'package:irmamobile/src/theme/theme.dart';
 class WalletDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      // Add a ListView to the drawer. This ensures the user can scroll
-      // through the options in the drawer if there isn't enough vertical
-      // space to fit everything.
-      child: Container(
-        color: IrmaTheme.of(context).primaryLight,
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: ListView(
-                // Important: Remove any padding from the ListView.
-                padding: EdgeInsets.zero,
-                children: <Widget>[
-                  Container(
-                    height: 90.0,
-                    child: DrawerHeader(
-                      decoration: BoxDecoration(
-                        border: Border.all(width: 0.0, color: Colors.white),
-                        color: IrmaTheme.of(context).grayscale85,
-                      ),
-                      padding: EdgeInsets.only(
-                        left: IrmaTheme.of(context).mediumSpacing,
-                        right: IrmaTheme.of(context).defaultSpacing,
-                      ),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          SizedBox.fromSize(
-                            size: const Size(40.0, 40.0),
-                            child: SvgPicture.asset(
-                              'assets/non-free/irma_logo.svg',
-                              fit: BoxFit.scaleDown,
-                              alignment: Alignment.topLeft,
+    return SafeArea(
+      child: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: Container(
+          color: IrmaTheme.of(context).primaryLight,
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: ListView(
+                  // Important: Remove any padding from the ListView.
+                  padding: EdgeInsets.zero,
+                  children: <Widget>[
+                    Container(
+                      height: Size.fromHeight(kToolbarHeight).height,
+                      child: DrawerHeader(
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 0.0, color: Colors.white),
+                          color: IrmaTheme.of(context).grayscale85,
+                        ),
+                        padding: EdgeInsets.only(
+                          left: IrmaTheme.of(context).mediumSpacing,
+                          right: IrmaTheme.of(context).defaultSpacing,
+                        ),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            SizedBox.fromSize(
+                              size: const Size(50.0, 32.0),
+                              child: SvgPicture.asset(
+                                'assets/non-free/irma_logo.svg',
+                                fit: BoxFit.scaleDown,
+                                alignment: Alignment.topLeft,
+                              ),
                             ),
-                          ),
-                          IconButton(
-                            iconSize: 18.0,
-                            icon: Icon(IrmaIcons.close),
-                            onPressed: () => Navigator.pop(context),
-                          )
-                        ],
+                            IconButton(
+                              iconSize: 18.0,
+                              icon: Icon(IrmaIcons.close),
+                              onPressed: () => Navigator.pop(context),
+                            )
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  _createDrawerItem(context,
-                      icon: IrmaIcons.add, text: FlutterI18n.translate(context, 'drawer.add_cards'), onTap: () {
-                    Navigator.of(context).pop();
-                    Navigator.of(context).pushNamed(CardStoreScreen.routeName);
-                  }),
-                  _createDrawerItem(context,
-                      icon: IrmaIcons.time, text: FlutterI18n.translate(context, 'drawer.history'), onTap: () {
-                    Navigator.of(context).pop();
-                    Navigator.of(context).pushNamed(HistoryScreen.routeName);
-                  }),
-                  _createDrawerItem(context,
-                      icon: IrmaIcons.settings, text: FlutterI18n.translate(context, 'drawer.settings'), onTap: () {
-                    Navigator.of(context).pop();
-                    Navigator.of(context).pushNamed(SettingsScreen.routeName);
-                  }),
-                  _createDrawerItem(
-                    context,
-                    icon: IrmaIcons.question,
-                    text: FlutterI18n.translate(context, 'drawer.help'),
-                    onTap: () => Navigator.pushNamed(context, HelpScreen.routeName),
-                  ),
-                  _createDrawerItem(context, icon: IrmaIcons.info, text: FlutterI18n.translate(context, 'drawer.about'),
-                      onTap: () {
-                    Navigator.of(context).pop();
-                    Navigator.of(context).pushNamed(AboutScreen.routeName);
-                  }),
-                ],
-              ),
-            ),
-            // This container holds the align
-            Container(
-              // This align moves the children to the bottom
-              child: Align(
-                alignment: FractionalOffset.bottomCenter,
-                // This container holds all the children that will be aligned
-                // on the bottom and should not scroll with the above ListView
-                child: Container(
-                  color: IrmaTheme.of(context).primaryBlue,
-                  child: ListTile(
-                    contentPadding: EdgeInsets.only(left: IrmaTheme.of(context).mediumSpacing),
-                    title: Text(
-                      FlutterI18n.translate(context, 'drawer.lock_wallet'),
-                      style: TextStyle(fontWeight: FontWeight.w500, color: Colors.white),
-                    ),
-                    leading: Icon(IrmaIcons.lock, color: Colors.white),
-                    onTap: () {
+                    _createDrawerItem(context,
+                        icon: IrmaIcons.add, text: FlutterI18n.translate(context, 'drawer.add_cards'), onTap: () {
                       Navigator.of(context).pop();
-                      IrmaRepository.get().lock();
-                    },
+                      Navigator.of(context).pushNamed(CardStoreScreen.routeName);
+                    }),
+                    _createDrawerItem(context,
+                        icon: IrmaIcons.time, text: FlutterI18n.translate(context, 'drawer.history'), onTap: () {
+                      Navigator.of(context).pop();
+                      Navigator.of(context).pushNamed(HistoryScreen.routeName);
+                    }),
+                    _createDrawerItem(context,
+                        icon: IrmaIcons.settings, text: FlutterI18n.translate(context, 'drawer.settings'), onTap: () {
+                      Navigator.of(context).pop();
+                      Navigator.of(context).pushNamed(SettingsScreen.routeName);
+                    }),
+                    _createDrawerItem(
+                      context,
+                      icon: IrmaIcons.question,
+                      text: FlutterI18n.translate(context, 'drawer.help'),
+                      onTap: () => Navigator.pushNamed(context, HelpScreen.routeName),
+                    ),
+                    _createDrawerItem(context,
+                        icon: IrmaIcons.info, text: FlutterI18n.translate(context, 'drawer.about'), onTap: () {
+                      Navigator.of(context).pop();
+                      Navigator.of(context).pushNamed(AboutScreen.routeName);
+                    }),
+                  ],
+                ),
+              ),
+              // This container holds the align
+              Container(
+                // This align moves the children to the bottom
+                child: Align(
+                  alignment: FractionalOffset.bottomCenter,
+                  // This container holds all the children that will be aligned
+                  // on the bottom and should not scroll with the above ListView
+                  child: Container(
+                    color: IrmaTheme.of(context).primaryBlue,
+                    child: ListTile(
+                      contentPadding: EdgeInsets.only(left: IrmaTheme.of(context).mediumSpacing),
+                      title: Text(
+                        FlutterI18n.translate(context, 'drawer.lock_wallet'),
+                        style: TextStyle(fontWeight: FontWeight.w500, color: Colors.white),
+                      ),
+                      leading: Icon(IrmaIcons.lock, color: Colors.white),
+                      onTap: () {
+                        Navigator.of(context).pop();
+                        IrmaRepository.get().lock();
+                      },
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
