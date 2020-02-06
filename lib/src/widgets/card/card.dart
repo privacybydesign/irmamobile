a00301a8daf00c10fefb5e3bcc79656ceb1c8f04
Edwin Martin <edwin@bitstorm.org>
2020-02-06 13:32:21 +0100

    Formatting

--- lib/src/widgets/card/card.dart
+++ lib/src/widgets/card/card.dart
@@ -60,81 +60,81 @@ class _IrmaCardState extends State<IrmaCard> with SingleTickerProviderStateMixin
         });
       },
       child: stackedCard(
-        card: Container(
-          margin: EdgeInsets.symmetric(horizontal: IrmaTheme.of(context).smallSpacing),
-          decoration: BoxDecoration(
-            color: irmaCardTheme.bgColorDark,
-            gradient: LinearGradient(
-              colors: [
-                irmaCardTheme.bgColorDark,
-                irmaCardTheme.bgColorLight,
+          card: Container(
+            margin: EdgeInsets.symmetric(horizontal: IrmaTheme.of(context).smallSpacing),
+            decoration: BoxDecoration(
+              color: irmaCardTheme.bgColorDark,
+              gradient: LinearGradient(
+                colors: [
+                  irmaCardTheme.bgColorDark,
+                  irmaCardTheme.bgColorLight,
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+              border: Border.all(
+                width: 1.0,
+                color: irmaCardTheme.bgColorLight,
+              ),
+              borderRadius: BorderRadius.all(
+                _borderRadius,
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: _transparentBlack,
+                  blurRadius: _blurRadius,
+                  offset: const Offset(
+                    0.0,
+                    2.0,
+                  ),
+                )
               ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
             ),
-            border: Border.all(
-              width: 1.0,
-              color: irmaCardTheme.bgColorLight,
-            ),
-            borderRadius: BorderRadius.all(
-              _borderRadius,
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: _transparentBlack,
-                blurRadius: _blurRadius,
-                offset: const Offset(
-                  0.0,
-                  2.0,
-                ),
-              )
-            ],
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(
-                  top: IrmaTheme.of(context).smallSpacing,
-                  left: IrmaTheme.of(context).defaultSpacing,
-                  bottom: 0,
-                ),
-                child: Text(
-                  getTranslation(widget.attributes.credentialType.name),
-                  style: Theme.of(context).textTheme.subhead.copyWith(
-                    color: irmaCardTheme.fgColor,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: IrmaTheme.of(context).smallSpacing,
+                    left: IrmaTheme.of(context).defaultSpacing,
+                    bottom: 0,
+                  ),
+                  child: Text(
+                    getTranslation(widget.attributes.credentialType.name),
+                    style: Theme.of(context).textTheme.subhead.copyWith(
+                          color: irmaCardTheme.fgColor,
+                        ),
                   ),
                 ),
-              ),
-              CardAttributes(
-                personalData: widget.attributes,
-                issuer: widget.attributes.issuer,
-                isCardUnblurred: isCardReadable,
-                irmaCardTheme: irmaCardTheme,
-                photo: photo,
-                scrollOverflowCallback: widget.scrollBeyondBoundsCallback,
-              ),
-            ],
+                CardAttributes(
+                  personalData: widget.attributes,
+                  issuer: widget.attributes.issuer,
+                  isCardUnblurred: isCardReadable,
+                  irmaCardTheme: irmaCardTheme,
+                  photo: photo,
+                  scrollOverflowCallback: widget.scrollBeyondBoundsCallback,
+                ),
+              ],
+            ),
           ),
-        ),
-        stackedCard: LayoutBuilder(
-          builder: (BuildContext context, BoxConstraints constraints) {
-            return IgnorePointer(
-              ignoring: true,
-              child: Container(
-                height: constraints.smallest.height,
-                margin: EdgeInsets.symmetric(horizontal: IrmaTheme.of(context).smallSpacing),
-                decoration: BoxDecoration(
-                  color: _transparentWhite,
-                  borderRadius: BorderRadius.all(
-                    _borderRadius,
+          stackedCard: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              return IgnorePointer(
+                ignoring: true,
+                child: Container(
+                  height: constraints.smallest.height,
+                  margin: EdgeInsets.symmetric(horizontal: IrmaTheme.of(context).smallSpacing),
+                  decoration: BoxDecoration(
+                    color: _transparentWhite,
+                    borderRadius: BorderRadius.all(
+                      _borderRadius,
+                    ),
                   ),
                 ),
-              ),
-            );
-          },
-        ),
-        applyStack: widget.isDeleted),
+              );
+            },
+          ),
+          applyStack: widget.isDeleted),
     );
   }
 
@@ -153,7 +153,7 @@ class _IrmaCardState extends State<IrmaCard> with SingleTickerProviderStateMixin
     Image photoImage;
 
     final photoData = personalData.attributes.entries
-      .firstWhere((personal) => personal.key.name['en'] == 'Photo', orElse: () => null);
+        .firstWhere((personal) => personal.key.name['en'] == 'Photo', orElse: () => null);
 
     if (photoData != null) {
       photoImage = Image.memory(const Base64Decoder().convert(photoData.value['en']));
@@ -164,12 +164,12 @@ class _IrmaCardState extends State<IrmaCard> with SingleTickerProviderStateMixin
 }
 
 Widget stackedCard({Widget card, Widget stackedCard, bool applyStack}) => applyStack
-  ? Stack(
-  children: <Widget>[
-    card,
-    Positioned.fill(
-      child: stackedCard,
-    ),
-  ],
-)
-  : card;
+    ? Stack(
+        children: <Widget>[
+          card,
+          Positioned.fill(
+            child: stackedCard,
+          ),
+        ],
+      )
+    : card;
--- lib/src/widgets/card/card_attributes.dart
+++ lib/src/widgets/card/card_attributes.dart
@@ -24,7 +24,7 @@ class CardAttributes extends StatelessWidget {
   final void Function(double) scrollOverflowCallback;
 
   CardAttributes(
-    {this.personalData,
+      {this.personalData,
       this.issuer,
       this.isCardUnblurred,
       this.irmaCardTheme,
@@ -106,86 +106,86 @@ class CardAttributes extends StatelessWidget {
   }
 
   List<Widget> getAttributes(BuildContext context, TextStyle bodyTheme) => personalData.attributes.entries
-    .expand(
-      (personal) => [
-      Opacity(
-        opacity: 0.8,
-        child: Text(
-          personal.key.name[_lang],
-          style: IrmaTheme.of(context).textTheme.body1.copyWith(color: irmaCardTheme.fgColor, fontSize: 14),
-          overflow: TextOverflow.ellipsis,
-        ),
-      ),
-      BlurText(
-        text: personal.value[_lang],
-        theme: IrmaTheme.of(context).textTheme.body2.copyWith(color: irmaCardTheme.fgColor),
-        color: irmaCardTheme.fgColor,
-        isTextBlurred: false,
-      ),
-      SizedBox(
-        height: IrmaTheme.of(context).smallSpacing,
-      ),
-    ],
-  )
-    .toList();
+      .expand(
+        (personal) => [
+          Opacity(
+            opacity: 0.8,
+            child: Text(
+              personal.key.name[_lang],
+              style: IrmaTheme.of(context).textTheme.body1.copyWith(color: irmaCardTheme.fgColor, fontSize: 14),
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          BlurText(
+            text: personal.value[_lang],
+            theme: IrmaTheme.of(context).textTheme.body2.copyWith(color: irmaCardTheme.fgColor),
+            color: irmaCardTheme.fgColor,
+            isTextBlurred: false,
+          ),
+          SizedBox(
+            height: IrmaTheme.of(context).smallSpacing,
+          ),
+        ],
+      )
+      .toList();
 
   Widget getIssuer(BuildContext context, TextStyle bodyTheme) => Padding(
-    padding: EdgeInsets.only(
-      top: IrmaTheme.of(context).smallSpacing,
-      left: IrmaTheme.of(context).defaultSpacing,
-      right: IrmaTheme.of(context).defaultSpacing,
-    ),
-    child: Row(
-      children: [
-        Container(
-          width: _indent,
-          child: Opacity(
-            opacity: 0.8,
-            child: Text(
-              FlutterI18n.translate(context, 'wallet.issuer'),
-              style: IrmaTheme.of(context).textTheme.body1.copyWith(color: irmaCardTheme.fgColor, fontSize: 12),
+        padding: EdgeInsets.only(
+          top: IrmaTheme.of(context).smallSpacing,
+          left: IrmaTheme.of(context).defaultSpacing,
+          right: IrmaTheme.of(context).defaultSpacing,
+        ),
+        child: Row(
+          children: [
+            Container(
+              width: _indent,
+              child: Opacity(
+                opacity: 0.8,
+                child: Text(
+                  FlutterI18n.translate(context, 'wallet.issuer'),
+                  style: IrmaTheme.of(context).textTheme.body1.copyWith(color: irmaCardTheme.fgColor, fontSize: 12),
+                ),
+              ),
             ),
-          ),
+            Expanded(
+              child: Text(
+                issuer.name[_lang],
+                style: IrmaTheme.of(context).textTheme.body1.copyWith(color: irmaCardTheme.fgColor, fontSize: 12),
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ],
         ),
-        Expanded(
-          child: Text(
-            issuer.name[_lang],
-            style: IrmaTheme.of(context).textTheme.body1.copyWith(color: irmaCardTheme.fgColor, fontSize: 12),
-            overflow: TextOverflow.ellipsis,
-          ),
-        ),
-      ],
-    ),
-  );
+      );
 
   Widget getExpiration(BuildContext context, TextStyle bodyTheme) => Padding(
-    padding: EdgeInsets.only(
-      bottom: IrmaTheme.of(context).smallSpacing,
-      left: IrmaTheme.of(context).defaultSpacing,
-      right: IrmaTheme.of(context).defaultSpacing,
-    ),
-    child: Row(
-      children: [
-        Container(
-          width: _indent,
-          child: Opacity(
-            opacity: 0.8,
-            child: Text(
-              FlutterI18n.translate(context, 'wallet.expiration'),
-              style: IrmaTheme.of(context).textTheme.body1.copyWith(color: irmaCardTheme.fgColor, fontSize: 12),
+        padding: EdgeInsets.only(
+          bottom: IrmaTheme.of(context).smallSpacing,
+          left: IrmaTheme.of(context).defaultSpacing,
+          right: IrmaTheme.of(context).defaultSpacing,
+        ),
+        child: Row(
+          children: [
+            Container(
+              width: _indent,
+              child: Opacity(
+                opacity: 0.8,
+                child: Text(
+                  FlutterI18n.translate(context, 'wallet.expiration'),
+                  style: IrmaTheme.of(context).textTheme.body1.copyWith(color: irmaCardTheme.fgColor, fontSize: 12),
+                ),
+              ),
             ),
-          ),
+            Expanded(
+              child: Text(
+                getReadableDate(personalData.expires, _lang),
+                style: IrmaTheme.of(context).textTheme.body1.copyWith(color: irmaCardTheme.fgColor, fontSize: 12),
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ],
         ),
-        Expanded(
-          child: Text(
-            getReadableDate(personalData.expires, _lang),
-            style: IrmaTheme.of(context).textTheme.body1.copyWith(color: irmaCardTheme.fgColor, fontSize: 12),
-            overflow: TextOverflow.ellipsis,
-          ),
-        ),
-      ],
-    ),
-  );
+      );
 
   String getReadableDate(DateTime date, String lang) {
     return DateFormat.yMMMMd(lang).format(date);
@@ -193,11 +193,11 @@ class CardAttributes extends StatelessWidget {
 }
 
 Widget photoCard({Widget card, Widget photoCard, bool applyPhoto}) => applyPhoto
-  ? Row(
-  crossAxisAlignment: CrossAxisAlignment.start,
-  children: <Widget>[
-    photoCard,
-    Expanded(child: card),
-  ],
-)
-  : card;
+    ? Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          photoCard,
+          Expanded(child: card),
+        ],
+      )
+    : card;
