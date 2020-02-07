c2854a486659a053fafcd8c611d86245b0465aad
Rowan Goemans <rowan.goemans@bdtnl.com>
2020-02-07 15:02:16 +0100

    -- Implemented new tooltip based on flutters own tooltip.

--- lib/src/widgets/irma_bottom_bar.dart
+++ lib/src/widgets/irma_bottom_bar.dart
@@ -5,6 +5,7 @@ import 'package:irmamobile/src/theme/theme.dart';
 import 'package:irmamobile/src/widgets/irma_button.dart';
 import 'package:irmamobile/src/widgets/irma_text_button.dart';
 import 'package:irmamobile/src/widgets/irma_themed_button.dart';
+import 'package:irmamobile/src/widgets/irma_tooltip.dart';
 
 class IrmaBottomBar extends StatelessWidget {
   const IrmaBottomBar({
@@ -12,18 +13,18 @@ class IrmaBottomBar extends StatelessWidget {
     @required this.primaryButtonLabel,
     this.onPrimaryPressed,
     this.onPrimaryDisabledPressed,
-    this.tooltipOnPrimaryButton = false,
+    this.showTooltipOnPrimary = false,
     this.secondaryButtonLabel,
     this.onSecondaryPressed,
     this.toolTipLabel,
-  })  : assert((tooltipOnPrimaryButton == true) == (toolTipLabel != null)),
+  })  : assert((showTooltipOnPrimary == false) || (toolTipLabel != null)),
         assert(primaryButtonLabel != null),
         super(key: key);
 
   final String primaryButtonLabel;
   final VoidCallback onPrimaryPressed;
   final VoidCallback onPrimaryDisabledPressed;
-  final bool tooltipOnPrimaryButton;
+  final bool showTooltipOnPrimary;
   final String secondaryButtonLabel;
   final VoidCallback onSecondaryPressed;
   final String toolTipLabel;
@@ -43,7 +44,7 @@ class IrmaBottomBar extends StatelessWidget {
       ));
     }
 
-    Widget primaryButtonn = IrmaButton(
+    Widget primaryButton = IrmaButton(
       size: IrmaButtonSize.large,
       minWidth: buttonWidth,
       onPressed: onPrimaryPressed,
@@ -51,14 +52,12 @@ class IrmaBottomBar extends StatelessWidget {
       label: primaryButtonLabel,
     );
 
-    if (tooltipOnPrimaryButton) {
-      primaryButtonn = _IrmaToolTip(
-        label: FlutterI18n.translate(context, toolTipLabel),
-        child: primaryButtonn,
-      );
+    if (toolTipLabel != null) {
+      primaryButton = IrmaToolTip(
+          label: FlutterI18n.translate(context, toolTipLabel), show: showTooltipOnPrimary, child: primaryButton);
     }
 
-    btns.add(primaryButtonn);
+    btns.add(primaryButton);
 
     return btns;
   }
@@ -89,69 +88,4 @@ class IrmaBottomBar extends StatelessWidget {
       );
     });
   }
-}
-
-class _IrmaToolTip extends StatelessWidget {
-  const _IrmaToolTip({
-    Key key,
-    @required this.label,
-    @required this.child,
-  })  : assert(label != null),
-        assert(child != null),
-        super(key: key);
-
-  final String label;
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    return Tooltip(
-      decoration: ShapeDecoration(
-          color: IrmaTheme.of(context).primaryLight,
-          shape: const _TooltipShapeBorder(),
-          shadows: [BoxShadow(color: Colors.black54, blurRadius: 0.8, offset: const Offset(0, 1))]),
-      padding: EdgeInsets.symmetric(
-          horizontal: IrmaTheme.of(context).mediumSpacing, vertical: IrmaTheme.of(context).smallSpacing),
-      textStyle: IrmaTheme.of(context).textTheme.display1,
-      message: FlutterI18n.translate(context, label),
-      child: child,
-    );
-  }
-}
-
-class _TooltipShapeBorder extends ShapeBorder {
-  final double arrowWidth;
-  final double arrowHeight;
-  final double arrowArc;
-  final double radius;
-
-  const _TooltipShapeBorder({
-    this.radius = 4.0,
-    this.arrowWidth = 30.0,
-    this.arrowHeight = 15.0,
-    this.arrowArc = 0.0,
-  }) : assert(arrowArc <= 1.0 && arrowArc >= 0.0);
-
-  @override
-  EdgeInsetsGeometry get dimensions => EdgeInsets.only(bottom: arrowHeight);
-
-  @override
-  Path getInnerPath(Rect rect, {TextDirection textDirection}) => null;
-
-  @override
-  Path getOuterPath(Rect rect, {TextDirection textDirection}) {
-    final newRect = Rect.fromPoints(rect.topLeft, rect.bottomRight - Offset(0, arrowHeight));
-    final double x = arrowWidth, y = arrowHeight, r = 1 - arrowArc;
-    return Path()
-      ..addRRect(RRect.fromRectAndRadius(newRect, Radius.circular(radius)))
-      ..moveTo(newRect.bottomCenter.dx + x / 2, newRect.bottomCenter.dy)
-      ..relativeLineTo(-x / 2 * r, y * r)
-      ..relativeLineTo(-x / 2 * r, -y * r);
-  }
-
-  @override
-  void paint(Canvas canvas, Rect rect, {TextDirection textDirection}) {}
-
-  @override
-  ShapeBorder scale(double t) => this;
 }
---
+++ lib/src/widgets/programmable_tooltip.dart
@@ -0,0 +1,263 @@
+// Based on flutter source of their tooltip.
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter/widgets.dart';
+import 'package:rxdart/subjects.dart';
+
+class ProgrammableTooltip extends StatefulWidget {
+  const ProgrammableTooltip({
+    Key key,
+    @required this.message,
+    this.height,
+    this.padding,
+    this.margin,
+    this.verticalOffset,
+    this.preferBelow,
+    this.excludeFromSemantics,
+    this.decoration,
+    this.textStyle,
+    this.show,
+    this.child,
+  })  : assert(message != null),
+        super(key: key);
+
+  final String message;
+  final double height;
+  final EdgeInsetsGeometry padding;
+  final EdgeInsetsGeometry margin;
+  final double verticalOffset;
+  final bool preferBelow;
+  final bool excludeFromSemantics;
+  final Widget child;
+  final Decoration decoration;
+  final TextStyle textStyle;
+  final bool show;
+
+  @override
+  _TooltipState createState() => _TooltipState();
+}
+
+class _TooltipState extends State<ProgrammableTooltip> with SingleTickerProviderStateMixin {
+  static const double _defaultTooltipHeight = 32.0;
+  static const double _defaultVerticalOffset = 24.0;
+  static const bool _defaultPreferBelow = true;
+  static const EdgeInsetsGeometry _defaultPadding = EdgeInsets.symmetric(horizontal: 16.0);
+  static const EdgeInsetsGeometry _defaultMargin = EdgeInsets.all(0.0);
+  static const Duration _fadeDuration = Duration(milliseconds: 100);
+  static const bool _defaultExcludeFromSemantics = false;
+
+  double height;
+  EdgeInsetsGeometry padding;
+  EdgeInsetsGeometry margin;
+  Decoration decoration;
+  TextStyle textStyle;
+  double verticalOffset;
+  bool preferBelow;
+  bool excludeFromSemantics;
+  OverlayEntry _entry;
+  BehaviorSubject<bool> _showChanges;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _showChanges = BehaviorSubject<bool>();
+    SchedulerBinding.instance.addPostFrameCallback((_) => _createNewEntry());
+  }
+
+  void _createNewEntry() {
+    _entry?.remove();
+
+    final RenderBox box = context.findRenderObject() as RenderBox;
+    final Offset target = box.localToGlobal(box.size.center(Offset.zero));
+
+    _entry = OverlayEntry(
+        builder: (BuildContext context) => StreamBuilder(
+            stream: _showChanges,
+            builder: (BuildContext context, AsyncSnapshot<bool> snapshot) => Directionality(
+                  textDirection: Directionality.of(context),
+                  child: _TooltipOverlay(
+                    message: widget.message,
+                    height: height,
+                    padding: padding,
+                    margin: margin,
+                    decoration: decoration,
+                    textStyle: textStyle,
+                    target: target,
+                    verticalOffset: verticalOffset,
+                    preferBelow: preferBelow,
+                    show: snapshot.hasData ? snapshot.data : false,
+                    fadeDuration: _fadeDuration,
+                  ),
+                )));
+    Overlay.of(context).insert(_entry);
+    SemanticsService.tooltip(widget.message);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _showChanges.add(widget.show);
+    assert(Overlay.of(context) != null);
+    final ThemeData theme = Theme.of(context);
+    final TooltipThemeData tooltipTheme = TooltipTheme.of(context);
+    TextStyle defaultTextStyle;
+    BoxDecoration defaultDecoration;
+    if (theme.brightness == Brightness.dark) {
+      defaultTextStyle = theme.textTheme.body1.copyWith(
+        color: Colors.black,
+      );
+      defaultDecoration = BoxDecoration(
+        color: Colors.white.withOpacity(0.9),
+        borderRadius: const BorderRadius.all(Radius.circular(4)),
+      );
+    } else {
+      defaultTextStyle = theme.textTheme.body1.copyWith(
+        color: Colors.white,
+      );
+      defaultDecoration = BoxDecoration(
+        color: Colors.grey[700].withOpacity(0.9),
+        borderRadius: const BorderRadius.all(Radius.circular(4)),
+      );
+    }
+
+    height = widget.height ?? tooltipTheme.height ?? _defaultTooltipHeight;
+    padding = widget.padding ?? tooltipTheme.padding ?? _defaultPadding;
+    margin = widget.margin ?? tooltipTheme.margin ?? _defaultMargin;
+    verticalOffset = widget.verticalOffset ?? tooltipTheme.verticalOffset ?? _defaultVerticalOffset;
+    preferBelow = widget.preferBelow ?? tooltipTheme.preferBelow ?? _defaultPreferBelow;
+    excludeFromSemantics =
+        widget.excludeFromSemantics ?? tooltipTheme.excludeFromSemantics ?? _defaultExcludeFromSemantics;
+    decoration = widget.decoration ?? tooltipTheme.decoration ?? defaultDecoration;
+    textStyle = widget.textStyle ?? tooltipTheme.textStyle ?? defaultTextStyle;
+
+    return Semantics(
+      label: excludeFromSemantics ? null : widget.message,
+      child: widget.child,
+    );
+  }
+}
+
+/// A delegate for computing the layout of a tooltip to be displayed above or
+/// bellow a target specified in the global coordinate system.
+class _TooltipPositionDelegate extends SingleChildLayoutDelegate {
+  /// Creates a delegate for computing the layout of a tooltip.
+  ///
+  /// The arguments must not be null.
+  _TooltipPositionDelegate({
+    @required this.target,
+    @required this.verticalOffset,
+    @required this.preferBelow,
+  })  : assert(target != null),
+        assert(verticalOffset != null),
+        assert(preferBelow != null);
+
+  /// The offset of the target the tooltip is positioned near in the global
+  /// coordinate system.
+  final Offset target;
+
+  /// The amount of vertical distance between the target and the displayed
+  /// tooltip.
+  final double verticalOffset;
+
+  /// Whether the tooltip is displayed below its widget by default.
+  ///
+  /// If there is insufficient space to display the tooltip in the preferred
+  /// direction, the tooltip will be displayed in the opposite direction.
+  final bool preferBelow;
+
+  @override
+  BoxConstraints getConstraintsForChild(BoxConstraints constraints) => constraints.loosen();
+
+  @override
+  Offset getPositionForChild(Size size, Size childSize) {
+    return positionDependentBox(
+      size: size,
+      childSize: childSize,
+      target: target,
+      verticalOffset: verticalOffset,
+      preferBelow: preferBelow,
+    );
+  }
+
+  @override
+  bool shouldRelayout(_TooltipPositionDelegate oldDelegate) {
+    return target != oldDelegate.target ||
+        verticalOffset != oldDelegate.verticalOffset ||
+        preferBelow != oldDelegate.preferBelow;
+  }
+}
+
+class _TooltipOverlay extends StatelessWidget {
+  const _TooltipOverlay({
+    Key key,
+    this.message,
+    this.height,
+    this.padding,
+    this.margin,
+    this.decoration,
+    this.textStyle,
+    this.animation,
+    this.target,
+    this.verticalOffset,
+    this.preferBelow,
+    this.fadeDuration,
+    this.show,
+  }) : super(key: key);
+
+  final String message;
+  final double height;
+  final EdgeInsetsGeometry padding;
+  final EdgeInsetsGeometry margin;
+  final Decoration decoration;
+  final TextStyle textStyle;
+  final Animation<double> animation;
+  final Offset target;
+  final double verticalOffset;
+  final bool preferBelow;
+  final Duration fadeDuration;
+  final bool show;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fill(
+      child: IgnorePointer(
+        child: CustomSingleChildLayout(
+          delegate: _TooltipPositionDelegate(
+            target: target,
+            verticalOffset: verticalOffset,
+            preferBelow: preferBelow,
+          ),
+          child: AnimatedOpacity(
+            opacity: show ? 1.0 : 0.0,
+            duration: fadeDuration,
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: height),
+              child: DefaultTextStyle(
+                style: Theme.of(context).textTheme.body1,
+                child: Container(
+                  decoration: decoration,
+                  padding: padding,
+                  margin: margin,
+                  child: Center(
+                    widthFactor: 1.0,
+                    heightFactor: 1.0,
+                    child: Text(
+                      message,
+                      style: textStyle,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
