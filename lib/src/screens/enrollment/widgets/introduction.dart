af05937a37a7f68b61fdd2e48ce22f8ea9c3a844
Edwin Martin <edwin@bitstorm.org>
2020-02-12 09:13:15 +0100

    Resolve "Add: privacy sentence on onboarding"

--- lib/src/screens/enrollment/widgets/introduction.dart
+++ lib/src/screens/enrollment/widgets/introduction.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
 import 'package:irmamobile/src/screens/enrollment/widgets/choose_pin.dart';
 import 'package:irmamobile/src/theme/irma_icons.dart';
 import 'package:irmamobile/src/theme/theme.dart';
 import 'package:irmamobile/src/widgets/irma_button.dart';
+import 'package:irmamobile/src/widgets/irma_text_button.dart';
+import 'package:irmamobile/src/screens/webview/webview_screen.dart';
 
 class Introduction extends StatefulWidget {
   static const String routeName = 'introduction';
@@ -60,6 +64,8 @@ class _IntroductionState extends State<Introduction> {
                 excludeFromSemantics: true, width: 341, height: 247),
             titleContent: FlutterI18n.translate(context, 'enrollment.introduction.screen3.title'),
             textContent: FlutterI18n.translate(context, 'enrollment.introduction.screen3.text'),
+            linkText: 'enrollment.introduction.screen3.privacy.text',
+            linkUrl: 'enrollment.introduction.screen3.privacy.url',
             onNextScreen: () => {},
             onPressButton: () => Navigator.of(context).pushNamed(ChoosePin.routeName),
             finalScreen: true,
@@ -73,6 +79,8 @@ class _IntroductionState extends State<Introduction> {
 class Walkthrough extends StatelessWidget {
   final String titleContent;
   final String textContent;
+  final String linkText;
+  final String linkUrl;
   final SvgPicture image;
   final bool finalScreen;
   final void Function() onNextScreen;
@@ -84,6 +92,8 @@ class Walkthrough extends StatelessWidget {
     @required this.textContent,
     @required this.image,
     @required this.onNextScreen,
+    this.linkText,
+    this.linkUrl,
     this.onPressButton,
     this.finalScreen = false,
   }) : super(key: key);
@@ -97,8 +107,8 @@ class Walkthrough extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               image,
-              const SizedBox(height: 44.0),
               Container(
+                padding: const EdgeInsets.only(top: 44.0),
                 alignment: Alignment.center,
                 constraints: const BoxConstraints(maxWidth: 256.0),
                 child: Text(
@@ -107,8 +117,8 @@ class Walkthrough extends StatelessWidget {
                   textAlign: TextAlign.center,
                 ),
               ),
-              const SizedBox(height: 16.0),
               Container(
+                padding: EdgeInsets.only(top: IrmaTheme.of(context).defaultSpacing),
                 alignment: Alignment.center,
                 constraints: const BoxConstraints(maxWidth: 256.0),
                 child: Text(
@@ -117,6 +127,37 @@ class Walkthrough extends StatelessWidget {
                   textAlign: TextAlign.center,
                 ),
               ),
+              if (linkText != null)
+                Container(
+                  padding: EdgeInsets.only(top: IrmaTheme.of(context).defaultSpacing),
+                  alignment: Alignment.center,
+                  constraints: const BoxConstraints(maxWidth: 256.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      try {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return WebviewScreen(FlutterI18n.translate(context, linkUrl));
+                            },
+                          ),
+                        );
+                      } on PlatformException catch (e) {
+                        debugPrint(e.toString());
+                        debugPrint("error on launch of url - probably bad certificate?");
+                      }
+                    },
+                    child: Center(
+                      child: Text(
+                        FlutterI18n.translate(context, linkText),
+                        style: IrmaTheme.of(context).hyperlinkTextStyle.copyWith(
+                              decoration: TextDecoration.underline,
+                            ),
+                      ),
+                    ),
+                  ),
+                ),
             ],
           ),
         ),
