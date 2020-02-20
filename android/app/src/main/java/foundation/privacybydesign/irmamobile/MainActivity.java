481c43a8358727595a02950437ba02211fece7ba
Hanna Schraffenberger <hanna@science.ru.nl>
2020-02-20 13:04:32 +0100

    static splash <3

--- android/app/src/main/java/foundation/privacybydesign/irmamobile/MainActivity.java
+++ android/app/src/main/java/foundation/privacybydesign/irmamobile/MainActivity.java
@@ -7,16 +7,39 @@ import io.flutter.plugins.GeneratedPluginRegistrant;
 import foundation.privacybydesign.irmamobile.plugins.irma_mobile_bridge.IrmaMobileBridgePlugin;
 import irmagobridge.Irmagobridge;
 
+import android.os.Build;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    boolean flutter_native_splash = true;
+    int originalStatusBarColor = 0;
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        originalStatusBarColor = getWindow().getStatusBarColor();
+        getWindow().setStatusBarColor(0xffdee1e4);
+    }
+    int originalStatusBarColorFinal = originalStatusBarColor;
+
 
     // Initialize the Go binding here by calling a seemingly noop function
     Irmagobridge.prestart();
 
     // Plugins
     GeneratedPluginRegistrant.registerWith(this);
+    ViewTreeObserver vto = getFlutterView().getViewTreeObserver();
+    vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+      @Override
+      public void onGlobalLayout() {
+        getFlutterView().getViewTreeObserver().removeOnGlobalLayoutListener(this);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+          getWindow().setStatusBarColor(originalStatusBarColorFinal);
+        }
+      }
+    });
+
     IrmaMobileBridgePlugin.registerWith(
       this.registrarFor("foundation.privacybydesign.irmamobile.plugins.irma_mobile_bridge.IrmaMobileBridgePlugin")
     );
