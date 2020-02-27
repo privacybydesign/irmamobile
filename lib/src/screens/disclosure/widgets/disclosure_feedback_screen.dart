f6cc8e67cf0f7066d4881d326b2e33bcc2218a5d
Geert-Johan Riemer <geertjohan@geertjohan.net>
2020-02-27 15:13:23 +0100

    refactor: Split disclosure feedback into separate widget, implement suggestion to use OK button to pop to wallet

---
+++ lib/src/screens/disclosure/widgets/disclosure_feedback_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:irmamobile/src/theme/irma_icons.dart';
+import 'package:irmamobile/src/theme/theme.dart';
+import 'package:irmamobile/src/widgets/irma_outlined_button.dart';
+
+class DisclosureFeedbackScreen extends StatelessWidget {
+  final bool success;
+  final String otherParty;
+  final Function(BuildContext) popToWallet;
+
+  const DisclosureFeedbackScreen({this.success, this.otherParty, this.popToWallet});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // This screen intentionally doesn't container an AppBar, as this screen can be closed
+      // to get the app bac back. Otherwise, strange routes such as the settings or side menu
+      // could be pushed on top of this screen, where it doesn't make sense
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Container(),
+          ),
+          Icon(
+            success ? IrmaIcons.valid : IrmaIcons.invalid,
+            size: 120,
+            color: success ? IrmaTheme.of(context).interactionValid : IrmaTheme.of(context).interactionAlert,
+          ),
+          const SizedBox(height: 43),
+          Text(
+            FlutterI18n.translate(
+              context,
+              "disclosure.feedback.header.$success",
+              {"otherParty": otherParty},
+            ),
+            style: Theme.of(context).textTheme.display3,
+          ),
+          const SizedBox(height: 10),
+          Text(
+            FlutterI18n.translate(
+              context,
+              "disclosure.feedback.text.$success",
+              {"otherParty": otherParty},
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 38),
+          IrmaOutlinedButton(
+            label: FlutterI18n.translate(context, "disclosure.feedback.ok"),
+            onPressed: () => popToWallet(context),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(),
+          ),
+        ],
+      ),
+    );
+  }
+}
