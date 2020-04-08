69b54a364407f402b94f6db32a132731d7e37a21
Hanna <git@creativecode.org>
2020-04-08 15:49:39 +0200

    copy VD

--- lib/src/screens/disclosure/call_info_screen.dart
+++ lib/src/screens/disclosure/call_info_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:irmamobile/src/theme/theme.dart';
 import 'package:irmamobile/src/util/translated_text.dart';
 import 'package:irmamobile/src/widgets/irma_bottom_bar.dart';
@@ -10,7 +11,6 @@ class CallInfoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: IrmaTheme.of(context).grayscaleWhite,
       appBar: AppBar(
         centerTitle: true,
         title: Text(
@@ -23,124 +23,162 @@ class CallInfoScreen extends StatelessWidget {
           //
         },
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: IrmaTheme.of(context).defaultSpacing),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    SizedBox(height: IrmaTheme.of(context).defaultSpacing),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        TranslatedText(
-                          'Je postcode en woonplaats zijn doorgegegeven aan bellen.nijmegen.nl.',
-                          style: Theme.of(context).textTheme.body1,
-                        ),
-                        SizedBox(height: IrmaTheme.of(context).defaultSpacing),
-                        TranslatedText(
-                          'Voer de volgende stappen uit om te bellen.',
-                          style: Theme.of(context).textTheme.body1,
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: IrmaTheme.of(context).smallSpacing),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Container(
-                          child: Padding(
-                            padding: EdgeInsets.only(
-                                left: IrmaTheme.of(context).smallSpacing,
-                                top: IrmaTheme.of(context).tinySpacing * 0.75,
-                                right: IrmaTheme.of(context).defaultSpacing),
-                            child: Container(
-                                child: CircleAvatar(
-                                    child: const Text('1'),
-                                    foregroundColor: IrmaTheme.of(context).primaryDark,
-                                    backgroundColor: IrmaTheme.of(context).primaryLight),
-                                width: 26.0,
-                                height: 26.0,
-                                padding: const EdgeInsets.all(1.5), // borde width
-                                decoration: new BoxDecoration(
-                                  color: const Color(0xFF000000), // border color
-                                  shape: BoxShape.circle,
-                                )),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: IrmaTheme.of(context).defaultSpacing),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      SizedBox(height: IrmaTheme.of(context).defaultSpacing),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          TranslatedText(
+                            'Je postcode en woonplaats zijn doorgegegeven aan bellen.nijmegen.nl.',
+                            style: Theme.of(context).textTheme.body1,
                           ),
-                        ),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              TranslatedText(
-                                'Kies doorgaan',
-                                style: Theme.of(context).textTheme.body2,
-                              ),
-                              SizedBox(height: IrmaTheme.of(context).tinySpacing),
-                              TranslatedText(
-                                'Je belapplicatie opent nu vanzelf. Het telefoonnummer is al ingevuld, met een koppelcode. ',
-                                style: Theme.of(context).textTheme.body1,
-                              ),
-                            ],
+                          SizedBox(height: IrmaTheme.of(context).defaultSpacing),
+                          TranslatedText(
+                            'Voer de volgende stappen uit om te bellen.',
+                            style: Theme.of(context).textTheme.body1,
                           ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: IrmaTheme.of(context).smallSpacing),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Container(
-                          child: Padding(
-                            padding: EdgeInsets.only(
-                                left: IrmaTheme.of(context).smallSpacing,
-                                top: IrmaTheme.of(context).tinySpacing * 0.75,
-                                right: IrmaTheme.of(context).defaultSpacing),
-                            child: Container(
-                                child: CircleAvatar(
-                                    child: const Text('2'),
-                                    foregroundColor: IrmaTheme.of(context).primaryDark,
-                                    backgroundColor: IrmaTheme.of(context).primaryLight),
-                                width: 26.0,
-                                height: 26.0,
-                                padding: const EdgeInsets.all(1.5), // borde width
-                                decoration: new BoxDecoration(
-                                  color: const Color(0xFF000000), // border color
-                                  shape: BoxShape.circle,
-                                )),
+                        ],
+                      ),
+                      SizedBox(height: IrmaTheme.of(context).smallSpacing),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.only(
+                                  left: IrmaTheme.of(context).smallSpacing,
+                                  top: IrmaTheme.of(context).tinySpacing * 0.75,
+                                  right: IrmaTheme.of(context).defaultSpacing),
+                              child: Container(
+                                  child: CircleAvatar(
+                                      child: const Text('1'),
+                                      foregroundColor: IrmaTheme.of(context).primaryDark,
+                                      backgroundColor: IrmaTheme.of(context).primaryLight),
+                                  width: 26.0,
+                                  height: 26.0,
+                                  padding: const EdgeInsets.all(1.5), // borde width
+                                  decoration: new BoxDecoration(
+                                    color: const Color(0xFF000000), // border color
+                                    shape: BoxShape.circle,
+                                  )),
+                            ),
                           ),
-                        ),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              TranslatedText(
-                                'Klik de belknop in je belapplicatie',
-                                style: Theme.of(context).textTheme.body2,
-                              ),
-                              SizedBox(height: IrmaTheme.of(context).tinySpacing),
-                              TranslatedText(
-                                'Je hoort en paar piepjes en wordt verbonden.',
-                                style: Theme.of(context).textTheme.body1,
-                              ),
-                            ],
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                TranslatedText(
+                                  'Kies doorgaan',
+                                  style: Theme.of(context).textTheme.body2,
+                                ),
+                                SizedBox(height: IrmaTheme.of(context).tinySpacing),
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Expanded(
+                                      flex: 1,
+                                      child: SvgPicture.asset(
+                                        'assets/non-free/noun_number pad_374833.svg',
+                                        fit: BoxFit.scaleDown,
+                                      ),
+                                    ),
+                                    Expanded(
+                                      flex: 9,
+                                      child: Padding(
+                                        padding: const EdgeInsets.only(left: 8.0),
+                                        child: TranslatedText(
+                                          'Je belapplicatie opent nu vanzelf. Het telefoonnummer is al ingevuld, met een koppelcode. ',
+                                          style: Theme.of(context).textTheme.body1,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: IrmaTheme.of(context).largeSpacing),
-                  ],
-                ),
-              ],
+                        ],
+                      ),
+                      SizedBox(height: IrmaTheme.of(context).smallSpacing),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.only(
+                                  left: IrmaTheme.of(context).smallSpacing,
+                                  top: IrmaTheme.of(context).tinySpacing * 0.75,
+                                  right: IrmaTheme.of(context).defaultSpacing),
+                              child: Container(
+                                  child: CircleAvatar(
+                                      child: const Text('2'),
+                                      foregroundColor: IrmaTheme.of(context).primaryDark,
+                                      backgroundColor: IrmaTheme.of(context).primaryLight),
+                                  width: 26.0,
+                                  height: 26.0,
+                                  padding: const EdgeInsets.all(1.5), // borde width
+                                  decoration: new BoxDecoration(
+                                    color: const Color(0xFF000000), // border color
+                                    shape: BoxShape.circle,
+                                  )),
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                TranslatedText(
+                                  'Klik de belknop in je belapplicatie',
+                                  style: Theme.of(context).textTheme.body2,
+                                ),
+                                SizedBox(height: IrmaTheme.of(context).tinySpacing),
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Expanded(
+                                      flex: 1,
+                                      child: SvgPicture.asset(
+                                        'assets/non-free/noun_call_906214.svg',
+                                        fit: BoxFit.scaleDown,
+                                      ),
+                                    ),
+                                    Expanded(
+                                      flex: 9,
+                                      child: Padding(
+                                        padding: const EdgeInsets.only(left: 8.0),
+                                        child: TranslatedText(
+                                          'Je belapplicatie opent nu vanzelf. Het telefoonnummer is al ingevuld, met een koppelcode. ',
+                                          style: Theme.of(context).textTheme.body1,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: IrmaTheme.of(context).largeSpacing),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
