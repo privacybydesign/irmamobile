92d67651e3560fc3d0a04017d3c3d2dd30c255dc
Hanna <git@creativecode.org>
2020-04-08 16:51:07 +0200

    alternative layout

--- lib/src/screens/disclosure/call_info_screen.dart
+++ lib/src/screens/disclosure/call_info_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:irmamobile/src/theme/irma_icons.dart';
 import 'package:irmamobile/src/theme/theme.dart';
 import 'package:irmamobile/src/util/translated_text.dart';
 import 'package:irmamobile/src/widgets/irma_bottom_bar.dart';
@@ -37,17 +38,48 @@ class CallInfoScreen extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
                       SizedBox(height: IrmaTheme.of(context).defaultSpacing),
-                      Column(
+                      Row(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          TranslatedText(
-                            'Je postcode en woonplaats zijn doorgegegeven aan bellen.nijmegen.nl.',
-                            style: Theme.of(context).textTheme.body1,
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.only(
+                                  left: IrmaTheme.of(context).smallSpacing,
+                                  top: IrmaTheme.of(context).tinySpacing * 0.75,
+                                  right: IrmaTheme.of(context).defaultSpacing),
+                              child: Container(
+                                child: CircleAvatar(
+                                    child: Icon(IrmaIcons.valid),
+                                    foregroundColor: IrmaTheme.of(context).primaryDark,
+                                    backgroundColor: IrmaTheme.of(context).grayscaleWhite),
+                                width: 26.0,
+                                height: 26.0,
+                                padding: const EdgeInsets.all(1.5), // borde width
+                              ),
+                            ),
                           ),
-                          SizedBox(height: IrmaTheme.of(context).defaultSpacing),
-                          TranslatedText(
-                            'Voer de volgende stappen uit om te bellen.',
-                            style: Theme.of(context).textTheme.body1,
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                TranslatedText(
+                                  'Gelukt',
+                                  style: Theme.of(context).textTheme.body2,
+                                ),
+                                SizedBox(height: IrmaTheme.of(context).tinySpacing),
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Expanded(
+                                      child: TranslatedText(
+                                        'Je postcode en woonplaats zijn doorgegegeven aan bellen.nijmegen.nl',
+                                        style: Theme.of(context).textTheme.body1,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
                         ],
                       ),
@@ -65,7 +97,7 @@ class CallInfoScreen extends StatelessWidget {
                                   child: CircleAvatar(
                                       child: const Text('1'),
                                       foregroundColor: IrmaTheme.of(context).primaryDark,
-                                      backgroundColor: IrmaTheme.of(context).primaryLight),
+                                      backgroundColor: IrmaTheme.of(context).grayscaleWhite),
                                   width: 26.0,
                                   height: 26.0,
                                   padding: const EdgeInsets.all(1.5), // borde width
@@ -88,20 +120,56 @@ class CallInfoScreen extends StatelessWidget {
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: <Widget>[
                                     Expanded(
-                                      flex: 1,
-                                      child: SvgPicture.asset(
-                                        'assets/non-free/noun_number pad_374833.svg',
-                                        fit: BoxFit.scaleDown,
+                                      child: TranslatedText(
+                                        'Je belapplicatie opent nu vanzelf.',
+                                        style: Theme.of(context).textTheme.body1,
                                       ),
                                     ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: IrmaTheme.of(context).tinySpacing),
+                      Center(
+                        child: SizedBox(
+                          height: 50.0,
+                          child: SvgPicture.asset(
+                            'assets/non-free/noun_number_pad_374833.svg',
+                            fit: BoxFit.scaleDown,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: IrmaTheme.of(context).tinySpacing),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.only(
+                                  left: IrmaTheme.of(context).smallSpacing,
+                                  top: IrmaTheme.of(context).tinySpacing * 0.75,
+                                  right: IrmaTheme.of(context).defaultSpacing),
+                              child: Container(
+                                width: 26.0,
+                                height: 26.0,
+                                padding: const EdgeInsets.all(1.5), // borde width
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
                                     Expanded(
-                                      flex: 9,
-                                      child: Padding(
-                                        padding: const EdgeInsets.only(left: 8.0),
-                                        child: TranslatedText(
-                                          'Je belapplicatie opent nu vanzelf. Het telefoonnummer is al ingevuld, met een koppelcode. ',
-                                          style: Theme.of(context).textTheme.body1,
-                                        ),
+                                      child: TranslatedText(
+                                        'Het telefoonnummer is al ingevuld, met een koppelcode. ',
+                                        style: Theme.of(context).textTheme.body1,
                                       ),
                                     ),
                                   ],
@@ -125,7 +193,7 @@ class CallInfoScreen extends StatelessWidget {
                                   child: CircleAvatar(
                                       child: const Text('2'),
                                       foregroundColor: IrmaTheme.of(context).primaryDark,
-                                      backgroundColor: IrmaTheme.of(context).primaryLight),
+                                      backgroundColor: IrmaTheme.of(context).grayscaleWhite),
                                   width: 26.0,
                                   height: 26.0,
                                   padding: const EdgeInsets.all(1.5), // borde width
@@ -148,25 +216,43 @@ class CallInfoScreen extends StatelessWidget {
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: <Widget>[
                                     Expanded(
-                                      flex: 1,
-                                      child: SvgPicture.asset(
-                                        'assets/non-free/noun_call_906214.svg',
-                                        fit: BoxFit.scaleDown,
-                                      ),
-                                    ),
-                                    Expanded(
-                                      flex: 9,
-                                      child: Padding(
-                                        padding: const EdgeInsets.only(left: 8.0),
-                                        child: TranslatedText(
-                                          'Je belapplicatie opent nu vanzelf. Het telefoonnummer is al ingevuld, met een koppelcode. ',
-                                          style: Theme.of(context).textTheme.body1,
-                                        ),
+                                      child: TranslatedText(
+                                        'Je hoort een paar piepjes en wordt verbonden.',
+                                        style: Theme.of(context).textTheme.body1,
                                       ),
                                     ),
                                   ],
                                 ),
                               ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: IrmaTheme.of(context).tinySpacing),
+                      Center(
+                        child: SizedBox(
+                          height: 50.0,
+                          child: SvgPicture.asset(
+                            'assets/non-free/noun_call_906214.svg',
+                            fit: BoxFit.scaleDown,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: IrmaTheme.of(context).tinySpacing),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.only(
+                                  left: IrmaTheme.of(context).smallSpacing,
+                                  top: IrmaTheme.of(context).tinySpacing * 0.75,
+                                  right: IrmaTheme.of(context).defaultSpacing),
+                              child: Container(
+                                width: 26.0,
+                                height: 26.0,
+                                padding: const EdgeInsets.all(1.5), // borde width
+                              ),
                             ),
                           ),
                         ],
