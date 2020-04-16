fbb9cc8688c2cf92a115ddc94084ba866e14a986
Leon <leon.botros@ru.nl>
2020-04-16 10:20:46 +0200

    fix: do not lock when unenrolled

--- lib/app.dart
+++ lib/app.dart
@@ -103,7 +103,9 @@ class AppState extends State<App> with WidgetsBindingObserver {
         state == AppLifecycleState.resumed) {
       // First check whether we should redo pin verification
       final lastActive = await repo.getLastActiveTime().first;
-      if (lastActive.isBefore(DateTime.now().subtract(const Duration(minutes: 5)))) {
+      final status = await repo.getEnrollmentStatus().first;
+      if (lastActive.isBefore(DateTime.now().subtract(const Duration(minutes: 5))) &&
+          status != EnrollmentStatus.unenrolled) {
         repo.lock();
         _navigatorKey.currentState.pushNamed(PinScreen.routeName);
       } else if (startQrScanner) {
