95e02c7a359cd5d36d686b92ee1b86ac6fed4570
Leon <leon.botros@ru.nl>
2020-04-22 15:08:58 +0200

    make EmailSentScreen a named route

--- lib/src/screens/enrollment/widgets/submit.dart
+++ lib/src/screens/enrollment/widgets/submit.dart
@@ -62,10 +62,9 @@ class _SubmitState extends State<Submit> {
               ));
             } else {
               // Enrollment succeeded
-              Navigator.of(context).pop();
               if (state.emailValid) {
                 Navigator.of(context, rootNavigator: true)
-                    .pushReplacement(MaterialPageRoute(builder: (context) => EmailSentScreen(email: state.email)));
+                    .pushReplacementNamed(EmailSentScreen.routeName, arguments: state.email);
               } else {
                 Navigator.of(context, rootNavigator: true).pushReplacementNamed(WalletScreen.routeName);
               }
