c35c746e4569c6196793f394219ab9f4761df53a
David Venhoek <d.venhoek@privacybydesign.foundation>
2020-11-20 15:01:16 +0100

    Fixed bug in IIAB launcher using incorrect context for launch.

--- android/app/src/main/java/foundation/privacybydesign/irmamobile/plugins/iiab/IIABPlugin.java
+++ android/app/src/main/java/foundation/privacybydesign/irmamobile/plugins/iiab/IIABPlugin.java
@@ -5,13 +5,18 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 
 import android.net.Uri;
+import android.util.Log;
+import android.app.Activity;
 import androidx.browser.customtabs.CustomTabsIntent;
 import android.graphics.Color;
 
-public class IIABPlugin implements MethodCallHandler, FlutterPlugin {
+public class IIABPlugin implements MethodCallHandler, FlutterPlugin, ActivityAware {
     private FlutterPlugin.FlutterPluginBinding binding;
+    private Activity mainActivity;
 
     public void onAttachedToEngine(FlutterPlugin.FlutterPluginBinding binding) {
         this.binding = binding;
@@ -24,6 +29,26 @@ public class IIABPlugin implements MethodCallHandler, FlutterPlugin {
     }
 
     @Override
+    public void onAttachedToActivity(ActivityPluginBinding binding) {
+        mainActivity = binding.getActivity();
+    }
+
+    @Override
+    public void onDetachedFromActivity(){
+        mainActivity = null;
+    }
+
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+        onDetachedFromActivity();
+    }
+
+    @Override
+    public void onReattachedToActivityForConfigChanges(ActivityPluginBinding binding) {
+        onAttachedToActivity(binding);
+    }
+
+    @Override
     public void onMethodCall(MethodCall call, Result result) {
         switch (call.method) {
             case "open_browser":
@@ -32,7 +57,7 @@ public class IIABPlugin implements MethodCallHandler, FlutterPlugin {
                     CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
                     builder.setToolbarColor(Color.parseColor("#DFE6EE"));
                     CustomTabsIntent customTabsIntent = builder.build();
-                    customTabsIntent.launchUrl(binding.getApplicationContext(), Uri.parse(url));
+                    customTabsIntent.launchUrl(mainActivity, Uri.parse(url));
                 } catch (Exception e) {
                     result.error("", e.toString(), e);
                     return;
