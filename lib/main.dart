70b34137bbb1a17598b1bee5edb128b9b9b358e3
Ivar Derksen <ivar.derksen@sidn.nl>
2021-06-28 13:38:05 +0200

    Use dependency for privacy screen to resolve iOS 14.6 issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_privacy_screen/flutter_privacy_screen.dart';
 import 'package:irmamobile/app.dart';
 import 'package:irmamobile/src/data/irma_client_bridge.dart';
 import 'package:irmamobile/src/data/irma_repository.dart';
@@ -15,6 +16,8 @@ Future<void> main() async {
   runZonedGuarded<Future<void>>(() async {
     WidgetsFlutterBinding.ensureInitialized();
     IrmaRepository(client: IrmaClientBridge());
+
+    await FlutterPrivacyScreen.enablePrivacyScreen();
 
     runApp(
       const CredentialNudgeProvider(
