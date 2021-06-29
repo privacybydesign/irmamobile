6f4ebd13feed44e6dcb2529c46b23738246cd7f7
Ivar Derksen <ivar.derksen@sidn.nl>
2021-06-29 16:00:31 +0200

    Migrate to Sentry version that is compatible with Flutter 2

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ Future<void> main() async {
 
   runZonedGuarded<Future<void>>(() async {
     WidgetsFlutterBinding.ensureInitialized();
+    await initSentry();
     IrmaRepository(client: IrmaClientBridge());
 
     runApp(
--- lib/src/sentry/sentry.dart
+++ lib/src/sentry/sentry.dart
@@ -1,16 +1,21 @@
-import 'dart:io';
-
-/* TODO: Migrate to new Sentry version
 import 'package:flutter/foundation.dart';
 import 'package:irmamobile/sentry_dsn.dart';
 import 'package:irmamobile/src/data/irma_preferences.dart';
-import 'package:sentry/sentry.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
-final _sentry = dsn != '' ? SentryClient(SentryOptions(dsn: dsn)) : null;
-*/
+Future<void> initSentry() async {
+  if (dsn != '') {
+    await SentryFlutter.init(
+      (options) {
+        options.dsn = dsn;
+      },
+    );
+  }
+}
+
 Future<void> reportError(dynamic error, dynamic stackTrace, {bool userInitiated = false}) async {
   // Print the exception to the console.
-  /*if (_sentry == null) {
+  if (dsn == '') {
     // Print the full stacktrace when not provided with dsn
     debugPrint(error.toString());
     if (stackTrace != null) debugPrint(stackTrace.toString());
@@ -18,16 +23,10 @@ Future<void> reportError(dynamic error, dynamic stackTrace, {bool userInitiated 
     final enabled = await IrmaPreferences.get().getReportErrors().first;
     // Send the Exception and Stacktrace to Sentry when enabled
     if (enabled || userInitiated) {
-      _sentry.captureEvent(
-        SentryEvent(
-          throwable: error,
-          stackTrace: SentryStackTrace(stackTrace),
-          release: version,
-          tags: {
-            "OS": Platform.operatingSystem,
-            "OS Version": Platform.operatingSystemVersion,
-          },
-        ),
+      Sentry.captureException(
+        error,
+        stackTrace: stackTrace,
       );
-    }*/
+    }
+  }
 }
