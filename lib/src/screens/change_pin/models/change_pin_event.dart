0171b527dbec41ef5845efe90261545e762b1e62
Ivar Derksen <ivar.derksen@sidn.nl>
2021-06-30 20:53:26 +0200

    Upgrade equatable to v2.0.3

--- lib/src/screens/change_pin/models/change_pin_event.dart
+++ lib/src/screens/change_pin/models/change_pin_event.dart
@@ -4,36 +4,51 @@ import 'package:meta/meta.dart';
 class OldPinEntered extends Equatable {
   final String pin;
 
-  OldPinEntered({@required this.pin}) : super([pin]);
+  const OldPinEntered({@required this.pin});
 
   @override
   String toString() => 'OldPinEntered { pin: ${'*' * pin.length} }';
+
+  @override
+  List<Object> get props => [pin];
 }
 
 class NewPinChosen extends Equatable {
   final String pin;
 
-  NewPinChosen({@required this.pin}) : super([pin]);
+  const NewPinChosen({@required this.pin});
 
   @override
   String toString() => 'NewPinChosen { pin: ${'*' * pin.length} }';
+
+  @override
+  List<Object> get props => [pin];
 }
 
 class ToggleLongPin extends Equatable {
   @override
   String toString() => 'ToggleLongPin';
+
+  @override
+  List<Object> get props => [];
 }
 
 class NewPinConfirmed extends Equatable {
   final String pin;
 
-  NewPinConfirmed({@required this.pin}) : super([pin]);
+  const NewPinConfirmed({@required this.pin});
 
   @override
   String toString() => 'NewPinConfirmed { pin: ${'*' * pin.length} }';
+
+  @override
+  List<Object> get props => [pin];
 }
 
 class ChangePinCanceled extends Equatable {
   @override
   String toString() => 'ChangePinCanceled';
+
+  @override
+  List<Object> get props => [];
 }
