60cba0d6b677cb2c7c60f7e72c1944f292168a07
Ivar Derksen <ivar.derksen@sidn.nl>
2021-07-20 09:29:12 +0200

    Also report an error for dismissed events

--- lib/src/data/irma_bridge.dart
+++ lib/src/data/irma_bridge.dart
@@ -12,13 +12,15 @@ abstract class IrmaBridge {
   Stream<Event> get events => _eventSubject.stream;
 
   /// Adds a new event to the bridge's event subject. Events are not buffered. So, when no observer is listening,
-  /// error events are reported to Sentry and all other events are dismissed.
+  /// events are dismissed and the issue is reported to Sentry.
   @protected
   void addEvent(Event event) {
     if (_eventSubject.hasListener) {
       _eventSubject.add(event);
     } else if (event is ErrorEvent) {
       reportError(event.exception, event.stack);
+    } else {
+      reportError('Unhandled bridge event: ${event.runtimeType}', null);
     }
   }
 
