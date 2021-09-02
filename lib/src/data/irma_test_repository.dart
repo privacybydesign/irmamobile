96e5f1153351c764772598b8db1143f46fbb5234
Ivar Derksen <ivar.derksen@sidn.nl>
2021-09-02 11:39:38 +0200

    Move logic and util code of integration tests to other files

---
+++ lib/src/data/irma_test_repository.dart
@@ -0,0 +1,49 @@
+import 'package:irmamobile/src/models/clear_all_data_event.dart';
+
+import 'irma_client_bridge.dart';
+import 'irma_preferences.dart';
+import 'irma_repository.dart';
+
+class IrmaTestRepository {
+  static IrmaTestRepository _instance;
+
+  final IrmaRepository inner;
+  final IrmaPreferences preferences;
+
+  IrmaTestRepository._(this.inner, this.preferences);
+
+  static Future<IrmaTestRepository> ensureInitialized() async {
+    if (_instance == null) {
+      final repo = IrmaRepository(client: IrmaClientBridge());
+      final pref = IrmaPreferences.get();
+
+      // Wait until the bridge responds as an indication that the bridge is fully active.
+      await repo.bridge.events.first;
+
+      // The scheme's KeyshareAttribute field is not fully accurate, so
+      // we do a educated guess for additional myIRMA credentials.
+      final config = await repo.getIrmaConfiguration().first;
+      config.credentialTypes.forEach((_, cred) {
+        final schemeManager = config.schemeManagers[cred.schemeManagerId];
+        final keyshareAttr = schemeManager.keyshareAttribute?.split('.') ?? [];
+        final keyshareCred = keyshareAttr.length == 4 ? keyshareAttr.sublist(0, 3).join('.') : '';
+        if (cred.fullId == keyshareCred || !schemeManager.demo && cred.disallowDelete) {
+          repo.addMyIrmaCredential(cred);
+        }
+      });
+
+      _instance = IrmaTestRepository._(repo, pref);
+    }
+    return _instance;
+  }
+
+  Future<void> init() async {
+    await preferences.setAcceptedRootedRisk(true);
+    inner.setDeveloperMode(true);
+  }
+
+  Future<void> clean() async {
+    inner.dispatch(ClearAllDataEvent(), isBridgedEvent: true);
+    await inner.getEvents().firstWhere((event) => event is! ClearAllDataEvent);
+  }
+}
--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -4,10 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:irmamobile/main.dart';
-import 'package:irmamobile/src/data/irma_repository.dart';
+import 'package:irmamobile/src/data/irma_test_repository.dart';
 import 'package:irmamobile/src/widgets/irma_button.dart';
 
-import 'app.dart';
+import 'util.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
@@ -15,8 +15,10 @@ void main() {
 
   group('IrmaMobile', () {
     // Initialize the app's repository for integration tests (enable developer mode, etc.)
-    setUpAll(setUpRepository);
-    tearDown(cleanRepository);
+    IrmaTestRepository testRepo;
+    setUpAll(() async => testRepo = await IrmaTestRepository.ensureInitialized());
+    setUp(() => testRepo.init());
+    tearDown(() => testRepo.clean());
 
     testWidgets('irma-screens-tc1', (tester) async {
       await tester.pumpWidgetAndSettle(IrmaApp());
@@ -555,7 +557,7 @@ void main() {
       await tester.waitFor(find.byKey(const Key('wallet_present')));
 
       // Start session
-      await IrmaRepository.get().startTestSession("""
+      await testRepo.inner.startTestSession("""
       {
         "@context": "https://irma.app/ld/request/issuance/v2",
         "credentials": [
@@ -719,56 +721,4 @@ void main() {
       await tester.scrollAndCheckText('wallet_card_1', 'attr_4_value', 'Arnhem');
     }, timeout: const Timeout(Duration(minutes: 4)));
   });
-}
-
-// TODO: Move this extension into lib/src
-extension WidgetTesterUtil on WidgetTester {
-  /// Renders the given widget and waits until it settles.
-  Future<void> pumpWidgetAndSettle(Widget w) async {
-    await pumpWidget(w);
-    await pumpAndSettle();
-  }
-
-  // TODO: Find element that currently has focus to test autofocus
-  /// Enters the given text in the EditableText that currently is in focus.
-  Future<void> enterTextAtFocusedAndSettle(String text) async {
-    await enterText(find.byType(EditableText), text);
-    await pumpAndSettle(const Duration(milliseconds: 500));
-  }
-
-  /// Taps on the element the given widget, waits for a response, triggers a new frame sequence
-  /// to be rendered (check the description of pump) and waits until the widget settles.
-  /// The waiting time can be specified using 'duration'.
-  Future<void> tapAndSettle(Finder f, {Duration duration = const Duration(milliseconds: 100)}) async {
-    await tap(f);
-    await pumpAndSettle(duration);
-  }
-
-  /// Waits for the given widget to appear. When the timeout passes, an exception is given.
-  Future<void> waitFor(Finder f, {Duration timeout = const Duration(minutes: 1)}) => Future.doWhile(() async {
-        await pumpAndSettle();
-        return !any(f);
-      }).timeout(timeout);
-
-  /// Returns the string being inside the given Text widget.
-  /// When 'firstMatchOnly' is true, it also checks descendants of the given widget for text being present.
-  /// Only the first match is returned.
-  String getText(Finder f, {bool firstMatchOnly = false}) => firstMatchOnly
-      ? firstWidget<Text>(find.descendant(of: f, matching: find.byType(Text), matchRoot: true)).data
-      : widget<Text>(f).data;
-
-  /// Looks for a Scrollable inside a widget with Key 'parentKey', scrolls through all items
-  /// to look for a Text widget with Key 'textKey' and checks whether its value equals to 'textValue'.
-  Future<void> scrollAndCheckText(String parentKey, String textKey, String textValue) async {
-    final parentWidget = find.byKey(Key(parentKey));
-    final textWidget = find.descendant(of: parentWidget, matching: find.byKey(Key(textKey)));
-    await scrollUntilVisible(textWidget, 30,
-        scrollable: find.descendant(
-          of: parentWidget,
-          matching: find.byWidgetPredicate((widget) => widget is Scrollable),
-          matchRoot: true,
-        ));
-    final string = getText(textWidget);
-    expect(string, textValue);
-  }
 }
