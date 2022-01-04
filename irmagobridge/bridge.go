73fa0863e45e60592696941531ed672f7fe82844
Ivar Derksen <ivar.derksen@sidn.nl>
2022-01-04 18:18:50 +0100

    Feat: require secure cipher suite to be used for TLS connections

--- irmagobridge/bridge.go
+++ irmagobridge/bridge.go
@@ -103,6 +103,15 @@ func Start(givenBridge IrmaMobileBridge, appDataPath string, assetsPath string) 
 	}
 	irma.SetTLSClientConfig(&tls.Config{
 		RootCAs: rootCAs,
+		CipherSuites: []uint16{
+			// TLS 1.3 cipher suites are always accepted, so we only specify the secure cipher suites for <= TLS 1.2.
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+		},
 	})
 
 	// Check for user data directory, and create version-specific directory
