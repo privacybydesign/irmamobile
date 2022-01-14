d0789af5954db02db1eff4819dda272942534ef0
Sietse Ringers <mail@sietseringers.net>
2022-01-14 13:39:15 +0100

    fix: remove double enabling of privacyscreen from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_privacy_screen/flutter_privacy_screen.dart';
 import 'package:irmamobile/app.dart';
 import 'package:irmamobile/src/data/irma_client_bridge.dart';
-import 'package:irmamobile/src/data/irma_preferences.dart';
 import 'package:irmamobile/src/data/irma_repository.dart';
 import 'package:irmamobile/src/sentry/sentry.dart';
 import 'package:irmamobile/src/widgets/credential_nudge.dart';
@@ -21,10 +19,6 @@ Future<void> main() async {
     WidgetsFlutterBinding.ensureInitialized();
     await initSentry();
     IrmaRepository(client: IrmaClientBridge());
-
-    if (!(await IrmaPreferences.get().getScreenshotsEnabled().first)) {
-      await FlutterPrivacyScreen.enablePrivacyScreen();
-    }
 
     runApp(const IrmaApp());
   }, (error, stackTrace) => reportError(error, stackTrace));
