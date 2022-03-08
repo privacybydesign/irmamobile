ac0a56f375bf2eedfe4234c47ab143de7d4c1478
Sjoerd Lemkens <sjoerd.lemkens@sidn.nl>
2022-03-08 14:51:48 +0100

    Format

--- lib/src/screens/home/home_screen.dart
+++ lib/src/screens/home/home_screen.dart
@@ -6,8 +6,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      bottomNavigationBar: IrmaNavBar()
-    );
+    return const Scaffold(bottomNavigationBar: IrmaNavBar());
   }
 }
