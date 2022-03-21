a132c649ab196fca488fe7dad95a797869b36c6a
Sjoerd Lemkens <sjoerd.lemkens@sidn.nl>
2022-03-21 12:41:45 +0100

    Fix: Made scanner landscape compatible

--- lib/src/screens/scanner/widgets/qr_view_container.dart
+++ lib/src/screens/scanner/widgets/qr_view_container.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:native_device_orientation/native_device_orientation.dart';
 
 class QRViewContainer extends StatefulWidget {
   final Function(String) onFound;
@@ -15,12 +17,44 @@ class _QRViewContainerState extends State<QRViewContainer> {
   final _controller = MobileScannerController(formats: [BarcodeFormat.qrCode]);
 
   @override
-  Widget build(BuildContext context) => MobileScanner(
-      key: _qrKey,
-      controller: _controller,
-      onDetect: (barcode, args) {
-        if (barcode.rawValue != null) {
-          widget.onFound(barcode.rawValue!);
-        }
-      });
+  Widget build(BuildContext context) =>
+      // TODO: The mobile scanner package does not support landscape mode out of the box.(mobile_scanner: 1.0.0)
+      // The creator seems to be working on it.
+      // https://github.com/juliansteenbakker/mobile_scanner/discussions/17
+      // When this is supported remove orientation checks.
+      NativeDeviceOrientationReader(
+        builder: (context) {
+          final orientation = NativeDeviceOrientationReader.orientation(context);
+          late int quarterTurns;
+          switch (orientation) {
+            case NativeDeviceOrientation.landscapeLeft:
+              quarterTurns = 3;
+              break;
+            case NativeDeviceOrientation.portraitDown:
+              quarterTurns = 2;
+              break;
+            case NativeDeviceOrientation.landscapeRight:
+              quarterTurns = 1;
+              break;
+            case NativeDeviceOrientation.portraitUp:
+            case NativeDeviceOrientation.unknown:
+              quarterTurns = 0;
+              break;
+          }
+          return RotatedBox(
+            quarterTurns: quarterTurns,
+            child: SizedBox(
+              height: double.infinity,
+              child: MobileScanner(
+                  key: _qrKey,
+                  controller: _controller,
+                  onDetect: (barcode, args) {
+                    if (barcode.rawValue != null) {
+                      widget.onFound(barcode.rawValue!);
+                    }
+                  }),
+            ),
+          );
+        },
+      );
 }
