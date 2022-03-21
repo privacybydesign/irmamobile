efb10ad890d66afd52b246c75a1b0436fa428f36
Sjoerd Lemkens <sjoerd.lemkens@sidn.nl>
2022-03-21 09:05:14 +0100

    Improvement: Replaced deprecated scanner with a newer one

--- lib/src/screens/scanner/widgets/qr_view_container.dart
+++ lib/src/screens/scanner/widgets/qr_view_container.dart
@@ -1,7 +1,5 @@
-import 'dart:async';
-
 import 'package:flutter/cupertino.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 class QRViewContainer extends StatefulWidget {
   final Function(String) onFound;
@@ -14,37 +12,15 @@ class QRViewContainer extends StatefulWidget {
 
 class _QRViewContainerState extends State<QRViewContainer> {
   final GlobalKey _qrKey = GlobalKey(debugLabel: 'QR');
-
-  late QRViewController _qrViewController;
-  late StreamSubscription _qrViewSubscription;
+  final _controller = MobileScannerController(formats: [BarcodeFormat.qrCode]);
 
   @override
-  void dispose() {
-    _qrViewSubscription.cancel();
-    // TODO: Test this in UX-2.0
-    // Due to an issue in the qr code scanner library, the camera is not always
-    // disabled properly on iOS. Therefore we pause it manually for now.
-    // https://github.com/juliuscanute/qr_code_scanner/issues/137
-    // TODO: Is this still necessary? (check CHANGELOG qr_code_scanner 0.0.14)
-    // if (Platform.isIOS) {
-    //   _qrViewController.pauseCamera();
-    // }
-    _qrViewController.dispose();
-    super.dispose();
-  }
-
-  void _onQRViewCreated(QRViewController controller) {
-    _qrViewController = controller;
-    _qrViewSubscription = controller.scannedDataStream.listen((qr) {
-      if (qr.code != null) {
-        widget.onFound(qr.code!);
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) => QRView(
-        key: _qrKey,
-        onQRViewCreated: _onQRViewCreated,
-      );
+  Widget build(BuildContext context) => MobileScanner(
+      key: _qrKey,
+      controller: _controller,
+      onDetect: (barcode, args) {
+        if (barcode.rawValue != null) {
+          widget.onFound(barcode.rawValue!);
+        }
+      });
 }
