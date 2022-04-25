af5ddd3d9c2ff48324ce614bdf790af6f01c609f
Sjoerd Lemkens <sjoerd.lemkens@sidn.nl>
2022-04-25 15:02:13 +0200

    Feat: Show attributes that don't have a null value in template credential card attribute list

--- lib/src/screens/session/disclosure/widgets/irma_template_credential_card.dart
+++ lib/src/screens/session/disclosure/widgets/irma_template_credential_card.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../../../../theme/theme.dart';
 import '../../../../util/language.dart';
+import '../../../../widgets/credential_card/irma_credential_card_attribute_list.dart';
 import '../../../../widgets/credential_card/irma_credential_card_header.dart';
+import '../../../../widgets/dotted_divider.dart';
 import '../../../../widgets/irma_card.dart';
 import '../../../../widgets/irma_repository_provider.dart';
 import '../../models/template_disclosure_credential.dart';
@@ -24,7 +27,13 @@ class IrmaCredentialTemplateCard extends StatelessWidget {
             type: credential.obtained ? CredentialHeaderType.success : CredentialHeaderType.template,
             title: getTranslation(context, credential.credentialType.name),
             subtitle: getTranslation(context, credential.issuer.name),
-          )
+          ),
+          if (credential.attributesWithValue.isNotEmpty) ...[
+            const DottedDivider(),
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: IrmaTheme.of(context).largeSpacing),
+                child: IrmaCredentialCardAttributeList(credential.attributes))
+          ]
         ],
       ),
     );
