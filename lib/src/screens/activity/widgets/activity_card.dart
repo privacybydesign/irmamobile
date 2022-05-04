328e0f6b62b1585173ea86cbaacc8e7da0c6699f
Sjoerd Lemkens <sjoerd.lemkens@sidn.nl>
2022-05-04 09:35:17 +0200

    Refactor: Changed avatar shape to circle, plus minor refactor

--- lib/src/screens/activity/widgets/activity_card.dart
+++ lib/src/screens/activity/widgets/activity_card.dart
@@ -16,7 +16,10 @@ class ActivityCard extends StatelessWidget {
   final LogEntry logEntry;
   final IrmaConfiguration irmaConfiguration;
 
-  const ActivityCard({required this.logEntry, required this.irmaConfiguration});
+  const ActivityCard({
+    required this.logEntry,
+    required this.irmaConfiguration,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -47,73 +50,75 @@ class ActivityCard extends StatelessWidget {
         subtitleTranslationKey =
             logEntry.type == LogEntryType.issuing ? 'activity.data_received' : 'activity.data_deleted';
         logoFile = File(
-          Credential.fromRaw(irmaConfiguration: irmaConfiguration, rawCredential: logEntry.issuedCredentials.first)
-              .info
-              .credentialType
-              .logo!,
+          Credential.fromRaw(
+            irmaConfiguration: irmaConfiguration,
+            rawCredential: logEntry.issuedCredentials.first,
+          ).info.credentialType.logo!,
         );
     }
 
     return IrmaCard(
       onTap: () => Navigator.of(context).push(
         MaterialPageRoute(
-            builder: (context) => ActivityDetailScreen(
-                  logEntry: logEntry,
-                  irmaConfiguration: irmaConfiguration,
-                )),
-      ),
-      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-        Flexible(
-          child: Row(
-            children: [
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: theme.tinySpacing),
-                decoration: BoxDecoration(
-                  color: Colors.grey.shade100,
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(8),
-                  ),
-                ),
-                child: Container(
-                  padding: EdgeInsets.all(theme.smallSpacing),
-                  height: 48,
-                  width: 48,
-                  child: logoFile.existsSync()
-                      ? SizedBox(height: 24, child: Image.file(logoFile, excludeFromSemantics: true))
-                      : null,
-                ),
-              ),
-              Flexible(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      title,
-                      overflow: TextOverflow.ellipsis,
-                      style: theme.themeData.textTheme.caption!.copyWith(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      formatDate(logEntry.time, lang),
-                      style: theme.themeData.textTheme.bodyText2!.copyWith(fontSize: 12),
-                    ),
-                    TranslatedText(
-                      subtitleTranslationKey,
-                      style: theme.themeData.textTheme.caption,
-                    )
-                  ],
-                ),
-              )
-            ],
+          builder: (context) => ActivityDetailScreen(
+            logEntry: logEntry,
+            irmaConfiguration: irmaConfiguration,
           ),
         ),
-        Icon(
-          Icons.chevron_right,
-          color: Colors.grey.shade700,
-        ),
-      ]),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Flexible(
+            child: Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: theme.tinySpacing),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade100,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Container(
+                    padding: EdgeInsets.all(theme.smallSpacing),
+                    height: 48,
+                    width: 48,
+                    child: logoFile.existsSync()
+                        ? SizedBox(height: 24, child: Image.file(logoFile, excludeFromSemantics: true))
+                        : null,
+                  ),
+                ),
+                Flexible(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        title,
+                        overflow: TextOverflow.ellipsis,
+                        style: theme.themeData.textTheme.caption!.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        formatDate(logEntry.time, lang),
+                        style: theme.themeData.textTheme.bodyText2!.copyWith(fontSize: 12),
+                      ),
+                      TranslatedText(
+                        subtitleTranslationKey,
+                        style: theme.themeData.textTheme.caption,
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Icon(
+            Icons.chevron_right,
+            color: Colors.grey.shade700,
+          ),
+        ],
+      ),
     );
   }
 }
