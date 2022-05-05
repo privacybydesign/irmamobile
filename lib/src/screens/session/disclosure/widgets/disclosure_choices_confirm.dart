15855b4245502a4806ee980f11e99cb61e69e9e4
Sjoerd Lemkens <sjoerd.lemkens@sidn.nl>
2022-05-05 13:44:42 +0200

    Feat: Added signed message to disclosure confirm

--- lib/src/screens/session/disclosure/widgets/disclosure_choices_confirm.dart
+++ lib/src/screens/session/disclosure/widgets/disclosure_choices_confirm.dart
@@ -4,6 +4,7 @@ import 'package:flutter_i18n/flutter_i18n.dart';
 import '../../../../models/session.dart';
 import '../../../../theme/theme.dart';
 import '../../../../widgets/credential_card/irma_credentials_card.dart';
+import '../../../../widgets/irma_quote.dart';
 import '../../../../widgets/translated_text.dart';
 import '../../../activity/widgets/issuer_verifier_header.dart';
 import '../bloc/disclosure_permission_event.dart';
@@ -29,17 +30,28 @@ class DisclosureChoicesConfirm extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         IssuerVerifierHeader(title: requestor.name.translate(lang)),
-        SizedBox(height: theme.defaultSpacing),
-        TranslatedText(
-          'disclosure_permission.confirm.share',
-          style: theme.themeData.textTheme.headline3,
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: theme.defaultSpacing),
+          child: TranslatedText(
+            'disclosure_permission.confirm.share',
+            style: theme.themeData.textTheme.headline3,
+          ),
         ),
-        SizedBox(height: theme.defaultSpacing),
         IrmaCredentialsCard(
           attributesByCredential: {
             for (var cred in state.currentSelection) cred: cred.attributes,
           },
-        )
+        ),
+        if (state.isSignatureSession) ...[
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: theme.defaultSpacing),
+            child: TranslatedText(
+              'disclosure_permission.confirm.sign',
+              style: theme.themeData.textTheme.headline3,
+            ),
+          ),
+          IrmaQuote(quote: state.signedMessage),
+        ]
       ],
     );
   }
