8675dd84d7189a5cebc0cdc97f7fca88d3377358
Sjoerd Lemkens <sjoerd.lemkens@sidn.nl>
2022-05-12 12:59:50 +0200

    Chore: Made SessionScreen null safe

--- lib/src/screens/session/session.dart
+++ lib/src/screens/session/session.dart
@@ -1,18 +1,15 @@
-// This code is not null safe yet.
-// @dart=2.11
-
 class SessionScreenArguments {
   final int sessionID;
   final String sessionType;
   final bool hasUnderlyingSession;
   final bool wizardActive;
-  final String wizardCred;
+  final String? wizardCred;
 
   SessionScreenArguments({
-    this.sessionID,
-    this.sessionType,
-    this.hasUnderlyingSession,
-    this.wizardActive,
+    required this.sessionID,
+    required this.sessionType,
+    required this.hasUnderlyingSession,
+    required this.wizardActive,
     this.wizardCred,
   });
 }
