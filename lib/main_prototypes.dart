4fb56e27968dc5e3d3f3b717b79afb5424ea3dbf
Sjoerd Lemkens <sjoerd.lemkens@sidn.nl>
2022-05-17 15:34:20 +0200

    Feat: Added prototypes app again to demo the arrow back screen

---
+++ lib/main_prototypes.dart
@@ -0,0 +1,50 @@
+// This code is not null safe yet.
+// @dart=2.11
+
+import 'package:flutter/material.dart';
+import 'package:irmamobile/app.dart';
+import 'package:irmamobile/src/data/irma_mock_bridge.dart';
+import 'package:irmamobile/src/data/irma_preferences.dart';
+import 'package:irmamobile/src/data/irma_repository.dart';
+import 'package:irmamobile/src/prototypes/prototypes_screen.dart';
+import 'package:irmamobile/src/theme/theme.dart';
+import 'package:irmamobile/src/widgets/irma_repository_provider.dart';
+
+Future<void> main() async {
+      WidgetsFlutterBinding.ensureInitialized();
+  final repository = IrmaRepository(
+    client: IrmaMockBridge(),
+    preferences: await IrmaPreferences.fromInstance(),
+  );
+
+  runApp(PrototypesApp(repository: repository));
+}
+
+class PrototypesApp extends StatelessWidget {
+  final Map<String, WidgetBuilder> routes = {
+    PrototypesScreen.routeName: (BuildContext context) => PrototypesScreen(),
+  };
+
+  final IrmaRepository repository;
+
+  PrototypesApp({this.repository});
+
+  @override
+  Widget build(BuildContext context) => IrmaRepositoryProvider(
+        repository: repository,
+        child: IrmaTheme(
+          builder: (context) {
+            return MaterialApp(
+              key: const Key('app'),
+              title: 'IRMA',
+              theme: IrmaTheme.of(context).themeData,
+              localizationsDelegates: AppState.defaultLocalizationsDelegates(),
+              supportedLocales: AppState.defaultSupportedLocales(),
+              locale: const Locale('nl', 'NL'),
+              initialRoute: PrototypesScreen.routeName,
+              routes: routes,
+            );
+          },
+        ),
+      );
+}
---
+++ lib/src/prototypes/prototypes_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import '../screens/session/widgets/arrow_back_screen.dart';
+
+class PrototypesScreen extends StatelessWidget {
+  static const routeName = "/";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          ListTile(
+            title: const Text("Arrow back screen"),
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const ArrowBack(
+                  amountIssued: 0,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
