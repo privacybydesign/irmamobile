7e74a692de6c1ec04145ab66c152ccfa1d94b1a5
Sjoerd Lemkens <sjoerd.lemkens@sidn.nl>
2022-05-23 16:09:27 +0200

    Feat: New styling of RequiredUpdateScreen

--- lib/src/screens/required_update/required_update_screen.dart
+++ lib/src/screens/required_update/required_update_screen.dart
@@ -2,30 +2,62 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
-import 'package:irmamobile/src/widgets/irma_bottom_bar.dart';
 import 'package:url_launcher/url_launcher.dart';
+
+import '../../theme/theme.dart';
+import '../../widgets/irma_bottom_bar.dart';
+import '../../widgets/translated_text.dart';
 
 class RequiredUpdateScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final theme = IrmaTheme.of(context);
+
     return Scaffold(
-      body: Center(
-        child: Text(FlutterI18n.translate(context, 'update.required')),
+      body: Padding(
+        padding: EdgeInsets.all(theme.largeSpacing),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.cached,
+              color: theme.themeData.colorScheme.primary,
+              size: 125,
+            ),
+            SizedBox(
+              height: theme.mediumSpacing,
+            ),
+            TranslatedText(
+              'update.title',
+              style: theme.textTheme.headline1,
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: theme.mediumSpacing,
+            ),
+            TranslatedText(
+              'update.explanation',
+              style: theme.textTheme.bodyText2,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: IrmaBottomBar(
-          primaryButtonLabel: FlutterI18n.translate(context, 'update.doupdate'),
-          onPrimaryPressed: () {
-            switch (Platform.operatingSystem) {
-              case "android":
-                launch("market://details?id=org.irmacard.cardemu");
-                break;
-              case "ios":
-                launch("itms://itunes.apple.com/us/app/apple-store/id1294092994?mt=8");
-                break;
-              default:
-                throw Exception("Unsupported Platfrom.operatingSystem");
-            }
-          }),
+        primaryButtonLabel: FlutterI18n.translate(context, 'update.update_app'),
+        onPrimaryPressed: () {
+          switch (Platform.operatingSystem) {
+            case "android":
+              launch("market://details?id=org.irmacard.cardemu");
+              break;
+            case "ios":
+              launch("itms://itunes.apple.com/us/app/apple-store/id1294092994?mt=8");
+              break;
+            default:
+              throw Exception("Unsupported Platfrom.operatingSystem");
+          }
+        },
+      ),
     );
   }
 }
