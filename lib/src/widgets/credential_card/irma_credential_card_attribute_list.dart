79cb0443ea76c88943ded8e4b2893478700e3cd4
Sjoerd Lemkens <sjoerd.lemkens@sidn.nl>
2022-05-25 08:06:39 +0200

    Feat: Changed orientation of credential card attribute list

--- lib/src/widgets/credential_card/irma_credential_card_attribute_list.dart
+++ lib/src/widgets/credential_card/irma_credential_card_attribute_list.dart
@@ -20,65 +20,61 @@ class IrmaCredentialCardAttributeList extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = IrmaTheme.of(context);
     final lang = FlutterI18n.currentLocale(context)!.languageCode;
+    final attributesWithValue = attributes.where((att) => att.value is! NullValue);
 
-    return Column(
+    return Row(
       children: [
-        for (final attribute in attributes.where((att) => att.value is! NullValue))
-          Padding(
-            padding: EdgeInsets.only(bottom: theme.tinySpacing),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Flexible(
-                  child: Text(
-                    attribute.attributeType.name.translate(lang),
-                    textAlign: TextAlign.start,
-                    style: theme.themeData.textTheme.caption,
-                  ),
-                ),
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            for (final attribute in attributesWithValue) ...[
+              Text(
+                attribute.attributeType.name.translate(lang),
+                style: theme.themeData.textTheme.caption,
+              ),
+              SizedBox(width: theme.smallSpacing),
+              Builder(
+                builder: (context) {
+                  if (attribute.value is PhotoValue) {
+                    //If attribute is photo show link
+                    return GestureDetector(
+                      onTap: () {
+                        //TODO Implement open Photo.
+                      },
+                      child: TranslatedText(
+                        //TODO: Add translation
+                        'Image',
+                        textAlign: TextAlign.start,
+                        style: theme.textTheme.bodyText2!.copyWith(decoration: TextDecoration.underline),
+                      ),
+                    );
+                  } else if (attribute.value is TextValue) {
+                    final Attribute? compareValue =
+                        compareToAttributes?.firstWhereOrNull((e) => e.attributeType.id == attribute.attributeType.id);
+                    return TranslatedText(
+                      (attribute.value as TextValue).translated.translate(lang),
+                      style: theme.themeData.textTheme.bodyText1!.copyWith(
+                        color: compareValue == null || compareValue.value is NullValue
+                            ? Colors.black
+                            : attribute.value.raw == compareValue.value.raw
+                                ? Colors.green
+                                : Colors.red,
+                      ),
+                    );
+                  }
+                  //If value is null or default return empty container
+                  return Container();
+                },
+              ),
+              //If this is not the last item add some padding
+              if (attributesWithValue.last != attribute)
                 SizedBox(
-                  width: theme.smallSpacing,
-                ),
-                Flexible(
-                  child: Builder(
-                    builder: (context) {
-                      if (attribute.value is PhotoValue) {
-                        //If attribute is photo show link
-                        return GestureDetector(
-                          onTap: () {
-                            //TODO Implement open Photo.
-                          },
-                          child: TranslatedText(
-                            //TODO: Add translation
-                            'Image',
-                            textAlign: TextAlign.start,
-                            style: theme.textTheme.bodyText2!.copyWith(decoration: TextDecoration.underline),
-                          ),
-                        );
-                      } else if (attribute.value is TextValue) {
-                        final Attribute? compareValue = compareToAttributes
-                            ?.firstWhereOrNull((e) => e.attributeType.id == attribute.attributeType.id);
-                        return TranslatedText(
-                          (attribute.value as TextValue).translated.translate(lang),
-                          textAlign: TextAlign.end,
-                          style: theme.themeData.textTheme.caption!.copyWith(
-                            fontWeight: FontWeight.bold,
-                            color: compareValue == null || compareValue.value is NullValue
-                                ? Colors.grey.shade700
-                                : attribute.value.raw == compareValue.value.raw
-                                    ? Colors.green
-                                    : Colors.red,
-                          ),
-                        );
-                      }
-                      //If value is null or default return empty container
-                      return Container();
-                    },
-                  ),
-                ),
-              ],
-            ),
-          )
+                  height: theme.smallSpacing,
+                )
+            ]
+          ],
+        )
       ],
     );
   }
