0e0db1f330922b2dff7bf69af56357aac9709a9a
Sjoerd Lemkens <sjoerd.lemkens@sidn.nl>
2022-06-08 10:50:08 +0200

    Feat: Added RichText support on IrmaQuote

--- lib/src/widgets/irma_quote.dart
+++ lib/src/widgets/irma_quote.dart
@@ -3,26 +3,40 @@ import 'package:irmamobile/src/theme/theme.dart';
 
 class IrmaQuote extends StatelessWidget {
   final String? quote;
+  final RichText? richQuote;
+  final Color? color;
 
-  const IrmaQuote({required this.quote});
+  const IrmaQuote({
+    this.quote,
+    this.richQuote,
+    this.color,
+  })  : assert(
+          quote != null || richQuote != null,
+          "No quote given. Set quote or richQuote.",
+        ),
+        assert(
+          quote == null || richQuote == null,
+          "Either set quote or richQuote, not both.",
+        );
 
   @override
   Widget build(BuildContext context) {
     final theme = IrmaTheme.of(context);
 
     return Container(
-      constraints: const BoxConstraints(maxWidth: 400),
-      color: theme.lightBlue,
+      color: color ?? theme.lightBlue,
       child: Padding(
         padding: EdgeInsets.all(theme.defaultSpacing),
         child: Row(
           children: [
-            if (quote != null)
-              Flexible(
-                  child: Text(
-                quote!,
-                style: theme.themeData.textTheme.caption,
-              ))
+            Flexible(
+              child: quote != null
+                  ? Text(
+                      quote!,
+                      style: theme.themeData.textTheme.caption,
+                    )
+                  : richQuote!,
+            )
           ],
         ),
       ),
