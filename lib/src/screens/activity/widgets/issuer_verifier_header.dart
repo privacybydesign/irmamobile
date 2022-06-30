7d29a6c197db3050a2c6156802efc4d54730246b
Sjoerd Lemkens <sjoerd.lemkens@sidn.nl>
2022-06-30 12:52:13 +0200

    Fix: Fixed overflow issue with long verifier names

--- lib/src/screens/activity/widgets/issuer_verifier_header.dart
+++ lib/src/screens/activity/widgets/issuer_verifier_header.dart
@@ -1,23 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:irmamobile/src/theme/theme.dart';
+
+import '../../../theme/theme.dart';
 
 class IssuerVerifierHeader extends StatelessWidget {
   final String title;
   final String? logo;
 
-  const IssuerVerifierHeader({required this.title, this.logo});
+  const IssuerVerifierHeader({
+    required this.title,
+    this.logo,
+  });
 
   @override
   Widget build(BuildContext context) {
+    final theme = IrmaTheme.of(context);
+
     return Row(
       children: [
-        CircleAvatar(backgroundColor: Colors.grey.shade300, radius: 24),
-        SizedBox(
-          width: IrmaTheme.of(context).smallSpacing,
+        CircleAvatar(
+          backgroundColor: Colors.grey.shade300,
+          radius: 24,
         ),
-        Text(
-          title,
-          style: IrmaTheme.of(context).textTheme.bodyText1,
+        SizedBox(
+          width: theme.smallSpacing,
+        ),
+        Flexible(
+          child: Text(
+            title,
+            style: theme.textTheme.bodyText1,
+            overflow: TextOverflow.ellipsis,
+          ),
         ),
       ],
     );
