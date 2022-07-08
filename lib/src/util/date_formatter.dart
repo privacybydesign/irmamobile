592914ebf8863b8b75e404baeaedde5ec6b662e8
Sjoerd Lemkens <sjoerd.lemkens@sidn.nl>
2022-07-08 16:34:58 +0200

    Refactor: Removed old date formatter

--- lib/src/util/date_formatter.dart
+++ lib/src/util/date_formatter.dart
@@ -1,9 +1,5 @@
 import 'package:intl/intl.dart';
 
-String formatDate(DateTime date, String lang) {
-  return DateFormat.yMMMMd(lang).addPattern(" - ").add_jm().format(date);
-}
-
 String printableDate(DateTime date, String lang) {
   return DateFormat.yMMMMd(lang).format(date);
 }
