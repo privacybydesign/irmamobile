bc4bd73c981ffb24546bc675a31f30f0fb5f1e35
Jasm Sison <jasm.sison@sidn.nl>
2022-07-15 12:26:05 +0200

    * Remove print statement * Adjust comment

--- lib/src/util/tablet.dart
+++ lib/src/util/tablet.dart
@@ -1,13 +1,9 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 
 extension Tablet on BuildContext {
-  // 600 a common breakpoint for a typical 7-inch tablet.
+  // 600 is a common breakpoint for a typical 7-inch tablet
   bool get isTabletDevice {
     var shortestSide = MediaQuery.of(this).size.shortestSide;
-    if (kDebugMode) {
-      print('shortest edge: $shortestSide');
-    }
     return shortestSide > 600;
   }
 }
