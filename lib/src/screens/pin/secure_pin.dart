41e92b1a61a62856111d1eee8894caf6a5e8c1f6
Jasm Sison <jasm.sison@sidn.nl>
2022-07-19 16:59:44 +0200

    use Pin typedef

--- lib/src/screens/pin/secure_pin.dart
+++ lib/src/screens/pin/secure_pin.dart
@@ -1,7 +1,7 @@
 part of pin;
 
 // aaaaa ababa, every permutation of abbbb
-bool _pinMustContainAtLeastThreeUniqueNumbers(List<int> pin) {
+bool _pinMustContainAtLeastThreeUniqueNumbers(Pin pin) {
   final counter = List.filled(10, 0, growable: false);
   for (final e in pin) {
     counter[e] = 1;
@@ -11,10 +11,10 @@ bool _pinMustContainAtLeastThreeUniqueNumbers(List<int> pin) {
 
 // abcba, abcab
 // n = 5
-bool _pinMustNotContainPatternAbcba(List<int> pin) => !(pin[0] == pin[4] && pin[1] == pin[3]);
-bool _pinMustNotContainPatternAbcab(List<int> pin) => !(pin[0] == pin[3] && pin[1] == pin[4]);
+bool _pinMustNotContainPatternAbcba(Pin pin) => !(pin[0] == pin[4] && pin[1] == pin[3]);
+bool _pinMustNotContainPatternAbcab(Pin pin) => !(pin[0] == pin[3] && pin[1] == pin[4]);
 
-bool Function(List<int>) sequenceChecker(int delta) => (List<int> pin) {
+bool Function(Pin) sequenceChecker(int delta) => (Pin pin) {
       bool tracker = true;
       for (var i = 0; i < pin.length - 1 && tracker; i++) {
         tracker &= (pin[i] + delta) % 10 == pin[i + 1];
@@ -22,7 +22,7 @@ bool Function(List<int>) sequenceChecker(int delta) => (List<int> pin) {
       return tracker;
     };
 
-bool _pinMustNotBeMemberOfSeriesAscDesc(List<int> pin) {
+bool _pinMustNotBeMemberOfSeriesAscDesc(Pin pin) {
   final isAsc = sequenceChecker(1);
   final isDesc = sequenceChecker(-1);
 
