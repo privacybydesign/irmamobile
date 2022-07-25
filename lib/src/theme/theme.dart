ede2e20f44ee583e83755aabb356cac751ba1794
Jasm Sison <jasm.sison@sidn.nl>
2022-07-25 14:59:57 +0200

    changed background color to design

--- lib/src/theme/theme.dart
+++ lib/src/theme/theme.dart
@@ -17,7 +17,7 @@ class IrmaThemeData {
   final Color secondary = const Color(0xFF363E6A); // Used for buttons and headlines
 
   // Background / contrast colors
-  final Color background = const Color(0xFFE5E5E5); //Used on scaffolds and scrollable backgrounds
+  final Color background = const Color(0xFFFAFAFA); //Used on scaffolds and scrollable backgrounds
   Color get surfacePrimary => light; // Used on cards etc, to contrast with the background
   final Color surfaceSecondary = const Color(0xFFE9F4FF); // Used on cards that are active etc.
   final Color dark = Colors.black; // Used as default, non headline, text color
