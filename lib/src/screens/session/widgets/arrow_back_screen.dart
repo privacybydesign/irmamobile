ee160e95db6c320ba17c410175a28f2eceefe055
Sjoerd Lemkens <sjoerd.lemkens@sidn.nl>
2022-08-11 13:35:32 +0200

    feat: arrow back screen with new asset

--- lib/src/screens/session/widgets/arrow_back_screen.dart
+++ lib/src/screens/session/widgets/arrow_back_screen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:native_device_orientation/native_device_orientation.dart';
-import 'package:path_drawing/path_drawing.dart';
 
-import '../../../theme/theme.dart';
+import '../../../widgets/irma_info_scaffold_body.dart';
 
 class ArrowBack extends StatefulWidget {
   final bool success;
@@ -53,8 +51,6 @@ class _ArrowBackState extends State<ArrowBack> with WidgetsBindingObserver {
 
   @override
   Widget build(BuildContext context) {
-    final theme = IrmaTheme.of(context);
-
     // The NativeDeviceOrientationReader is configured to rebuild according to the gyroscope.
     // On the IOS emulator it is not possible to reproduce this, so this has to be tested on a real device.
     return NativeDeviceOrientationReader(
@@ -78,37 +74,14 @@ class _ArrowBackState extends State<ArrowBack> with WidgetsBindingObserver {
         }
 
         return Scaffold(
-          body: SafeArea(
-            child: CustomPaint(
-              painter: Arrow(context),
-              child: Center(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: theme.defaultSpacing),
-                  child: RotatedBox(
-                    quarterTurns: quarterTurns,
-                    child: RichText(
-                      textAlign: TextAlign.center,
-                      text: TextSpan(
-                        style: theme.textTheme.bodyText1,
-                        children: [
-                          TextSpan(
-                            text: widget.success
-                                ? FlutterI18n.plural(context, 'arrow_back.info_success', widget.amountIssued)
-                                : FlutterI18n.translate(context, 'arrow_back.info_no_success'),
-                          ),
-                          const TextSpan(
-                            text: '\n\n',
-                          ),
-                          TextSpan(
-                            text: FlutterI18n.translate(context, 'arrow_back.safari'),
-                            style: theme.textTheme.bodyText2,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
+          body: RotatedBox(
+            quarterTurns: quarterTurns,
+            child: IrmaInfoScaffoldBody(
+              imagePath: 'assets/arrow_back/pointing_up.svg',
+              titleTranslationKey: widget.success
+                  ? 'arrow_back.info_success-' + widget.amountIssued.toString()
+                  : 'arrow_back.info_no_success',
+              bodyTranslationKey: 'arrow_back.safari',
             ),
           ),
         );
@@ -122,52 +95,5 @@ class _ArrowBackState extends State<ArrowBack> with WidgetsBindingObserver {
     if (state == AppLifecycleState.resumed) {
       Navigator.of(context).removeRoute(ModalRoute.of(context)!);
     }
-  }
-}
-
-class Arrow extends CustomPainter {
-  BuildContext context;
-
-  Arrow(this.context);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint();
-
-    paint.color = IrmaTheme.of(context).secondary;
-    paint.style = PaintingStyle.stroke;
-    paint.strokeWidth = 3;
-    paint.strokeCap = StrokeCap.round;
-    paint.strokeJoin = StrokeJoin.round;
-
-    const start = Offset(23, 15);
-    const cP1 = Offset(15, 140);
-    final cP2 = Offset(size.width / 2, size.height / 10);
-    final end = Offset(size.width / 2, size.height / 2.6);
-    final line = Path();
-    line.moveTo(start.dx, start.dy);
-    line.cubicTo(cP1.dx, cP1.dy, cP2.dx, cP2.dy, end.dx, end.dy);
-
-    canvas.drawPath(
-      dashPath(
-        line,
-        dashArray: CircularIntervalList<double>(<double>[5.0, 6.0]),
-      ),
-      paint,
-    );
-
-    paint.style = PaintingStyle.fill;
-
-    final triangle = Path();
-    triangle.moveTo(23, 7);
-    triangle.lineTo(31, 17);
-    triangle.lineTo(15, 17);
-    triangle.close();
-    canvas.drawPath(triangle, paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
   }
 }
