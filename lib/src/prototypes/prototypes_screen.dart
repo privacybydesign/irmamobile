f3ca51fa32c271dd1e324549b56438d1460ed896
Jasm Sison <jasm.sison@sidn.nl>
2022-08-15 11:52:10 +0200

    fixed imports

--- lib/src/prototypes/prototypes_screen.dart
+++ lib/src/prototypes/prototypes_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../screens/session/widgets/pairing_required.dart';
 
 import '../screens/error/blocked_screen.dart';
 import '../screens/error/error_screen.dart';
@@ -10,6 +9,7 @@ import '../screens/required_update/required_update_screen.dart';
 import '../screens/rooted_warning/rooted_warning_screen.dart';
 import '../screens/session/widgets/arrow_back_screen.dart';
 import '../screens/session/widgets/disclosure_feedback_screen.dart';
+import '../screens/session/widgets/pairing_required.dart';
 import '../screens/splash_screen/splash_screen.dart';
 import '../widgets/irma_error_scaffold_body.dart';
 import 'prototype_pin_screen.dart';
