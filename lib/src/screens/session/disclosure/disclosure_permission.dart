b83ca5ea03a142df47768ad5bd223166e607a1ac
Sjoerd Lemkens <sjoerd.lemkens@sidn.nl>
2022-08-22 14:17:58 +0200

    fix: provided the onDismiss in the DisclosurePermission builder

--- lib/src/screens/session/disclosure/disclosure_permission.dart
+++ lib/src/screens/session/disclosure/disclosure_permission.dart
@@ -11,6 +11,7 @@ import 'bloc/disclosure_permission_bloc.dart';
 import 'bloc/disclosure_permission_event.dart';
 import 'bloc/disclosure_permission_state.dart';
 import 'widgets/disclosure_permission_choices_screen.dart';
+import 'widgets/disclosure_permission_close_dialog.dart';
 import 'widgets/disclosure_permission_introduction_screen.dart';
 import 'widgets/disclosure_permission_issue_wizard_screen.dart';
 import 'widgets/disclosure_permission_make_choice_screen.dart';
@@ -60,6 +61,7 @@ class ProvidedDisclosurePermission extends StatelessWidget {
   Widget build(BuildContext context) {
     final bloc = context.read<DisclosurePermissionBloc>();
     void addEvent(DisclosurePermissionBlocEvent event) => bloc.add(event);
+    void onDismiss() => DisclosurePermissionCloseDialog.show(context);
 
     // Wrap our widget in a custom navigator, such that popping this widget from the root navigator will include
     // popping the DisclosurePermissionWrongCredentialsAddedDialog.
@@ -95,12 +97,14 @@ class ProvidedDisclosurePermission extends StatelessWidget {
               if (state is DisclosurePermissionIntroduction) {
                 return DisclosurePermissionIntroductionScreen(
                   onEvent: addEvent,
+                  onDismiss: onDismiss,
                 );
               } else if (state is DisclosurePermissionIssueWizard) {
                 return DisclosurePermissionIssueWizardScreen(
                   requestor: requestor,
                   state: state,
                   onEvent: addEvent,
+                  onDismiss: onDismiss,
                 );
               } else if (state is DisclosurePermissionMakeChoice) {
                 return DisclosurePermissionMakeChoiceScreen(
@@ -117,6 +121,7 @@ class ProvidedDisclosurePermission extends StatelessWidget {
                   requestor: requestor,
                   state: state,
                   onEvent: addEvent,
+                  onDismiss: onDismiss,
                 );
               }
               // If state is loading/initial show centered loading indicator
