9d37453880dae49c1dab668386acb36b127d924e
Sjoerd Lemkens <sjoerd.lemkens@sidn.nl>
2022-08-25 12:49:09 +0200

    feat: updated icon on IrmaQrScanButton

--- lib/src/screens/home/widgets/irma_qr_scan_button.dart
+++ lib/src/screens/home/widgets/irma_qr_scan_button.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:irmamobile/src/screens/scanner/scanner_screen.dart';
-import 'package:irmamobile/src/theme/irma_icons.dart';
 import 'package:irmamobile/src/theme/theme.dart';
 import 'package:irmamobile/src/widgets/translated_text.dart';
 
@@ -22,20 +21,27 @@ class IrmaQrScanButton extends StatelessWidget {
             backgroundColor: theme.themeData.colorScheme.secondary,
             radius: 36,
             child: IconButton(
-                tooltip: FlutterI18n.translate(context, 'home.nav_bar.scan_qr'),
-                color: Colors.white,
-                onPressed: () {
-                  Navigator.pushNamed(context, ScannerScreen.routeName);
-                },
-                icon: const Icon(IrmaIcons.scanQrcode, size: 32)),
+              icon: const Icon(Icons.qr_code_scanner),
+              iconSize: 42,
+              color: Colors.white,
+              tooltip: FlutterI18n.translate(context, 'home.nav_bar.scan_qr'),
+              onPressed: () {
+                Navigator.pushNamed(context, ScannerScreen.routeName);
+              },
+            ),
           ),
           SizedBox(
             height: theme.tinySpacing,
           ),
           ExcludeSemantics(
-            child: TranslatedText('home.nav_bar.scan_qr',
-                textAlign: TextAlign.center,
-                style: theme.themeData.textTheme.caption!.copyWith(fontSize: 12, color: Colors.grey.shade600)),
+            child: TranslatedText(
+              'home.nav_bar.scan_qr',
+              textAlign: TextAlign.center,
+              style: theme.themeData.textTheme.caption!.copyWith(
+                fontSize: 12,
+                color: Colors.grey.shade600,
+              ),
+            ),
           ),
         ],
       ),
