ea1539558dbfff90231be36ee0553608acc4a6c4
Jasm Sison <jasm.sison@sidn.nl>
2022-09-08 19:36:46 +0200

    group images and add them below the card

--- lib/src/widgets/credential_card/irma_credential_card_attribute_list.dart
+++ lib/src/widgets/credential_card/irma_credential_card_attribute_list.dart
@@ -28,67 +28,100 @@ class IrmaCredentialCardAttributeList extends StatelessWidget {
                 attrComp.value is! NullValue && attr.attributeType.fullId == attrComp.attributeType.fullId) ??
             true));
 
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        for (final attribute in attributesWithValue) ...[
-          Text(
-            attribute.attributeType.name.translate(lang),
-            style: theme.themeData.textTheme.caption!.copyWith(
-              color: theme.neutralDark,
-            ),
+    Text buildLabel(Attribute attribute) => Text(
+          attribute.attributeType.name.translate(lang),
+          style: theme.themeData.textTheme.caption!.copyWith(
+            color: theme.neutralDark,
           ),
-          SizedBox(height: theme.tinySpacing),
-          Builder(
-            builder: (context) {
-              if (attribute.value is PhotoValue) {
-                final image = (attribute.value as PhotoValue).image;
-                //If attribute is photo show link
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Scaffold(
-                          appBar: IrmaAppBar(
-                            title: attribute.attributeType.name.translate(lang),
-                            leadingAction: Navigator.of(context).pop,
-                          ),
-                          body: SingleChildScrollView(
-                            child: Center(child: image),
-                          ),
+        );
+
+    TranslatedText buildTextContent(Attribute attribute) {
+      final Attribute? compareValue =
+          compareTo?.firstWhereOrNull((e) => e.attributeType.fullId == attribute.attributeType.fullId);
+      return TranslatedText(
+        (attribute.value as TextValue).translated.translate(lang),
+        style: theme.themeData.textTheme.bodyText1!.copyWith(
+          color: compareValue == null || compareValue.value is NullValue
+              ? theme.dark
+              : attribute.value.raw == compareValue.value.raw
+                  ? theme.success
+                  : theme.error,
+        ),
+      );
+    }
+
+    GestureDetector buildTappableImage(Attribute attribute) {
+      final image = (attribute.value as PhotoValue).image;
+      //If attribute is photo show link
+      return GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => Scaffold(
+                appBar: IrmaAppBar(
+                  title: attribute.attributeType.name.translate(lang),
+                  leadingAction: Navigator.of(context).pop,
+                ),
+                body: SingleChildScrollView(
+                  child: Center(child: image),
+                ),
+              ),
+            ),
+          );
+        },
+        child: ConstrainedBox(
+          child: image,
+          constraints: const BoxConstraints(
+            maxWidth: 66,
+            maxHeight: 100,
+          ),
+        ),
+      );
+    }
+
+    final photoValueAttrs = attributesWithValue.where((a) => a.value is PhotoValue);
+    final groupedByAttrTypeName = groupBy(photoValueAttrs.toList(), (Attribute a) => a.attributeType.name);
+
+    return LayoutBuilder(
+      builder: (context, constraints) => Wrap(
+        direction: Axis.vertical,
+        spacing: theme.smallSpacing,
+        children: [
+          ...(attributesWithValue.where((a) => a.value is TextValue)).map((attribute) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                buildLabel(attribute),
+                buildTextContent(attribute),
+              ],
+            );
+          }).toList(),
+          ...groupedByAttrTypeName.keys
+              .map((e) => Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: theme.defaultSpacing),
+                      Container(
+                        height: 1,
+                        width: constraints.maxWidth * 0.8,
+                        decoration: BoxDecoration(
+                          color: theme.neutralExtraLight,
                         ),
                       ),
-                    );
-                  },
-                  child: image,
-                );
-              } else if (attribute.value is TextValue) {
-                final Attribute? compareValue =
-                    compareTo?.firstWhereOrNull((e) => e.attributeType.fullId == attribute.attributeType.fullId);
-                return TranslatedText(
-                  (attribute.value as TextValue).translated.translate(lang),
-                  style: theme.themeData.textTheme.bodyText1!.copyWith(
-                    color: compareValue == null || compareValue.value is NullValue
-                        ? theme.dark
-                        : attribute.value.raw == compareValue.value.raw
-                            ? theme.success
-                            : theme.error,
-                  ),
-                );
-              }
-              //If value is null or default return empty container
-              return Container();
-            },
-          ),
-          //If this is not the last item add some padding
-          if (attributesWithValue.last != attribute)
-            SizedBox(
-              height: theme.smallSpacing,
-            )
+                      SizedBox(height: theme.defaultSpacing),
+                      buildLabel(groupedByAttrTypeName[e]![0]),
+                      SizedBox(height: theme.tinySpacing),
+                      Wrap(
+                        direction: Axis.horizontal,
+                        spacing: theme.tinySpacing,
+                        children: groupedByAttrTypeName[e]!.map((a) => buildTappableImage(a)).toList(),
+                      ),
+                    ],
+                  ))
+              .toList(),
         ],
-      ],
+      ),
     );
   }
 }
