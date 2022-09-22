add427843f4299f1613060e1729a1e929ef9a4d0
Sjoerd Lemkens <sjoerd.lemkens@sidn.nl>
2022-09-22 16:11:15 +0200

    test: added enroll_test to test_all again

--- integration_test/test_all.dart
+++ integration_test/test_all.dart
@@ -2,7 +2,7 @@
 // @dart=2.11
 
 // import 'activity_test.dart' as activity_test;
-// import 'enroll_test.dart' as enroll_test;
+import 'enroll_test.dart' as enroll_test;
 // import 'issuance_test.dart' as issuance_test;
 // import 'login_test.dart' as login_test;
 import 'more_tab_test.dart' as more_tab_test;
@@ -14,7 +14,7 @@ import 'home_test.dart' as home_test;
 /// Wrapper to execute all tests at once.
 void main() {
   // TODO: most tests are disabled because they do not work at the moment.
-  // enroll_test.main();
+  enroll_test.main();
   // screens_test.main();
   // login_test.main();
   settings_test.main();
