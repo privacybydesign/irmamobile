3d8797b72d76aecf88168b9535f78de7e088c986
Jasm Sison <jasm.sison@sidn.nl>
2022-09-27 12:14:49 +0200

    replace const color with theme neutral light

--- lib/src/screens/activity/widgets/title_initial_uppercased.dart
+++ lib/src/screens/activity/widgets/title_initial_uppercased.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 
+import '../../../theme/theme.dart';
+
 class TitleInitialUpperCased extends StatelessWidget {
   final String title;
   const TitleInitialUpperCased(this.title, {Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => FittedBox(
-        fit: BoxFit.fitHeight,
-        child: Text(
-          title[0].toUpperCase(),
-          textAlign: TextAlign.center,
-          style: const TextStyle(
-            color: Color(0xFF9A9EB4),
-            fontWeight: FontWeight.bold,
-          ),
+  Widget build(BuildContext context) {
+    final theme = IrmaTheme.of(context);
+    return FittedBox(
+      fit: BoxFit.fitHeight,
+      child: Text(
+        title[0].toUpperCase(),
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: theme.neutralLight,
+          fontWeight: FontWeight.bold,
         ),
-      );
+      ),
+    );
+  }
 }
