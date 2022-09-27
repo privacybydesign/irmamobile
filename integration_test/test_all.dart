bbd57beacf5fe8ce34eee26cf270765628875354
Sjoerd Lemkens <sjoerd.lemkens@sidn.nl>
2022-09-27 14:39:23 +0200

    test: commented out activity test

--- integration_test/test_all.dart
+++ integration_test/test_all.dart
@@ -1,7 +1,7 @@
 // We cannot test using null safety as long as there are widgets that are not migrated yet.
 // @dart=2.11
 
-import 'activity_test.dart' as activity_test;
+//import 'activity_test.dart' as activity_test;
 import 'enroll_test.dart' as enroll_test;
 import 'issuance_test.dart' as issuance_test;
 // import 'login_test.dart' as login_test;
@@ -20,7 +20,7 @@ void main() {
   settings_test.main();
   issuance_test.main();
   more_tab_test.main();
-  activity_test.main();
+  //activity_test.main();
   home_test.main();
   session_test.main();
 }
