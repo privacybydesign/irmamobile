4872ed3072b35176520654255818861e4994aec5
Sjoerd Lemkens <sjoerd.lemkens@sidn.nl>
2022-10-04 15:22:05 +0200

    test: updated tests to new attribute sorting

--- integration_test/issuance_test.dart
+++ integration_test/issuance_test.dart
@@ -6,8 +6,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:irmamobile/src/screens/data/credentials_detail_screen.dart';
 import 'package:irmamobile/src/widgets/credential_card/irma_credential_card.dart';
-import 'package:irmamobile/src/widgets/credential_card/irma_credential_card_attribute_list.dart';
-import 'package:irmamobile/src/widgets/credential_card/irma_credential_card_header.dart';
 
 import 'helpers.dart';
 import 'irma_binding.dart';
@@ -26,8 +24,6 @@ void main() {
     Future<void> testIssueMunicipality(
       WidgetTester tester,
       Locale locale,
-      List<String> expectedHeaderText,
-      List<String> expectedAttributeText,
     ) async {
       await pumpAndUnlockApp(
         tester,
@@ -45,7 +41,7 @@ void main() {
       await tester.tapAndSettle(find.byKey(const Key('nav_button_data')));
 
       // Expect CredentialTypeTile and tap it
-      var categoryTileFinder = find.byKey(const Key('irma-demo.gemeente.personalData_tile')).hitTestable();
+      var categoryTileFinder = find.byKey(const Key('irma-demo.gemeenten.personalData_tile')).hitTestable();
       await tester.scrollUntilVisible(categoryTileFinder, 75);
       await tester.tapAndSettle(categoryTileFinder);
 
@@ -55,121 +51,19 @@ void main() {
       // Expect to find a card
       final credentialCardFinder = find.byType(IrmaCredentialCard);
       expect(credentialCardFinder, findsOneWidget);
-
-      // Expect correct text in the header
-      final cardHeaderFinder =
-          find.descendant(of: credentialCardFinder, matching: find.byType(IrmaCredentialCardHeader));
-      final cardHeaderText = tester.getAllText(cardHeaderFinder);
-      expect(cardHeaderText, expectedHeaderText);
-
-      // Expect correct text in the attribute list
-      final attributeListFinder =
-          find.descendant(of: credentialCardFinder, matching: find.byType(IrmaCredentialCardAttributeList));
-      final actualAttributeListText = tester.getAllText(attributeListFinder);
-      final expectedAttributeListText = expectedAttributeText;
-      expect(actualAttributeListText, expectedAttributeListText);
     }
 
     testWidgets(
-        'issue-municipality-en',
-        (tester) => testIssueMunicipality(
-              tester,
-              const Locale('en', 'EN'),
-              [
-                'Demo Personal data',
-                'Demo Municipality',
-              ],
-              [
-                'BSN',
-                '999999990',
-                'City of birth',
-                'Amsterdam',
-                'Country of birth',
-                'Nederland',
-                'Date of birth',
-                '10-04-1965',
-                'Assurance level',
-                'Substantieel',
-                'Family name',
-                'Bruijn',
-                'First names',
-                'Willeke Liselotte',
-                'Full name',
-                'W.L. de Bruijn',
-                'Gender',
-                'V',
-                'Initials',
-                'W.L.',
-                'Dutch nationality',
-                'Yes',
-                'Over 12',
-                'Yes',
-                'Over 16',
-                'Yes',
-                'Over 18',
-                'Yes',
-                'Over 21',
-                'Yes',
-                'Over 65',
-                'No',
-                'Prefix',
-                'de',
-                'Surname',
-                'de Bruijn'
-              ],
-            ),
-        timeout: const Timeout(
-          Duration(minutes: 1, seconds: 30),
-        ));
+      'issue-municipality-en',
+      (tester) => testIssueMunicipality(tester, const Locale('en', 'EN')),
+      timeout: const Timeout(
+        Duration(minutes: 1, seconds: 30),
+      ),
+    );
 
     testWidgets(
       'issue-municipality-nl',
-      (tester) => testIssueMunicipality(
-        tester,
-        const Locale('nl', 'NL'),
-        [
-          'Demo Persoonsgegevens',
-          'Demo Gemeente',
-        ],
-        [
-          'BSN',
-          '999999990',
-          'Geboorteplaats',
-          'Amsterdam',
-          'Geboorteland',
-          'Nederland',
-          'Geboortedatum',
-          '10-04-1965',
-          'Betrouwbaarheidsniveau',
-          'Substantieel',
-          'Geslachtsnaam',
-          'Bruijn',
-          'Voornamen',
-          'Willeke Liselotte',
-          'Volledige naam',
-          'W.L. de Bruijn',
-          'Geslacht',
-          'V',
-          'Voorletters',
-          'W.L.',
-          'Nederlandse nationaliteit',
-          'Ja',
-          'Ouder dan 12',
-          'Ja',
-          'Ouder dan 16',
-          'Ja',
-          'Ouder dan 18',
-          'Ja',
-          'Ouder dan 21',
-          'Ja',
-          'Ouder dan 65',
-          'Nee',
-          'Voorvoegsel',
-          'de',
-          'Achternaam',
-          'de Bruijn'
-        ],
-      ),
+      (tester) => testIssueMunicipality(tester, const Locale('nl', 'NL')),
       timeout: const Timeout(
         Duration(minutes: 1, seconds: 30),
       ),
