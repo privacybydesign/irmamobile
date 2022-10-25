2ab4450f7c48d37dcba0e125664d86a1deadd8e6
Sjoerd Lemkens <sjoerd.lemkens@sidn.nl>
2022-10-25 09:25:47 +0200

    test: added timeouts

--- integration_test/disclosure_session/disclosure_session_test.dart
+++ integration_test/disclosure_session/disclosure_session_test.dart
@@ -25,32 +25,80 @@ void main() {
 
     group('empty-app-scenarios', () {
       // Full name AND nationality
-      testWidgets('no-choice', (tester) => noChoiceTest(tester, irmaBinding));
+      testWidgets(
+        'no-choice',
+        (tester) => noChoiceTest(tester, irmaBinding),
+        timeout: const Timeout(
+          Duration(minutes: 1),
+        ),
+      );
 
       // Email OR your mobile number.
-      testWidgets('choice', (tester) => choiceTest(tester, irmaBinding));
+      testWidgets(
+        'choice',
+        (tester) => choiceTest(tester, irmaBinding),
+        timeout: const Timeout(
+          Duration(minutes: 1),
+        ),
+      );
 
       // Email AND mobile number
-      testWidgets('no-choice-mltpl-creds', (tester) => noChoiceMltplCredsTest(tester, irmaBinding));
+      testWidgets(
+        'no-choice-mltpl-creds',
+        (tester) => noChoiceMltplCredsTest(tester, irmaBinding),
+        timeout: const Timeout(
+          Duration(minutes: 1),
+        ),
+      );
 
       // Address from multiplicity OR iDIN
       // AND your AGB code (from Nuts)
-      testWidgets('choice-mixed', (tester) => choiceMixedTest(tester, irmaBinding));
+      testWidgets(
+        'choice-mixed',
+        (tester) => choiceMixedTest(tester, irmaBinding),
+        timeout: const Timeout(
+          Duration(minutes: 1),
+        ),
+      );
 
       // Student/employee id from university OR
       // Full name from municipality AND email address
-      testWidgets('choice-mixed-sources', (tester) => choiceMixedSourcesTest(tester, irmaBinding));
+      testWidgets(
+        'choice-mixed-sources',
+        (tester) => choiceMixedSourcesTest(tester, irmaBinding),
+        timeout: const Timeout(
+          Duration(minutes: 1),
+        ),
+      );
 
       // Bank account number from iDeal. BIC has to be RABONL2U. AND
       // Initials, family name and city from iDIN. The city has to be Arnhem
-      testWidgets('specific-att-values', (tester) => specificAttValuesTest(tester, irmaBinding));
+      testWidgets(
+        'specific-att-values',
+        (tester) => specificAttValuesTest(tester, irmaBinding),
+        timeout: const Timeout(
+          Duration(minutes: 1),
+        ),
+      );
 
       // Address from iDIN or municipality
       // And optionally mobile number or e-mail address
-      testWidgets('optionals', (tester) => optionalsTest(tester, irmaBinding));
+      testWidgets(
+        'optionals',
+        (tester) => optionalsTest(tester, irmaBinding),
+        timeout: const Timeout(
+          Duration(minutes: 1),
+        ),
+      );
 
       // E-mail address or nothing
-      testWidgets('completely-optional', (tester) => completelyOptionalTest(tester, irmaBinding));
+      testWidgets(
+        'completely-optional',
+        (tester) => completelyOptionalTest(tester, irmaBinding),
+        timeout: const Timeout(
+          Duration(minutes: 1),
+        ),
+      );
     });
   });
 }
