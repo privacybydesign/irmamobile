f3d0d7f7311a72c0a31f3a717322fdb80c7c8791
Sjoerd Lemkens <sjoerd.lemkens@sidn.nl>
2022-11-17 11:31:42 +0100

    refactor: improved bloc state type interpretation

--- lib/src/screens/session/disclosure/widgets/disclosure_permission_choices_screen.dart
+++ lib/src/screens/session/disclosure/widgets/disclosure_permission_choices_screen.dart
@@ -91,11 +91,11 @@ class DisclosurePermissionChoicesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final state = this.state;
     final theme = IrmaTheme.of(context);
     final lang = FlutterI18n.currentLocale(context)!.languageCode;
 
-    if (state is DisclosurePermissionChoicesOverview &&
-        (state as DisclosurePermissionChoicesOverview).showConfirmationPopup) {
+    if (state is DisclosurePermissionChoicesOverview && state.showConfirmationPopup) {
       Future.delayed(Duration.zero, () => _showConfirmationDialog(context));
     }
 
@@ -150,8 +150,7 @@ class DisclosurePermissionChoicesScreen extends StatelessWidget {
               ),
             ),
             SizedBox(height: theme.defaultSpacing),
-            if (state is DisclosurePermissionChoicesOverview &&
-                (state as DisclosurePermissionChoicesOverview).isSignatureSession) ...[
+            if (state is DisclosurePermissionChoicesOverview && state.isSignatureSession) ...[
               TranslatedText(
                 'disclosure_permission.overview.sign',
                 style: theme.themeData.textTheme.headline4,
@@ -163,7 +162,7 @@ class DisclosurePermissionChoicesScreen extends StatelessWidget {
                 ),
                 child: IrmaQuote(
                   key: const Key('signature_message'),
-                  quote: (state as DisclosurePermissionChoicesOverview).signedMessage,
+                  quote: state.signedMessage,
                 ),
               ),
             ],
