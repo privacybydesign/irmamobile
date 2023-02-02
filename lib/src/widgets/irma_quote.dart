3555ae8f5ddfc061cf007f48847d0050116e774c
Sjoerd Lemkens <sjoerd.lemkens@sidn.nl>
2023-02-02 09:27:02 +0100

    feat: IrmaQuote now uses IrmaCard

--- lib/src/widgets/irma_quote.dart
+++ lib/src/widgets/irma_quote.dart
@@ -3,6 +3,7 @@ import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 
 import '../theme/theme.dart';
+import 'irma_card.dart';
 
 class IrmaQuote extends StatelessWidget {
   final String? quote;
@@ -28,36 +29,29 @@ class IrmaQuote extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = IrmaTheme.of(context);
 
-    return Container(
-      decoration: BoxDecoration(
-          color: color ?? theme.surfaceSecondary,
-          borderRadius: BorderRadius.circular(8),
-          border: Border.all(
-            color: theme.surfaceSecondary,
-          )),
-      child: Padding(
-        padding: EdgeInsets.all(theme.defaultSpacing),
-        child: Row(
-          children: [
-            Flexible(
-              child: quote != null
-                  ? MarkdownBody(
-                      data: FlutterI18n.translate(context, quote!),
-                      styleSheet: MarkdownStyleSheet.fromTheme(
-                        ThemeData(
-                          textTheme: TextTheme(
-                            bodyText2: theme.textTheme.bodyText2!.copyWith(
-                              fontSize: 14,
-                              height: 24 / 14,
-                            ),
+    return IrmaCard(
+      padding: EdgeInsets.zero,
+      style: IrmaCardStyle.highlighted,
+      child: Row(
+        children: [
+          Flexible(
+            child: quote != null
+                ? MarkdownBody(
+                    data: FlutterI18n.translate(context, quote!),
+                    styleSheet: MarkdownStyleSheet.fromTheme(
+                      ThemeData(
+                        textTheme: TextTheme(
+                          bodyText2: theme.textTheme.bodyText2!.copyWith(
+                            fontSize: 14,
+                            fontWeight: FontWeight.normal,
                           ),
                         ),
                       ),
-                    )
-                  : richQuote!,
-            )
-          ],
-        ),
+                    ),
+                  )
+                : richQuote!,
+          )
+        ],
       ),
     );
   }
