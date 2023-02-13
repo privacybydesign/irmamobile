cc22ef85670abfc27cf7b1ff2c09f58466296dfd
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2023-02-13 15:25:48 +0100

    fix: don't show anmiation when returning to IntroductionScreen

--- lib/src/screens/enrollment/introduction/introduction_screen.dart
+++ lib/src/screens/enrollment/introduction/introduction_screen.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:irmamobile/src/screens/enrollment/introduction/widgets/introduction_animation_wrapper.dart';
 
 import '../widgets/enrollment_graphic.dart';
 import '../widgets/enrollment_instruction.dart';
 import '../widgets/enrollment_layout.dart';
 import 'models/introduction_step.dart';
+import 'widgets/introduction_animation_wrapper.dart';
 
-class IntroductionScreen extends StatelessWidget {
+class IntroductionScreen extends StatefulWidget {
   static const String routeName = 'introduction';
 
   static List<IntroductionStep> introductionSteps = List.generate(
@@ -33,23 +33,47 @@ class IntroductionScreen extends StatelessWidget {
   });
 
   @override
+  State<IntroductionScreen> createState() => _IntroductionScreenState();
+}
+
+class _IntroductionScreenState extends State<IntroductionScreen> {
+  bool skipAnimation = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // Skip the animation when going back from a screen
+    // further in the enrollment flow
+    if (widget.currentStepIndex > 0) {
+      skipAnimation = true;
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: IntroductionAnimationWrapper(
-        child: EnrollmentLayout(
-          graphic: EnrollmentGraphic(
-            introductionSteps[currentStepIndex].imagePath,
-          ),
-          instruction: EnrollmentInstruction(
-            stepIndex: currentStepIndex,
-            stepCount: introductionSteps.length,
-            titleTranslationKey: introductionSteps[currentStepIndex].titleTranslationKey,
-            explanationTranslationKey: introductionSteps[currentStepIndex].explanationTranslationKey,
-            onContinue: onContinue,
-            onPrevious: currentStepIndex != 0 ? onPrevious : null,
-          ),
-        ),
+    Widget contentWidget = EnrollmentLayout(
+      graphic: EnrollmentGraphic(
+        IntroductionScreen.introductionSteps[widget.currentStepIndex].imagePath,
       ),
+      instruction: EnrollmentInstruction(
+        stepIndex: widget.currentStepIndex,
+        stepCount: IntroductionScreen.introductionSteps.length,
+        titleTranslationKey: IntroductionScreen.introductionSteps[widget.currentStepIndex].titleTranslationKey,
+        explanationTranslationKey:
+            IntroductionScreen.introductionSteps[widget.currentStepIndex].explanationTranslationKey,
+        onContinue: widget.onContinue,
+        onPrevious: widget.currentStepIndex != 0 ? widget.onPrevious : null,
+      ),
+    );
+
+    if (!skipAnimation) {
+      contentWidget = IntroductionAnimationWrapper(
+        child: contentWidget,
+      );
+    }
+
+    return Scaffold(
+      body: contentWidget,
     );
   }
 }
