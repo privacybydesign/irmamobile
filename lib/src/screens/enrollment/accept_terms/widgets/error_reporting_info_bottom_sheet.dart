7b94137edd1dea34c2bdbd311f0b9c3fefc6b32e
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2023-03-09 07:54:33 +0100

    feat: enrollment error reporting opt in

---
+++ lib/src/screens/enrollment/accept_terms/widgets/error_reporting_info_bottom_sheet.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import '../../../../theme/theme.dart';
+import '../../../../widgets/irma_bottom_sheet.dart';
+import '../../../../widgets/translated_text.dart';
+
+class ErrorReportingInfoBottomSheet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final theme = IrmaTheme.of(context);
+
+    return IrmaBottomSheet(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TranslatedText(
+            'enrollment.error_reporting.dialog.title',
+            style: theme.textTheme.headline3,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: theme.defaultSpacing),
+            child: TranslatedText(
+              'enrollment.error_reporting.dialog.explanation',
+              style: theme.textTheme.bodyText2,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
