fa83ada88318a7804d98ba32649954611bde9b95
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2023-03-16 15:18:13 +0100

    refactor: duplicate code in add data and data tab

--- lib/src/screens/data/data_tab.dart
+++ lib/src/screens/data/data_tab.dart
@@ -1,98 +1,64 @@
-import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_i18n/flutter_i18n.dart';
-import 'package:sliver_tools/sliver_tools.dart';
 
-import '../../models/credentials.dart';
 import '../../models/irma_configuration.dart';
 import '../../theme/theme.dart';
 import '../../widgets/irma_action_card.dart';
-import '../../widgets/irma_repository_provider.dart';
 import '../add_data/add_data_screen.dart';
+import 'credentials_detail_screen.dart';
 import 'widgets/credential_category_list.dart';
+import 'widgets/credential_types_builder.dart';
 
 class DataTab extends StatelessWidget {
-  List<CredentialType> _distinctCredentialTypes(Iterable<CredentialType> credentialTypes) {
-    var idSet = <String>{};
-    var distinct = <CredentialType>[];
-    for (var credType in credentialTypes) {
-      if (idSet.add(credType.fullId)) {
-        distinct.add(credType);
-      }
-    }
-    return distinct;
-  }
+  _navToCredTypeDetailScreen(
+    BuildContext context,
+    String credentialTypeId,
+    String categoryName,
+  ) =>
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => CredentialsDetailScreen(
+            categoryName: categoryName,
+            credentialTypeId: credentialTypeId,
+          ),
+        ),
+      );
 
   @override
   Widget build(BuildContext context) {
-    final repo = IrmaRepositoryProvider.of(context);
     final theme = IrmaTheme.of(context);
-    final lang = FlutterI18n.currentLocale(context)!.languageCode;
 
-    return CustomScrollView(
-      slivers: [
-        SliverPadding(
-          padding: EdgeInsets.all(
-            theme.defaultSpacing,
+    return SingleChildScrollView(
+      padding: EdgeInsets.all(theme.defaultSpacing),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          IrmaActionCard(
+            titleKey: 'data.tab.obtain_data',
+            onTap: () => Navigator.of(context).pushNamed(AddDataScreen.routeName),
+            icon: Icons.add_circle_sharp,
           ),
-          sliver: SliverToBoxAdapter(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                IrmaActionCard(
-                  titleKey: 'data.tab.obtain_data',
-                  onTap: () => Navigator.of(context).pushNamed(AddDataScreen.routeName),
-                  icon: Icons.add_circle_sharp,
-                ),
-              ],
-            ),
-          ),
-        ),
-        StreamBuilder(
-          stream: repo.getCredentials(),
-          builder: (context, AsyncSnapshot<Credentials> snapshot) {
-            if (!snapshot.hasData) {
-              return SliverToBoxAdapter(
-                child: Container(),
-              );
-            }
-            final filteredCredentials = snapshot.data!.values.where(
-              (element) => !element.isKeyshareCredential,
-            );
-
-            final credentialTypesByCategoryNames = groupBy(
-              _distinctCredentialTypes(
-                filteredCredentials.map((e) => e.info.credentialType),
-              ),
-              (CredentialType credType) =>
-                  credType.category.hasTranslation(lang) ? credType.category.translate(lang) : null,
-            );
-            return MultiSliver(
-              children: [
-                for (var credentialTypesByCategory
-                    in credentialTypesByCategoryNames.entries.where((entry) => entry.key != null))
-                  CredentialCategoryList(
-                    categoryName: credentialTypesByCategory.key!,
-                    credentialTypes: credentialTypesByCategory.value,
-                  ),
-                // If 'other' credentials are present, render them last
-                if (credentialTypesByCategoryNames.containsKey(null))
-                  CredentialCategoryList(
-                    categoryName: FlutterI18n.translate(context, 'data.category_other'),
-                    credentialTypes: _distinctCredentialTypes(
-                      credentialTypesByCategoryNames[null]!,
+          CredentialTypesBuilder(
+            builder: (context, groupedCredentialTypes) => Column(
+              children: groupedCredentialTypes.entries
+                  .map(
+                    (credentialTypesByCategory) => CredentialCategoryList(
+                      categoryName: credentialTypesByCategory.key,
+                      credentialTypes: credentialTypesByCategory.value,
+                      onCredentialTypeTap: (CredentialType credType) => _navToCredTypeDetailScreen(
+                        context,
+                        credType.fullId,
+                        credentialTypesByCategory.key,
+                      ),
                     ),
                   )
-              ],
-            );
-          },
-        ),
-        SliverToBoxAdapter(
-          child: SizedBox(
-            height: theme.mediumSpacing,
+                  .toList(growable: false),
+            ),
           ),
-        )
-      ],
+          SizedBox(
+            height: theme.defaultSpacing,
+          )
+        ],
+      ),
     );
   }
 }
