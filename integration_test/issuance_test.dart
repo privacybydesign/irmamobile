40820972e59ec025ce75c2ec1dee40efc2b10412
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2023-07-31 14:12:38 +0200

    fix: accidentally staged commented tests

--- integration_test/issuance_test.dart
+++ integration_test/issuance_test.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:irmamobile/src/screens/data/credentials_detail_screen.dart';
+import 'package:irmamobile/src/screens/session/widgets/issuance_success_screen.dart';
+import 'package:irmamobile/src/screens/session/widgets/success_graphic.dart';
 import 'package:irmamobile/src/widgets/credential_card/irma_credential_card.dart';
 
 import 'helpers/helpers.dart';
@@ -19,90 +21,90 @@ void main() {
     setUp(() => irmaBinding.setUp());
     tearDown(() => irmaBinding.tearDown());
 
-    // Future<void> testIssueMunicipality(
-    //   WidgetTester tester,
-    //   Locale locale,
-    // ) async {
-    //   await pumpAndUnlockApp(
-    //     tester,
-    //     irmaBinding.repository,
-    //     locale,
-    //   );
+    Future<void> testIssueMunicipality(
+      WidgetTester tester,
+      Locale locale,
+    ) async {
+      await pumpAndUnlockApp(
+        tester,
+        irmaBinding.repository,
+        locale,
+      );
 
-    //   await issueMunicipalityPersonalData(
-    //     tester,
-    //     irmaBinding,
-    //     locale: locale,
-    //   );
-    //   await tester.pumpAndSettle();
+      await issueMunicipalityPersonalData(
+        tester,
+        irmaBinding,
+        locale: locale,
+      );
+      await tester.pumpAndSettle();
 
-    //   final successScreenFinder = find.byType(IssuanceSuccessScreen);
-    //   expect(successScreenFinder, findsOneWidget);
+      final successScreenFinder = find.byType(IssuanceSuccessScreen);
+      expect(successScreenFinder, findsOneWidget);
 
-    //   // Expect the SuccessGraphic in the IssuanceSuccessScreen
-    //   final successGraphicFinder = find.byType(SuccessGraphic);
-    //   expect(
-    //     find.descendant(
-    //       of: successScreenFinder,
-    //       matching: successGraphicFinder,
-    //     ),
-    //     findsOneWidget,
-    //   );
+      // Expect the SuccessGraphic in the IssuanceSuccessScreen
+      final successGraphicFinder = find.byType(SuccessGraphic);
+      expect(
+        find.descendant(
+          of: successScreenFinder,
+          matching: successGraphicFinder,
+        ),
+        findsOneWidget,
+      );
 
-    //   await tester.tapAndSettle(find.text('OK'));
+      await tester.tapAndSettle(find.text('OK'));
 
-    //   // Go to data tab
-    //   await tester.tapAndSettle(find.byKey(const Key('nav_button_data')));
+      // Go to data tab
+      await tester.tapAndSettle(find.byKey(const Key('nav_button_data')));
 
-    //   // Expect CredentialTypeTile and tap it
-    //   var categoryTileFinder = find.byKey(const Key('irma-demo.gemeente.personalData_tile')).hitTestable();
-    //   await tester.scrollUntilVisible(categoryTileFinder, 75);
-    //   await tester.tapAndSettle(categoryTileFinder);
+      // Expect CredentialTypeTile and tap it
+      var categoryTileFinder = find.byKey(const Key('irma-demo.gemeente.personalData_tile')).hitTestable();
+      await tester.scrollUntilVisible(categoryTileFinder, 75);
+      await tester.tapAndSettle(categoryTileFinder);
 
-    //   // Expect detail page
-    //   expect(find.byType(CredentialsDetailScreen), findsOneWidget);
+      // Expect detail page
+      expect(find.byType(CredentialsDetailScreen), findsOneWidget);
 
-    //   // Expect to find a card
-    //   final credentialCardFinder = find.byType(IrmaCredentialCard);
-    //   expect(credentialCardFinder, findsOneWidget);
-    // }
+      // Expect to find a card
+      final credentialCardFinder = find.byType(IrmaCredentialCard);
+      expect(credentialCardFinder, findsOneWidget);
+    }
 
-    // testWidgets(
-    //   'issue-municipality-en',
-    //   (tester) => testIssueMunicipality(tester, const Locale('en', 'EN')),
-    // );
+    testWidgets(
+      'issue-municipality-en',
+      (tester) => testIssueMunicipality(tester, const Locale('en', 'EN')),
+    );
 
-    // testWidgets(
-    //   'issue-municipality-nl',
-    //   (tester) => testIssueMunicipality(tester, const Locale('nl', 'NL')),
-    // );
+    testWidgets(
+      'issue-municipality-nl',
+      (tester) => testIssueMunicipality(tester, const Locale('nl', 'NL')),
+    );
 
-    // testWidgets('decline', (tester) async {
-    //   await pumpAndUnlockApp(tester, irmaBinding.repository);
+    testWidgets('decline', (tester) async {
+      await pumpAndUnlockApp(tester, irmaBinding.repository);
 
-    //   // Start an issuance session for email address and decline the offer.
-    //   await issueCredentials(
-    //     tester,
-    //     irmaBinding,
-    //     {
-    //       'irma-demo.sidn-pbdf.email.email': 'test@demo.com',
-    //       'irma-demo.sidn-pbdf.email.domain': 'demo.com',
-    //     },
-    //     declineOffer: true,
-    //   );
+      // Start an issuance session for email address and decline the offer.
+      await issueCredentials(
+        tester,
+        irmaBinding,
+        {
+          'irma-demo.sidn-pbdf.email.email': 'test@demo.com',
+          'irma-demo.sidn-pbdf.email.domain': 'demo.com',
+        },
+        declineOffer: true,
+      );
 
-    //   // Go to data tab.
-    //   await tester.tapAndSettle(find.byKey(const Key('nav_button_data')));
+      // Go to data tab.
+      await tester.tapAndSettle(find.byKey(const Key('nav_button_data')));
 
-    //   // Verify that the email address has not been added.
-    //   final emailTileFinder = find.byKey(const Key('irma-demo.sidn-pbdf.email_tile'));
-    //   expect(emailTileFinder, findsNothing);
+      // Verify that the email address has not been added.
+      final emailTileFinder = find.byKey(const Key('irma-demo.sidn-pbdf.email_tile'));
+      expect(emailTileFinder, findsNothing);
 
-    //   // Verify that no activity has been added.
-    //   await tester.tap(find.byKey(const Key('nav_button_activity')));
-    //   await tester.pump(const Duration(seconds: 1));
-    //   expect(find.text('There are no logged activities yet'), findsOneWidget);
-    // });
+      // Verify that no activity has been added.
+      await tester.tap(find.byKey(const Key('nav_button_activity')));
+      await tester.pump(const Duration(seconds: 1));
+      expect(find.text('There are no logged activities yet'), findsOneWidget);
+    });
 
     testWidgets('reissue', (tester) async {
       await pumpAndUnlockApp(tester, irmaBinding.repository);
