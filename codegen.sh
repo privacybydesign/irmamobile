46e03f4a57c17abf84f83e865f16a38be724141b
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2023-09-27 13:12:17 +0200

    fix: flutter format is deprecated

--- codegen.sh
+++ codegen.sh
@@ -2,4 +2,4 @@
 
 flutter pub run build_runner build --delete-conflicting-outputs
 flutter pub run flutter_launcher_icons:main
-flutter format --line-length=120 lib/ test/ integration_test/
+dart format --line-length=120 lib/ test/ integration_test/
