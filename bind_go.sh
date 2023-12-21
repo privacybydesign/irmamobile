3e8d3eaa930f93f05463d2cf0104adc790d247a1
Bob Hageman <bob@schluss.org>
2023-12-21 09:56:38 +0100

    fix: xcodebuild failing because of symlink issues

--- bind_go.sh
+++ bind_go.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# Fix xcodebuild (15.0.1) symlink tmpdir issue (https://developer.apple.com/forums/thread/738091)
+TMPDIR=$(cd -P "$TMPDIR" && pwd)
+
 gomobile bind -target android -androidapi 23 -o android/irmagobridge/irmagobridge.aar github.com/privacybydesign/irmamobile/irmagobridge
 gomobile bind -target ios -iosversion 12.0 -o ios/Runner/Irmagobridge.xcframework github.com/privacybydesign/irmamobile/irmagobridge
