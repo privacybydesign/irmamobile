0ac83c0937139fc0e72137765c1ac92845366e4e
Ivar Derksen <ivar.derksen@sidn.nl>
2024-03-12 01:04:50 +0100

    CI/CD: select xcode version in ci_scripts to improve cache handling

---
+++ ci_scripts/select_xcode.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+XCODE_VERSION="14.3.1"
+
+# Location is based on the Xcode versions bundled with the macOS runners in GitHub Actions.
+# https://github.com/actions/runner-images/blob/main/images/macos/macos-13-Readme.md#xcode
+sudo xcode-select -s /Applications/Xcode_${XCODE_VERSION}.app/Contents/Developer
