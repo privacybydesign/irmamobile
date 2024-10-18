b666b3533c044ff59596847355ffcc6a9b720d68
Ivar Derksen <ivar.derksen@sidn.nl>
2024-10-18 13:53:00 +0200

    Chore: use macos-14 runner

--- ci_scripts/select_xcode.sh
+++ ci_scripts/select_xcode.sh
@@ -4,5 +4,5 @@ set -euxo pipefail
 XCODE_VERSION="15.4"
 
 # Location is based on the Xcode versions bundled with the macOS runners in GitHub Actions.
-# https://github.com/actions/runner-images/blob/main/images/macos/macos-13-Readme.md#xcode
+# https://github.com/actions/runner-images/blob/main/images/macos/macos-14-Readme.md#xcode
 sudo xcode-select -s /Applications/Xcode_${XCODE_VERSION}.app/Contents/Developer
