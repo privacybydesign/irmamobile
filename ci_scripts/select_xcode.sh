56a99603d67e831d24335d3b8ac168900219a6a2
Wouter Ensink <46427708+w-ensink@users.noreply.github.com>
2024-11-04 16:50:03 +0100

    Chore: set Xcode version to 16.1

--- ci_scripts/select_xcode.sh
+++ ci_scripts/select_xcode.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-XCODE_VERSION="16.0"
+XCODE_VERSION="16.1"
 
 # Location is based on the Xcode versions bundled with the macOS runners in GitHub Actions.
 # https://github.com/actions/runner-images/blob/main/images/macos/macos-14-Readme.md#xcode
