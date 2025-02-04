a01657ca78d69c27d778e53acf9db918ef14b70b
Wouter Ensink <46427708+w-ensink@users.noreply.github.com>
2025-02-04 09:40:13 +0100

    make sheet draggable by title & add scroll indication
    - scroll indication is that the title bar changes color when scrolled
    under

--- lib/src/widgets/irma_bottom_sheet.dart
+++ lib/src/widgets/irma_bottom_sheet.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import '../theme/theme.dart';
 import 'irma_close_button.dart';
 
-class IrmaBottomSheet extends StatelessWidget {
+class IrmaBottomSheet extends StatefulWidget {
   final Widget child;
   final Widget? title;
 
@@ -11,6 +11,32 @@ class IrmaBottomSheet extends StatelessWidget {
     this.title,
     required this.child,
   });
+
+  @override
+  State<IrmaBottomSheet> createState() => _IrmaBottomSheetState();
+}
+
+class _IrmaBottomSheetState extends State<IrmaBottomSheet> {
+  final _controller = ScrollController();
+  bool _scrolledDown = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(_scrollPositionChanged);
+  }
+
+  _scrollPositionChanged() {
+    if (_controller.offset > 0 && !_scrolledDown) {
+      setState(() {
+        _scrolledDown = true;
+      });
+    } else if (_controller.offset <= 0 && _scrolledDown) {
+      setState(() {
+        _scrolledDown = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,27 +49,31 @@ class IrmaBottomSheet extends StatelessWidget {
           top: Radius.circular(20),
         ),
       ),
-      child: CustomScrollView(
-        slivers: [
-          PinnedHeaderSliver(
-            child: Container(
-              decoration: BoxDecoration(
-                color: theme.backgroundPrimary,
-                borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: theme.defaultSpacing),
-                    child: title ?? Container(),
-                  ),
-                  IrmaCloseButton(),
-                ],
-              ),
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: _scrolledDown ? theme.backgroundSecondary : theme.backgroundPrimary,
+              borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: theme.defaultSpacing),
+                  child: widget.title ?? Container(),
+                ),
+                IrmaCloseButton(),
+              ],
             ),
           ),
-          SliverToBoxAdapter(child: child),
+          Expanded(
+            child: SingleChildScrollView(
+              physics: AlwaysScrollableScrollPhysics(),
+              controller: _controller,
+              child: widget.child,
+            ),
+          ),
         ],
       ),
     );
