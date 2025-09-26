f8d31195b03beecddaba67d45ac4e1bc9804c87b
Wouter Ensink <46427708+w-ensink@users.noreply.github.com>
2025-09-26 10:27:04 +0200

    improve mrz reading screen

--- lib/src/screens/passport/mrz_reader_screen.dart
+++ lib/src/screens/passport/mrz_reader_screen.dart
@@ -1,9 +1,14 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:mrz_parser/mrz_parser.dart';
 
 import '../../theme/theme.dart';
 import '../../widgets/irma_app_bar.dart';
 import '../../widgets/irma_bottom_bar.dart';
+import '../../widgets/irma_themed_button.dart';
+import '../../widgets/yivi_themed_button.dart';
 import 'widgets/mzr_scanner.dart';
 
 typedef MRZController = GlobalKey<MRZScannerState>;
@@ -31,24 +36,87 @@ class _MzrReaderScreenState extends State<MzrReaderScreen> {
   Widget build(BuildContext context) {
     final theme = IrmaTheme.of(context);
 
-    return Scaffold(
-      backgroundColor: theme.backgroundTertiary,
-      appBar: IrmaAppBar(
-        titleTranslationKey: 'passport.scan.title',
-      ),
-      body: MRZScanner(
-        controller: controller,
-        showOverlay: true,
-        onSuccess: (mrzResult, lines) async {
-          widget.onSuccess(mrzResult);
-        },
-      ),
-      bottomNavigationBar: IrmaBottomBar(
-        primaryButtonLabel: 'passport.scan.manual',
-        onPrimaryPressed: widget.onManualAdd,
-        secondaryButtonLabel: 'ui.cancel',
-        onSecondaryPressed: widget.onCancel,
-        alignment: IrmaBottomBarAlignment.vertical,
+    return OrientationBuilder(builder: (context, orientation) {
+      if (orientation == Orientation.portrait) {
+        return Scaffold(
+          backgroundColor: theme.backgroundTertiary,
+          appBar: IrmaAppBar(
+            titleTranslationKey: 'passport.scan.title',
+          ),
+          body: MRZScanner(
+            controller: controller,
+            showOverlay: true,
+            onSuccess: (mrzResult, lines) async {
+              widget.onSuccess(mrzResult);
+            },
+          ),
+          bottomNavigationBar: IrmaBottomBar(
+            primaryButtonLabel: 'passport.scan.manual',
+            onPrimaryPressed: widget.onManualAdd,
+            secondaryButtonLabel: 'ui.cancel',
+            onSecondaryPressed: widget.onCancel,
+            alignment: IrmaBottomBarAlignment.vertical,
+          ),
+        );
+      }
+
+      return Scaffold(
+        backgroundColor: theme.backgroundTertiary,
+        appBar: IrmaAppBar(
+          titleTranslationKey: 'passport.scan.title',
+        ),
+        body: MRZScanner(
+          controller: controller,
+          showOverlay: true,
+          onSuccess: (mrzResult, lines) async {
+            widget.onSuccess(mrzResult);
+          },
+        ),
+        floatingActionButton: _ManualEntryButton(onTap: widget.onManualAdd),
+      );
+    });
+  }
+}
+
+class _ManualEntryButton extends StatelessWidget {
+  const _ManualEntryButton({required this.onTap});
+
+  final Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = IrmaTheme.of(context);
+
+    return SizedBox(
+      height: 72,
+      width: 72,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: SvgPicture.asset(
+              'assets/ui/round-btn-bg.svg',
+            ),
+          ),
+          Positioned.fill(
+            child: ClipOval(
+              child: Material(
+                type: MaterialType.transparency,
+                child: Semantics(
+                  button: true,
+                  label: FlutterI18n.translate(context, 'passport.scan.manual'),
+                  child: InkWell(
+                    onTap: onTap,
+                    child: Icon(
+                      Icons.edit,
+                      color: theme.light,
+                      size: 42,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
